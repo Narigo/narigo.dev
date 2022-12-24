11e5b54c5a1b74650bc6bdea21f1e2a5e6a5303f
Joern Bernhardt <joern.bernhardt@compose.us>
2022-12-24 01:33:38 +0100

    fix: Improve wording on talks page
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/talks/+page.svelte
+++ src/routes/talks/+page.svelte
@@ -4,8 +4,11 @@
 </script>
 
 <PageLayout>
-	<h2>Here will be a list of the talks.</h2>
-	<p>For now, it's a placeholder page.</p>
+	<h2>Slides for my talks</h2>
+	<p>
+		I usually share the slide deck to my talks for people who attended the
+		meetup/conference/whatever. Reach out if you have questions!
+	</p>
 	<ul>
 		<li>
 			<a href="{base}/talks/static-pages-with-dynamic-content"
@@ -13,7 +16,7 @@
 			>
 			-
 			<a href="https://www.meetup.com/de-DE/frankenjs/events/289645924/" rel="external"
-				>FrankenJS Meetup</a
+				>FrankenJS Meetup 2022-12-07</a
 			>
 		</li>
 	</ul>
