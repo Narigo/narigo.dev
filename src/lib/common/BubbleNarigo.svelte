8bb20e79c01bf4a12500f6ed08299b61a5dbe1a0
Joern Bernhardt <joern.bernhardt@compose.us>
2023-02-09 20:29:28 +0100

    feat: Use a different bubble avatar for reply
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/lib/common/BubbleNarigo.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Bubble from './Bubble.svelte';
+
+	export let side: 'left' | 'right' = 'left';
+</script>
+
+<Bubble {...$$restProps} {side} >
+	<slot />
+	<div slot="avatar">😏</div>
+</Bubble>
--- src/routes/specials/+page.svelte
+++ src/routes/specials/+page.svelte
@@ -1,29 +1,30 @@
 <script>
 	import { base } from '$app/paths';
 	import Bubble from '$lib/common/Bubble.svelte';
+	import BubbleNarigo from '$lib/common/BubbleNarigo.svelte';
 	import PageLayout from '$lib/common/PageLayout/PageLayout.svelte';
 </script>
 
 <PageLayout>
 	<h2>Specials</h2>
 	<Bubble animation="dialog" delayNext={1000}>Hey, what's up with this page?</Bubble>
-	<Bubble animation="dialog" delayNext={1000} side="right"
-		>I thought I may want to write some code here.</Bubble
+	<BubbleNarigo animation="dialog" delayNext={1000}
+		>I thought I may want to write some code here.</BubbleNarigo
 	>
 	<Bubble animation="dialog" delayNext={1000}>Okay, but ...</Bubble>
 	<Bubble animation="dialog" delayNext={1000}>what?</Bubble>
-	<Bubble animation="dialog" delayNext={1000} side="right"
-		>For example an animation for this "exchange" here.</Bubble
+	<BubbleNarigo animation="dialog" delayNext={1000} 
+		>For example an animation for this "exchange" here.</BubbleNarigo
 	>
-	<Bubble animation="dialog" delayNext={1000} side="right"
-		>You can <a href="{base}/specials/talking/create">create your own</a>, if you want.</Bubble
+	<BubbleNarigo animation="dialog" delayNext={1000}
+		>You can <a href="{base}/specials/talking/create">create your own</a>, if you want.</BubbleNarigo
 	>
-	<Bubble animation="dialog" delayNext={1000} side="right"
+	<BubbleNarigo animation="dialog" delayNext={1000}
 		>Or a clone of a word game where you can provide your own word: <a
 			href="{base}/specials/weirdle">Weirdle</a
-		></Bubble
+		></BubbleNarigo
 	>
-	<Bubble animation="dialog" delayNext={1000} side="right"
-		>Or <a href="{base}/specials/recharge">Recharge</a> someone</Bubble
+	<BubbleNarigo animation="dialog" delayNext={1000}
+		>Or <a href="{base}/specials/recharge">Recharge</a> someone</BubbleNarigo
 	>
 </PageLayout>
