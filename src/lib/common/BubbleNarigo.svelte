bbf846e373dd78f3321b67282408b10db822e739
Joern Bernhardt <joern.bernhardt@compose.us>
2023-02-10 16:42:19 +0100

    feat: Add image to my Bubble
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/common/BubbleNarigo.svelte
+++ src/lib/common/BubbleNarigo.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import Bubble from './Bubble.svelte';
+	import narigo from './avatar/narigo.png';
 
 	export let side: 'left' | 'right' = 'left';
 </script>
 
 <Bubble {...$$restProps} {side} >
 	<slot />
-	<div slot="avatar">😏</div>
+	<div slot="avatar"><img src="{narigo}" alt="Narigo" /></div>
 </Bubble>
+
+<style>
+	img {
+		border-radius: 50%;
+		max-height: 1em;
+		max-width: 1em;
+	}
+</style>
--- src/routes/specials/+page.svelte
+++ src/routes/specials/+page.svelte
@@ -8,23 +8,23 @@
 <PageLayout>
 	<h2>Specials</h2>
 	<Bubble animation="dialog" delayNext={1000}>Hey, what's up with this page?</Bubble>
-	<BubbleNarigo animation="dialog" delayNext={1000}
+	<BubbleNarigo animation="dialog" delayNext={1000} side="right"
 		>I thought I may want to write some code here.</BubbleNarigo
 	>
 	<Bubble animation="dialog" delayNext={1000}>Okay, but ...</Bubble>
 	<Bubble animation="dialog" delayNext={1000}>what?</Bubble>
-	<BubbleNarigo animation="dialog" delayNext={1000} 
+	<BubbleNarigo animation="dialog" delayNext={1000} side="right"
 		>For example an animation for this "exchange" here.</BubbleNarigo
 	>
-	<BubbleNarigo animation="dialog" delayNext={1000}
+	<BubbleNarigo animation="dialog" delayNext={1000} side="right"
 		>You can <a href="{base}/specials/talking/create">create your own</a>, if you want.</BubbleNarigo
 	>
-	<BubbleNarigo animation="dialog" delayNext={1000}
+	<BubbleNarigo animation="dialog" delayNext={1000} side="right"
 		>Or a clone of a word game where you can provide your own word: <a
 			href="{base}/specials/weirdle">Weirdle</a
 		></BubbleNarigo
 	>
-	<BubbleNarigo animation="dialog" delayNext={1000}
+	<BubbleNarigo animation="dialog" delayNext={1000} side="right"
 		>Or <a href="{base}/specials/recharge">Recharge</a> someone</BubbleNarigo
 	>
 </PageLayout>
