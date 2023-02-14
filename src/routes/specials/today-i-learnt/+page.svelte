d8e9bac424b057ed535c9ba6eb3ea570d712b2f0
Joern Bernhardt <joern.bernhardt@compose.us>
2023-02-14 23:52:14 +0100

    feat: Add TIL page
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/specials/today-i-learnt/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import PageLayout from '$lib/common/PageLayout/PageLayout.svelte';
+</script>
+
+<PageLayout>
+	<h2>Today I learnt</h2>
+	<Narigo>
+		To remember more of the little things I learn, I want to write them down. I'll update this page
+		whenever I find something worth to log. Most of this will be around web development, but I don't
+		want to limit myself to that. Let's see where this leads to.
+	</Narigo>
+</PageLayout>
