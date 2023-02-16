a2498bcc2cbefb95925d07abab7bf82ce41159f3
Joern Bernhardt <joern.bernhardt@compose.us>
2023-02-16 23:36:50 +0100

    feat: Add TIL for drawing with linear gradients
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/specials/today-i-learnt/+page.svelte
+++ src/routes/specials/today-i-learnt/+page.svelte
@@ -10,4 +10,16 @@
 		whenever I find something worth to log. Most of this will be around web development, but I don't
 		want to limit myself to that. Let's see where this leads to.
 	</Narigo>
+	<ul>
+		<li>
+			<h3>Drawing with linear gradients</h3>
+			<p>
+				In VirtualCoffee Frontend Friday Folks, we have to create divs that resemble a given image
+				on CSSBattle.dev. Lately, I've tried to do the puzzles by styling them with <code
+					>background: linear-gradient()</code
+				>. When you find out a way how to draw it like that, it always feels like a hack of the
+				puzzle itself.
+			</p>
+		</li>
+	</ul>
 </PageLayout>
