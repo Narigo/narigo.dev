279ad0ca283a0497e2649c55748131de0c1dd974
Joern Bernhardt <joern.bernhardt@compose.us>
2023-03-04 21:50:46 +0100

    feat: Add puzzle 37
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-37/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Bubble from '$lib/common/bubble/Bubble.svelte';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div><div><div></div></div></div>
+<style>
+  body,div {
+    background: #6592CF;
+    display: grid;
+    place-items: center;
+    rotate: -15deg;
+  }
+  div {
+    background: #243D83;
+    height: 250px;
+    width: 250px;
+    rotate: 15deg;
+  }
+  div > div {
+    background: #6592CF;
+    height: 150px;
+    width: 150px;
+  }
+  div > div > div {
+    background: #243D83;
+    height: 75px;
+    width: 75px;
+  }
+</style>
+`;
+</script>
+
+<PuzzlePageLayout>
+	<Puzzle title="Puzzle 37 solution" {solution} />
+	<Narigo>
+		Ok, I initially thought that <code>::before</code> and <code>::after</code> could be used for nesting.
+		But that's not really the case, so the rotation after rotating is not possible easily.
+	</Narigo>
+</PuzzlePageLayout>
