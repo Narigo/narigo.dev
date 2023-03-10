3ada1e1f74464e10397fbbe6bdbe98a05147c856
Joern Bernhardt <joern.bernhardt@compose.us>
2023-03-10 23:57:27 +0100

    feat: Add solution to puzzle 38
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-38/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div></div>
+<div class="b"></div>
+<style>
+  body {
+    background: #293462;
+    margin: 0;
+  }
+  div {
+    position: absolute;
+    width: 300px;
+    height: 200px;
+    background:
+      linear-gradient(90deg, #FFF1C1 200px, transparent 0),
+      linear-gradient(#FE5F55 150px, #A64942 0)
+    ;
+  }
+  .b {
+    transform: translate(-50px, 50px) scaleX(-1) rotate(90deg);
+  }
+</style>`;
+</script>
+
+<PuzzlePageLayout>
+	<Puzzle title="Puzzle 38 solution" {solution} />
+	<Narigo>
+		Whenever doing a <code>transform</code>, remember that the order matters...!
+	</Narigo>
+</PuzzlePageLayout>
