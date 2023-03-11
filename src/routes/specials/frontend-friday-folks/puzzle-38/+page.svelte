2bdbdb97cdd7bb26288135b3fac33ff7684d4640
Joern Bernhardt <joern.bernhardt@compose.us>
2023-03-11 03:29:43 +0100

    feat: Add solution for one without element
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/specials/frontend-friday-folks/puzzle-38/+page.svelte
+++ src/routes/specials/frontend-friday-folks/puzzle-38/+page.svelte
@@ -23,6 +23,19 @@
     transform: translate(-50px, 50px) scaleX(-1) rotate(90deg);
   }
 </style>`;
+
+	const solutionNoElement = `<style>
+  body {
+    background:
+      linear-gradient(#FFF1C1, #FFF1C1) no-repeat,
+      linear-gradient(90deg, #FE5F55 150px, #A64942 0 50%, transparent 0 300px, #293462 0),
+      linear-gradient(#FE5F55 150px, #A64942 0 200px, transparent 0),
+      #293462
+    ;
+    background-size: 50% 50vw, 100%, 100%;
+  }
+</style>
+`;
 </script>
 
 <PuzzlePageLayout>
@@ -30,4 +43,6 @@
 	<Narigo>
 		Whenever doing a <code>transform</code>, remember that the order matters...!
 	</Narigo>
+	<Puzzle title="Puzzle 38 solution" solution={solutionNoElement} />
+	<Narigo>Ok, one more solution with no elements set up.</Narigo>
 </PuzzlePageLayout>
