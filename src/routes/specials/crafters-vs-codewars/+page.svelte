b487f0ced5da9bfd4ddf3fcef04505627864a3c2
Joern Bernhardt <joern.bernhardt@compose.us>
2023-03-13 04:47:47 +0100

    feat: Add a page for crafters vs codewars
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/specials/crafters-vs-codewars/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { base } from '$app/paths';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import PageLayout from '$lib/common/PageLayout/PageLayout.svelte';
+</script>
+
+<PageLayout>
+	<h2>Crafters vs CodeWars</h2>
+	<Narigo
+		>At JSCraftCamp 2022, a lot of participants complained about the lack of events until the next
+		JSCraftCamp. We decided to push the online community around JSCraftCamp and start by organizing
+		more meetups.</Narigo
+	>
+	<ul>
+		<!-- <li><a href="{base}/specials/crafters-vs-codewars/event-2023-03-09">Disemvowel Trolls</a></li> -->
+	</ul>
+</PageLayout>
