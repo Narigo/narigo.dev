26b42a6706cae5890f61a4c4e713b31de72956ee
Joern Bernhardt <joern.bernhardt@compose.us>
2023-03-19 21:43:58 +0100

    feat: Add more info to index page
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/specials/frontend-friday-folks/+page.svelte
+++ src/routes/specials/frontend-friday-folks/+page.svelte
@@ -6,17 +6,28 @@
 
 <PageLayout>
 	<h2>Frontend Friday Folks</h2>
-	<Narigo
-		>In VirtualCoffee.io, we started a group that meets every Friday to work on a CSSBattle.dev
-		puzzle. I think there hasn't been a single time where I haven't learnt a new CSS trick. I'm
-		going to share some puzzle solutions and write about it.</Narigo
-	>
-	<ul>
-		<li><a href="{base}/specials/frontend-friday-folks/puzzle-33">Puzzle 33 - Birdie</a></li>
-		<li><a href="{base}/specials/frontend-friday-folks/puzzle-35">Puzzle 35 - Ice Cream</a></li>
-		<li><a href="{base}/specials/frontend-friday-folks/puzzle-36">Puzzle 36 - Interleaved</a></li>
-		<li><a href="{base}/specials/frontend-friday-folks/puzzle-37">Puzzle 37 - Tunnel</a></li>
-		<li><a href="{base}/specials/frontend-friday-folks/puzzle-38">Puzzle 38 - Not Simply Square</a></li>
-		<li><a href="{base}/specials/frontend-friday-folks/puzzle-39">Puzzle 39 - Sunset</a></li>
-	</ul>
+	<Narigo>
+		<p>
+			In VirtualCoffee.io, we started a group that meets every Friday to work on a CSSBattle.dev
+			puzzle. I think there hasn't been a single time where I haven't learnt a new CSS trick.
+		</p>
+		<p>
+			There was enough interest to actually start a second session per week now. I'm sharing
+			solutions I've done here, but I'm open to share others as well, if someone wants to submit
+			anything.
+		</p>
+	</Narigo>
+	<Narigo>
+		<p>Here are solutions to puzzles we completed:</p>
+		<ul>
+			<li><a href="{base}/specials/frontend-friday-folks/puzzle-33">Puzzle 33 - Birdie</a></li>
+			<li><a href="{base}/specials/frontend-friday-folks/puzzle-35">Puzzle 35 - Ice Cream</a></li>
+			<li><a href="{base}/specials/frontend-friday-folks/puzzle-36">Puzzle 36 - Interleaved</a></li>
+			<li><a href="{base}/specials/frontend-friday-folks/puzzle-37">Puzzle 37 - Tunnel</a></li>
+			<li>
+				<a href="{base}/specials/frontend-friday-folks/puzzle-38">Puzzle 38 - Not Simply Square</a>
+			</li>
+			<li><a href="{base}/specials/frontend-friday-folks/puzzle-39">Puzzle 39 - Sunset</a></li>
+		</ul>
+	</Narigo>
 </PageLayout>
