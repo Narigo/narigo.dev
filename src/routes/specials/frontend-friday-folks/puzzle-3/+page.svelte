3e8ae732a390cfe6abfbc3762d35be7e23ac65bb
Joern Bernhardt <joern.bernhardt@compose.us>
2023-03-25 19:27:11 +0100

    feat: Add puzzle 3 after redoing it during the Saturday edition
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-3/+page.svelte
@@ -0,0 +1,70 @@
+<script>
+	import Bubble from '$lib/common/bubble/Bubble.svelte';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solutionFirst = `<div class="bg-rect">
+<div class="blue-circle"></div>
+<div class="button"></div>
+</div>
+<style>
+  body {
+    margin: 0;
+    background: #6592CF;
+  }
+  .bg-rect {
+    position: absolute;
+    top: 75px;
+    bottom: 75px;
+    left: 50px;
+    right: 50px;
+    background: #243D83;
+    overflow: hidden;
+  }
+  .blue-circle {
+    border: 50px solid #6592CF;
+    border-radius: 50%;
+    position: absolute;
+    left: 25px;
+    right: 25px;
+    top: -50px;
+    bottom: -50px;
+  }
+  .button {
+    position: absolute;
+    border-radius: 50%;
+    background: #EEB850;
+    top: 50%;
+    left: 50%;
+    width: 50px;
+    height: 50px;
+    translate: -50% -50%;
+  }
+</style>`;
+
+	const solutionGradient = `<div></div>
+<style>
+  body {
+    background: #6592CF;
+    display: grid;
+    place-items: center;
+  }
+  div {
+    background: radial-gradient(#EEB850 25px, #243D83 0 75px, #6592CF 0 125px, #243D83 0) 0 50% / 300px 300px;
+    height: 150px;
+    width: 300px;
+  }
+</style>`;
+</script>
+
+<PuzzlePageLayout title="Push Button">
+	<Puzzle title="Puzzle 3 solution" solution={solutionFirst} />
+	<Narigo>My first solution when we started out.</Narigo>
+	<Puzzle title="Puzzle 3 solution" solution={solutionGradient} />
+	<Narigo
+		>Gradients! This has been done when the Saturday edition of the Frontend Friday Folks started
+		over from the beginning. I joined this time and tried solving it again in a different way. Using
+		a radial-gradient and moving the background position seemed to make sense to me.</Narigo
+	>
+</PuzzlePageLayout>
