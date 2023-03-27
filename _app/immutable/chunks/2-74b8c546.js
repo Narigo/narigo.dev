2d68e60482fc672415a710b5cbc1f17bbbbaaaad
Narigo <Narigo@users.noreply.github.com>
2023-03-27 22:01:42 +0000

    Deploying to gh-pages from @ Narigo/narigo.dev@464e980dea2a0af69d0fdd1808fc798c673976c0 🚀

---
+++ _app/immutable/chunks/2-74b8c546.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-0b5a694b.js";export{t as component};
--- _app/immutable/chunks/2-cc5928a6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-45261a02.js";export{t as component};
