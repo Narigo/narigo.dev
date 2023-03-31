dde8d8f152d0a3bf4bd9f79c71c94de466074aca
Joern Bernhardt <joern.bernhardt@compose.us>
2023-03-31 23:28:58 +0200

    feat: Add puzzle 40 from last week
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-40/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div></div>
+<style>
+  body {
+    background: #6592CF;
+    display: grid;
+    place-items: center;
+  }
+  div {
+    width: 200px;
+    height: 200px;
+  }
+  div::after,div::before {
+    position: absolute;
+    content: '';
+    height: 100px;
+    width: 100px;
+  }
+  div::before {
+    border: 50px solid #243D83;
+    border-radius: 0 100px 100px 100px;
+  }
+  div::after {
+    background: linear-gradient(to right, #243D83 50px, #6592CF 0 100px, #243D83 50px);
+  }
+</style>
+`;
+</script>
+
+<PuzzlePageLayout>
+	<Puzzle title="Puzzle 40 solution" {solution} />
+	<Narigo>Something with linear-gradients again!</Narigo>
+</PuzzlePageLayout>
