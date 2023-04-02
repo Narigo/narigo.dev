b6232e46839e87a5be3513b3ce9207b50238ec52
Joern Bernhardt <joern.bernhardt@compose.us>
2023-04-02 18:31:51 +0200

    feat: Add comment to the solutions
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/specials/crafters-vs-codewars/event-2023-03-09/+page.svelte
+++ src/routes/specials/crafters-vs-codewars/event-2023-03-09/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
 	import CodeBlock from '$lib/common/CodeBlock.svelte';
 	import KataLayout from '../KataLayout.svelte';
 
@@ -27,9 +28,21 @@ function disemvowel(str) {
 </script>
 
 <KataLayout id="52fba66badcd10859f00097e" name="Disemvowel Trolls">
+	<h3>Array.reduce</h3>
 	<p>A somewhat readable reducer</p>
 	<CodeBlock code={reducerSolution} />
+	<Narigo>
+		I think reducers are a really cool tool to do several things in one run through an array. It's
+		very easy for them to get complex and too clever to understand them quickly. I try moving things
+		out of them and keep descriptive names for variables and functions that I use.
+	</Narigo>
 
+	<h3>for-of</h3>
 	<p>Did you know that <code>for ... of string</code> does not need to split the string?</p>
 	<CodeBlock code={forOfSolution} />
+	<Narigo>
+		I didn't know that for ... of splits a string directly! It's fun to hear other people's
+		solutions and then come up with something else that nobody used to solve this. It seems to be a
+		good way of learning new stuff.
+	</Narigo>
 </KataLayout>
