c68c371ace514eccb8fac802c38aac10a8130509
Joern Bernhardt <joern.bernhardt@compose.us>
2023-04-03 23:20:02 +0200

    feat: Add sections around solutions
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/specials/crafters-vs-codewars/KataLayout.svelte
+++ src/routes/specials/crafters-vs-codewars/KataLayout.svelte
@@ -6,7 +6,23 @@
 </script>
 
 <PageLayout>
-	<h2>{name}</h2>
-	<a href="https://www.codewars.com/kata/{id}">Link</a>
-	<slot />
+	<div>
+		<section>
+			<h2>{name}</h2>
+			<p>
+				If you want to learn about the puzzle first, check out the <a
+					href="https://www.codewars.com/kata/{id}">Kata on CodeWars</a
+				>.
+			</p>
+		</section>
+		<slot />
+	</div>
 </PageLayout>
+
+<style>
+	div {
+		display: flex;
+		flex-flow: column;
+		gap: 2em;
+	}
+</style>
