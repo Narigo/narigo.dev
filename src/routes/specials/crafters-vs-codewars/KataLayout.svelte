d2fbaa61f154af2cd66e3a4f04e87ce7efb96a2f
Joern Bernhardt <joern.bernhardt@compose.us>
2023-04-04 03:21:37 +0200

    feat: Show button for showing solutions
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/specials/crafters-vs-codewars/KataLayout.svelte
+++ src/routes/specials/crafters-vs-codewars/KataLayout.svelte
@@ -1,21 +1,28 @@
 <script lang="ts">
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
 	import PageLayout from '$lib/common/PageLayout/PageLayout.svelte';
 
 	export let id: string;
 	export let name: string;
+
+	let showSolutions = false;
 </script>
 
 <PageLayout>
 	<div>
 		<section>
 			<h2>{name}</h2>
-			<p>
-				If you want to learn about the puzzle first, check out the <a
+			<Narigo>
+				If you want to learn more about the puzzle first, check out the <a
 					href="https://www.codewars.com/kata/{id}">Kata on CodeWars</a
 				>.
-			</p>
+			</Narigo>
 		</section>
-		<slot />
+		{#if showSolutions}
+			<slot />
+		{:else}
+			<button on:click={() => (showSolutions = true)}>Show solutions</button>
+		{/if}
 	</div>
 </PageLayout>
 
