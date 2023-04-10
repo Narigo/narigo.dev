04888230ce6fdc5837fab1194f48bbd41df9acc7
Joern Bernhardt <joern.bernhardt@compose.us>
2023-04-11 00:58:01 +0200

    feat: Add more logs
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/articles/pocuter-one/+page.svelte
+++ src/routes/articles/pocuter-one/+page.svelte
@@ -25,12 +25,12 @@
 	</p>
 
 	<h3>WatchKit assembly</h3>
-	<p>There are a few things that need to be done:</p>
+	<p>There are a few things that need to be done before playing around with apps:</p>
 	<ol>
 		<li>Solder a battery to the chip</li>
 		<li>Assemble the watch itself</li>
-		<li>Add apps on the watch</li>
 	</ol>
+
 	<h4>Soldering the battery</h4>
 	<p>
 		The very first one was the most intimidating for me as someone who doesn't have much experience
@@ -51,6 +51,100 @@
 		cable goes to the <code>+</code> pole and the black cable to the <code>-</code> pole. I can definitly
 		recommend joining their Discord as they seem to be very active and helpful regarding support.
 	</p>
+	<p>
+		To be able to solder a cable, I had to cut the outer parts of the wire, so the metallic inside
+		is visible. There are specialized tools for doing that and trying to cut it off with something
+		sharp resulted in losing quite a bit of the wire itself. I then found a small tool I could use
+		that I got from some random hackathon or conference that I attended. It worked out, but it was
+		quite painful to get it done. Anyways, once you see the metallic inside of the red and the black
+		wires from the battery, you can solder it. I opened it a couple of millimeters, so I was sure I
+		can hit it with my relatively big soldering iron.
+	</p>
+	<p>
+		I put the cables from "below" into the holes. "Below" means when folding the screen on top of
+		the chip as it needs to be put in the watch case, the battery is below the chip. The red and
+		black cables from the battery then don't need to be wrapped around the chip in the small watch
+		case.
+	</p>
+	<p>
+		I then started soldering with the black one on the <code>-</code> pole. I thought it's probably
+		easier to put the soldering down on the less exposed port first, so fixing something wouldn't
+		automatically mean I have to remove the potentially working and more exposed
+		<code>+</code> port as well.
+	</p>
+	<p>
+		While soldering, the watch started to boot up. For me, this was the sign that the soldering
+		worked out well and I made sure both cables were properly fixed. I made sure the soldered metal
+		wasn't too high, so the screen wouldn't be hurt by that. I had to take off some of the soldering
+		again by using the hot soldering iron and gently draw something away from it again. I guess it
+		was pure luck that I didn't destroy anything while doing that - the pocuter continued to work!
+	</p>
+	<p>
+		If the watch turned off at some point during the assembly, it wasn't because of my lack of
+		experience with the soldering: The included battery runs out pretty quickly. I think when using
+		the "Clock" application, it's running for about 1.5 hours after having the USB connected for an
+		extended period of time.
+	</p>
+
+	<h4>Assembly of the watch kit</h4>
+	<p>
+		Ok, so this looked pretty straightforward. The screen only has one place where it can go. I
+		could see from the outside where the USB port and the buttons / knobs should be. The buttons
+		were a bit too hard to put in and it felt a bit wrong to push them so hard into it. I made it,
+		but once I put in all the screws, I couldn't really push the buttons at all or they were "always
+		pushed". This felt really wrong.
+	</p>
+	<p>
+		After checking the Discord again, I saw that people used a file to make the inside of the button
+		a bit wider, so I tried that out myself: Unscrew the watch case, take out the knobs again.
+		Equipped with a small file for fingernails, I could scrub a little bit away from the sides from
+		the inside of the "U"-shaped buttons. When I put the buttons into the watch again, they were
+		fitting way better than before and I didn't even have to push them into the case - they actually
+		fit really nice after using the file. The problems with "always pushed" buttons or having a hard
+		time to press anything at all, vanished completely.
+	</p>
+	<p>
+		Setting up the wrist band worked well for me: I had to put the metal bars into one hole, then
+		press and hold the tiny metal piece so the whole bar fits into the side of the watch and once it
+		inside the sides, find the other hole by wiggling it around a bit. It took me a few tries but it
+		worked out eventually and didn't take too long.
+	</p>
+
+	<h3>Applications</h3>
+	<p>
+		There are a couple of apps you can download and add to the pocuter. This is actually quite easy
+		to do, but the documentation was a bit lacking. The SD card from the pocuter should be plugged
+		into a regular computer. The apps that can currently be downloaded are <code>.zip</code> files.
+		In these, there is a folder named with a number and in that folder, there is a file called
+		<code>esp32c3.app</code>. On the SD card, these need to be put into an <code>apps</code> folder,
+		keeping the numbered folder.
+	</p>
+	<p>To make it clear, the structure of the SD card should look like this:</p>
+	<pre>
+apps/
+     1/
+          esp32c3.app
+     2/
+          esp32c3.app
+     3/
+          esp32c3.app
+     4/
+          esp32c3.app
+     ...
+config/      (optionally)
+     1/
+          settings.ini
+          TIME.ini
+          WIFI.ini
+     2/      (these folders can be empty)
+     3/
+     ...
+	</pre>
+	<p>
+		Once the apps are put onto the SD card in the proper structure, putting it back into the pocuter
+		and restarting it yielded all the apps in there.
+	</p>
+	<p>Writing my own app took a bit longer...</p>
 </PageLayout>
 
 <style>
