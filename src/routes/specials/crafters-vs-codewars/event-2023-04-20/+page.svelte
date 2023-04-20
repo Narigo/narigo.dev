6d2017159754ae36590a2a8d16b07fc5c0bee221
Joern Bernhardt <joern.bernhardt@compose.us>
2023-04-20 23:55:34 +0200

    feat: Add link to the puzzle of todays crafters vs codewars
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/specials/crafters-vs-codewars/event-2023-04-20/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import CodeBlock from '$lib/common/CodeBlock.svelte';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import KataLayout from '../KataLayout.svelte';
+
+	const solution = `?`;
+</script>
+
+<KataLayout id="541c8630095125aba6000c00" name="Sum of Digits / Digital Root">
+	<section>
+		<h3>?</h3>
+		<CodeBlock code={solution} />
+		<Narigo
+			>I couldn't make it today, but Gustaf hosted it this time. I'm going to add my solution very
+			soon!</Narigo
+		>
+	</section>
+</KataLayout>
