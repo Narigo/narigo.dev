1b8b6a644f65be5886cb65b224ab2c1bdbe318c8
Joern Bernhardt <joern.bernhardt@compose.us>
2023-04-21 01:47:01 +0200

    feat: Added a naive implementation of the problem with no real optimizations
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/specials/crafters-vs-codewars/event-2023-04-20/+page.svelte
+++ src/routes/specials/crafters-vs-codewars/event-2023-04-20/+page.svelte
@@ -3,16 +3,27 @@
 	import Narigo from '$lib/common/bubble/Narigo.svelte';
 	import KataLayout from '../KataLayout.svelte';
 
-	const solution = `?`;
+	const solution = `function digitalRoot(n) {
+  const nString = \`\${n}\`;
+  if (nString.length === 1) {
+    return n;
+  }
+
+  let sum = 0;
+  for (let char of nString) {
+    sum = sum + Number(char);
+  }
+  return digitalRoot(sum);
+}`;
 </script>
 
 <KataLayout id="541c8630095125aba6000c00" name="Sum of Digits / Digital Root">
 	<section>
-		<h3>?</h3>
+		<h3>Sum of Digits / Digital Root</h3>
 		<CodeBlock code={solution} />
 		<Narigo
-			>I couldn't make it today, but Gustaf hosted it this time. I'm going to add my solution very
-			soon!</Narigo
+			>I couldn't make it today, but Gustaf hosted it this time. I've added this solution for
+			writing a very naive implementation for this issue.</Narigo
 		>
 	</section>
 </KataLayout>
