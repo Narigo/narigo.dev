f8891e1b56a667f903cfedc4f71e79015605512e
Joern Bernhardt <joern.bernhardt@compose.us>
2023-04-22 16:44:14 +0200

    feat: Add puzzle 43 and 44 solutions
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-43/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div class="wrap">
+  <div class="l"></div>
+  <div class="r"></div>
+</div>
+<style>
+  body {
+    background: #6592CF;
+    margin: 0;
+  }
+  .wrap {
+    inset: 30px 145px 28px;
+    overflow: hidden;
+    position: absolute;
+  }
+  .l,.r {
+    border: 30px solid #243D83;
+    border-radius: 70px;
+    right: 40px;
+    position: absolute;
+    height: 182px;
+    width: 90%;
+  }
+  .r {
+    left: 40px;
+  }
+</style>`;
+</script>
+
+<PuzzlePageLayout>
+	<Puzzle title="Puzzle 43 solution" {solution} />
+	<Narigo
+		>That was tricky! The numbers don't really line up well and it feels like there is something off
+		now. Usually, the numbers end with a <code>0</code> or at least <code>5</code> when using
+		<code>px</code> values. I didn't want to deep dive into using other units to see whether ther is
+		something else suited better for this.
+	</Narigo>
+</PuzzlePageLayout>
---
+++ src/routes/specials/frontend-friday-folks/puzzle-44/+page.svelte
@@ -0,0 +1,102 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<style>
+  body {
+    background: 
+      radial-gradient(circle, #1A4341 50vh, transparent 0) -50vw,
+      linear-gradient(#1A4341 0 60px, transparent 0 240px, #1A4341 0),
+      repeating-linear-gradient(#1A4341 0 20px, #F3AC3C 0 40px)
+    ;
+  }
+</style>`;
+
+	const solution99 = `<div class="b">
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+</div>
+<div class="l"></div>
+<div class="r"></div>
+<style>
+  body {
+    margin: 0;
+    background: #1A4341;
+    display: grid;
+    place-items: center;
+  }
+  .b {
+    width: 100%;
+    height: 180px;
+  }
+  .b div {
+    background: #F3AC3C;
+    height: 20px;
+  }
+  .b div:nth-child(even) {
+    background: #1A4341;
+    height: 20px;
+  }
+  .l,.r {
+    position: absolute;
+    background: #1A4341;
+    border-radius: 0 150px 150px 0;
+    left: 0;
+    height: 300px;
+    width: 150px;
+  }
+  .r {
+    border-radius: 150px 0 0 150px;
+    left: unset;
+    right: 0;
+  }
+</style>`;
+
+	const borderSolution = `<style>
+  body {
+    border: 52px solid #1A4341;
+    background: 
+      radial-gradient(circle,#1A4341 50vh,transparent 0) -50vw,
+      repeating-linear-gradient(#1A4341 0 20px,#F3AC3C 0 40px)
+    ;
+  }
+</style>`;
+</script>
+
+<PuzzlePageLayout>
+	<Puzzle title="Puzzle 44 solution 99.9%" solution={solution99} />
+	<Narigo
+		><p>
+			This was a hard one! We initially created this solution with less divs, but this was always
+			ending up in a 99.9% solution - even when the <code>Diff</code> view showed up almost completely
+			black. Only when we zooming in the black diff-preview, we could see tiny discrepancies, but they
+			weren't even solvable by incrementing by half-pixel values or similar.
+		</p>
+		<p>
+			Very frustrating experience - it looks like sometimes you need to find a special way how to
+			solve it. We still don't get, why this solution would be different to the one hundred percent
+			solution below.
+		</p></Narigo
+	>
+	<Puzzle title="Puzzle 44 solution 100%" {solution} />
+	<Narigo
+		>With this solution, achieving the 100% were no problem. I think I would have gone for this in
+		the first place, but solving everything with gradients almost seems to be cheating to me now.</Narigo
+	>
+	<Puzzle title="Puzzle 44 improved" solution={borderSolution} />
+	<Narigo
+		>There was another solution, which improved the character count a bit, by using a border on the
+		body instead of the transparent linear-gradient. Not much has changed otherwise, but it's good
+		to see that sometimes you can solve things differently. Still 100% with this approach. When
+		zooming in, I can't really see a difference to the 99% solution. I believe the circles to the
+		side somehow are calculated differently in the 99% solution then.</Narigo
+	>
+</PuzzlePageLayout>
