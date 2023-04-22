83bc721b11fc555cebee93013757e3ef27b4e542
Joern Bernhardt <joern.bernhardt@compose.us>
2023-04-22 16:52:18 +0200

    feat: Add puzzle 34 solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-34/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div></div>
+<style>
+  body {
+    background: #007065;
+    --c: #00A79D;
+  }
+  div {
+    inset: 25px 75px;
+  }
+  div::after {
+    inset: -225px -125px;
+    --c: #FFEECF;
+  }
+  div::before {
+    inset: -175px -125px;
+    --c: #F5C181;
+  }
+  div,div:after,div:before {
+    content: '';
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-top: 0;
+    border: 125px solid transparent;
+    border-bottom: 100px solid var(--c);
+  }
+</style>
+`;
+</script>
+
+<PuzzlePageLayout title="Birdie">
+	<Puzzle title="Puzzle 34 solution" {solution} />
+	<Narigo
+		>Using pseudo elements here to check the stacking seemed to be a good way to solve this.</Narigo
+	>
+</PuzzlePageLayout>
