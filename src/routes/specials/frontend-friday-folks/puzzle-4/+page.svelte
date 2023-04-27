35ee3da3a0fe04ed3efd36b04f164b6c42b2e534
Joern Bernhardt <joern.bernhardt@compose.us>
2023-04-27 22:53:57 +0200

    feat: Add puzzle 4
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-4/+page.svelte
@@ -0,0 +1,86 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div class="c r"></div>
+<div class="d m"></div>
+<div class="d l"></div>
+<style>
+  body {margin:0;background:#0B2429;}
+  div {
+    position: absolute;
+    width: 120px;
+    height: 120px;
+    background: #F3AC3C;
+    border-radius: 50%;
+  }
+  .m {
+    background: #998235;
+    top: 50%;
+    left: 50%;
+    translate: -50% -50%;
+  }
+  .c {
+    bottom: 50%;
+    left: 50%;
+  }
+  .d {
+    border-top-right-radius: 0;
+  }
+  .l {
+    top: 50%;
+    right: 50%;
+  }
+</style>
+`;
+
+	const solution2 = `<div></div>
+<div id=b></div>
+<style>
+  body {background: #0B2429;display: grid;place-items:center;}
+  #b {
+    position: absolute;
+    transform: scale(-1);
+    z-index: -1;
+  }
+  div {
+    background: #F3AC3C;
+    border-radius: 50% 0 50% 50%;
+    position: relative;
+    height: 120px;
+    width: 120px;
+  }
+  div:before,div:after {
+    border-radius: 50% 0;
+    content: '';
+    inset: 0;
+    position: absolute;
+  }
+  div:before {
+    background: #998235;
+  }
+  div:after {
+    background: #F3AC3C;
+    inset: 60px 60px -60px -60px;
+    border-radius: 50% 0 50% 50%;
+  }
+</style>
+`;
+</script>
+
+<PuzzlePageLayout title="Push Button">
+	<Puzzle title="Puzzle 4 solution" {solution} />
+	<Narigo
+		>In my first solution I used regular positioning and thought the left-bottom and middle one were
+		of similar shape.</Narigo
+	>
+	<Puzzle title="Second solution for puzzle 4" solution={solution2} />
+	<Narigo
+		>The second time I solved this, I was looking into ways how to mirror this. I wanted to use <code
+			>-webkit-box-reflect</code
+		>
+		but it looks like it can only mirror on the X or the Y axis - not both at the same time. So I resorted
+		to the <code>scale(-1)</code> trick. For this to work, the boxes needed to be duplicated.</Narigo
+	>
+</PuzzlePageLayout>
