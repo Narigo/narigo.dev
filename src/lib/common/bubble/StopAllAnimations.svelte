c74fd3b9541e7a060e4b0d1c696981519ec897a2
Joern Bernhardt <joern.bernhardt@compose.us>
2023-05-03 19:52:08 +0200

    feat: Style fast forward button
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/common/bubble/StopAllAnimations.svelte
+++ src/lib/common/bubble/StopAllAnimations.svelte
@@ -6,4 +6,23 @@
 	const { animationsDone, finishAllAnimations } = context;
 </script>
 
-<button on:click={() => finishAllAnimations()} disabled={$animationsDone}>Fast forward animations</button>
+<button on:click={() => finishAllAnimations()} disabled={$animationsDone}
+	>Fast forward animations &gt;&gt;</button
+>
+
+<style>
+	button {
+		background: none;
+		border: none;
+		cursor: pointer;
+		font: inherit;
+		padding: 0.5em 1em;
+	}
+	button:hover {
+		background: #0002;
+	}
+	button:disabled:hover {
+		background: none;
+		cursor: not-allowed;
+	}
+</style>
