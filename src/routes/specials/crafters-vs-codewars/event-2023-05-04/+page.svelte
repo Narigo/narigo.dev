89976b1bf95addda328ad98cb029c1b5b43dd575
Joern Bernhardt <joern.bernhardt@compose.us>
2023-05-04 21:06:12 +0200

    feat: Add page for latest event
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/specials/crafters-vs-codewars/event-2023-05-04/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import CodeBlock from '$lib/common/CodeBlock.svelte';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import KataLayout from '../KataLayout.svelte';
+
+	const solutionReduce = ``;
+</script>
+
+<KataLayout id="52597aa56021e91c93000cb0" name="Moving Zeros To The End">
+	<section>
+		<h3>Moving Zeros To The End</h3>
+		<CodeBlock code={solutionReduce} />
+		<Narigo>
+			There were a couple of solutions to this problem, lots of different ideas even though there were less people than usually tonight. It feels like for every challenge about an array, there has to be a solution with <code>Array.reduce</code>. So there is mine! 😅
+		</Narigo>
+	</section>
+</KataLayout>
