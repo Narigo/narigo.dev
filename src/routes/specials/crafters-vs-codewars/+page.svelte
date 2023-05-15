089dc2cab5e965473c182c8756125e8fc35ec1ec
Joern Bernhardt <joern.bernhardt@compose.us>
2023-05-15 22:09:25 +0200

    feat: Add link to last crafters vs codewars
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/specials/crafters-vs-codewars/+page.svelte
+++ src/routes/specials/crafters-vs-codewars/+page.svelte
@@ -15,7 +15,7 @@
 		<li><a href="{base}/specials/crafters-vs-codewars/event-2023-03-09">Disemvowel Trolls</a></li>
 		<li><a href="{base}/specials/crafters-vs-codewars/event-2023-04-11">Mumbling</a></li>
 		<li><a href="{base}/specials/crafters-vs-codewars/event-2023-04-20">Sum of Digits / Digital Root</a></li>
-
 		<li><a href="{base}/specials/crafters-vs-codewars/event-2023-05-04">Moving Zeros To The End</a></li>
+		<li><a href="{base}/specials/crafters-vs-codewars/event-2023-05-11">Find the unique number</a></li>
 	</ul>
 </PageLayout>
