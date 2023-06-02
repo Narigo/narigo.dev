93daab4732fb58e6512927684a9c5ace74ce97dc
Joern Bernhardt <joern.bernhardt@compose.us>
2023-06-02 21:43:34 +0200

    feat: Add puzzle of today
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-53/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Pastel Logo">
+	<Puzzle title="Puzzle 53 solution" {solution} />
+	<Narigo>
+		This is the first time I used <code>isolation: isolate</code>! From my understanding, it creates
+		a new z-index stack and it allows me to put regular elements on top of each other, without
+		having to <code>position: absolute</code> them. See
+		<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/isolation" rel="external"
+			>MDN for more information about isolation</a
+		>.
+	</Narigo>
+</PuzzlePageLayout>
