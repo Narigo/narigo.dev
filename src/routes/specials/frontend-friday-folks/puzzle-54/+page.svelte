b478dc1d96eb7071fdc8a7e78628fea7d1389ccf
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-06-09 18:15:09 +0200

    feat: Add puzzle solution for 54
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-54/+page.svelte
@@ -0,0 +1,79 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div class="w">
+  <div class="fs">
+    <div class="h f f1"></div>
+    <div class="h f f2"></div>
+    <div class="h f f3"></div>
+    <div class="h f f4"></div>
+  </div>
+  <div class="h x palm"></div>
+  <div class="h x arm"></div>
+  <div class="h f thumb"></div>
+</div>
+<style>
+  body {
+    background: #F9E492;
+    display: grid;
+    place-items: center;
+    margin: 0;
+  }
+  .w {
+    display: grid;
+    position: relative;
+    grid-template-columns: repeat(4, 1fr);
+    margin-top: -10px;
+  }
+  .fs {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-column: 1 / 5;
+    height: 70px;
+    place-items: end;
+  }
+  .h {
+    background: #191919;
+  }
+  .f {
+    box-sizing: border-box;
+    border-radius: 20px;
+    border: 5px solid #F9E492;
+    margin-right: -5px;
+    height: 45px;
+    width: 30px;
+  }
+  .f1 {
+    height: 55px;
+  }
+  .f2 {
+    height: 65px;
+  }
+  .f3 {
+    height: 55px;
+  }
+  .x {
+    border-radius: 0 0 10px 10px;
+  }
+  .palm {
+    grid-column: 1 / 5;
+    height: 40px;
+  }
+  .arm {
+    grid-column: 2 / 4;
+    height: 45px;
+  }
+  .thumb {
+    position: absolute;
+    height: 75px;
+    transform: translate(2px, 36.5px) rotate(60deg);
+  }
+</style>`;
+</script>
+
+<PuzzlePageLayout title="Black Lives Matter">
+	<Puzzle title="Puzzle 54 solution" {solution} />
+	<Narigo>Ok, interesting use of grid here. In the end, we got 100%, so "good enough".</Narigo>
+</PuzzlePageLayout>
