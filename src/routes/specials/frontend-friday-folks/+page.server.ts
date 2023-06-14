71fa90659ed9ffb4ba5f399d822eaf491a938b25
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-06-15 00:43:51 +0200

    chore: Move into load function
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/specials/frontend-friday-folks/+page.server.ts
+++ src/routes/specials/frontend-friday-folks/+page.server.ts
@@ -1,10 +1,9 @@
 import type { PageServerLoad } from './$types';
 
-const puzzleFiles = Object.entries(
-	import.meta.glob(['./puzzle-*/+page.svelte'], { as: 'raw', eager: true })
-);
-
 export const load: PageServerLoad = async () => {
+	const puzzleFiles = Object.entries(
+		import.meta.glob(['./puzzle-*/+page.svelte'], { as: 'raw', eager: true })
+	);
 	let puzzles = [];
 	for (let [path, content] of puzzleFiles) {
 		const number = parseInt(path.slice(path.indexOf('-') + 1, path.lastIndexOf('/')), 10);
