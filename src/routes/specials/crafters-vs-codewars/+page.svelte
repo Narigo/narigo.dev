94b4c110ec9ec9d0291a5b051c4da1d16b170310
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-06-15 18:39:26 +0200

    fix: Add other names of Kata to index
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/specials/crafters-vs-codewars/+page.svelte
+++ src/routes/specials/crafters-vs-codewars/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import { base } from '$app/paths';
 	import Narigo from '$lib/common/bubble/Narigo.svelte';
 	import PageLayout from '$lib/common/PageLayout/PageLayout.svelte';
@@ -14,9 +14,21 @@
 	<ul>
 		<li><a href="{base}/specials/crafters-vs-codewars/event-2023-03-09">Disemvowel Trolls</a></li>
 		<li><a href="{base}/specials/crafters-vs-codewars/event-2023-04-11">Mumbling</a></li>
-		<li><a href="{base}/specials/crafters-vs-codewars/event-2023-04-20">Sum of Digits / Digital Root</a></li>
-		<li><a href="{base}/specials/crafters-vs-codewars/event-2023-05-04">Moving Zeros To The End</a></li>
-		<li><a href="{base}/specials/crafters-vs-codewars/event-2023-05-11">Find the unique number</a></li>
-		<li><a href="{base}/specials/crafters-vs-codewars/event-2023-05-18">Stop gninnipS My sdroW!, </a></li>
+		<li>
+			<a href="{base}/specials/crafters-vs-codewars/event-2023-04-20"
+				>Sum of Digits / Digital Root</a
+			>
+		</li>
+		<li>
+			<a href="{base}/specials/crafters-vs-codewars/event-2023-05-04">Moving Zeros To The End</a>
+		</li>
+		<li>
+			<a href="{base}/specials/crafters-vs-codewars/event-2023-05-11">Find the unique number</a>
+		</li>
+		<li>
+			<a href="{base}/specials/crafters-vs-codewars/event-2023-05-18"
+				>Stop gninnipS My sdroW!, Training on Multiples of 3 or 5, Who likes it?</a
+			>
+		</li>
 	</ul>
 </PageLayout>
