911b0f6248549206cffa448af1ea8024da2c464d
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-07-06 23:44:25 +0200

    feat: Add first kata solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/specials/crafters-vs-codewars/event-2023-07-06/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Kata from '../Kata.svelte';
+	import KataLayout from '../KataLayout.svelte';
+
+	const solution = `function rotate(str, acc = [], at = 0) {
+  if (str.length <= at) {
+    return acc;
+  }
+  const nextSlice = str.slice(at + 1) + str.slice(0, at + 1);
+  return rotate(str, [...acc, nextSlice], at + 1);
+}`;
+</script>
+
+<KataLayout>
+	<Kata id="586560a639c5ab3a260000f3" name="All Star Code Challenge #15" {solution}>
+		<Narigo>
+			<p>
+				I had to leave shortly after finishing mine. They did another challenge and had a couple
+				more solutions. My version uses default arguments and recursion to get to the result without
+				having to write a second helper function.
+			</p>
+			<p>
+				The initial idea brought up by Gustaf in the screenshare was to add the same string to the
+				string again. Then, you can take a slice of the string's length starting from index 1 up to
+				the length of the string. While it takes a bit more memory, I'm still wondering which
+				approach would be faster for the computer to work with.
+			</p>
+		</Narigo>
+	</Kata>
+</KataLayout>
