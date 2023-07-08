e9e4a31fd5699b18c3814cfc791194b2f7ac6dc8
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-07-08 22:04:40 +0200

    feat: Add puzzle 58 and learning
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-58/+page.svelte
@@ -0,0 +1,76 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div class="beige f"></div>
+<div class="beige e"></div>
+<div class="blue d"></div>
+<div class="blue c"></div>
+<div class="blue b"></div>
+<div class="blue a"></div>
+<style>
+  body {
+    background: #191919;
+  }
+  .blue {
+    background: #4F77FF;
+  }
+  .beige {
+    background: #F9E492;
+    border: 0;
+  }
+  div {
+    position: fixed;
+    border: 10px solid #191919;
+    border-radius: 50%;
+    height: 30px;
+    width: 30px;
+  }
+  .a {
+    top: 35px;
+    left: 175px
+  }
+  .b {
+    border-radius: 15px 15px 35px 35px;
+    top: 55px;
+    left: 140px;
+    width: 100px;
+  }
+  .c {
+    border-radius: 15px 15px 35px 35px;
+    top: 75px;
+    left: 120px;
+    width: 140px;
+  }
+  .d {
+    border: 0;
+    top: 65px;
+    left: 150px;
+    height: 100px;
+    width: 100px;
+  }
+  .e {
+    top: 145px;
+    left: 180px;
+    height: 40px;
+    width: 40px;
+  }
+  .f {
+    border-radius: 10px;
+    top: 155px;
+    left: 190px;
+    height: 100px;
+    width: 20px;
+  }
+</style>
+`;
+</script>
+
+<PuzzlePageLayout title="Rose">
+	<Puzzle title="Puzzle 58 solution" {solution} />
+	<Narigo>
+		I learned something! The border itself is somehow counting towards the border-radius. So if you
+		need to have some radius with a thicker border, add the border thickness itself at least.
+	</Narigo>
+</PuzzlePageLayout>
