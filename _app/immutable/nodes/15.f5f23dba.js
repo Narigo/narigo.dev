28185a76bdd70e9b9496007f613e0bcbc9cdbe71
Narigo <Narigo@users.noreply.github.com>
2023-07-09 01:16:03 +0000

    Deploying to gh-pages from @ Narigo/narigo.dev@0f9558bc5cb718fe35a1efad7f72248d2cd662ca 🚀

---
+++ _app/immutable/nodes/15.f5f23dba.js
@@ -0,0 +1,37 @@
+import{S as O,i as C,s as T,y as h,z as c,A as m,g as d,d as $,B as p,a as P,c as I,b as f,h as l,k as w,q as g,l as y,m as b,r as _,G as k,H as A}from"../chunks/index.b1660a48.js";import{N as x}from"../chunks/Narigo.1c5e5246.js";import{K as S,a as v}from"../chunks/KataLayout.df9daedb.js";function G(r){let e,a,t,o,n;return{c(){e=w("p"),a=g(`I had to leave shortly after finishing mine. They did another challenge and had a couple
+				more solutions. My version uses default arguments and recursion to get to the result without
+				having to write a second helper function.`),t=P(),o=w("p"),n=g(`The initial idea brought up by Gustaf in the screenshare was to add the same string to the
+				string again. Then, you can take a slice of the string's length starting from index 1 up to
+				the length of the string. While it takes a bit more memory, I'm still wondering which
+				approach would be faster for the computer to work with.`)},l(s){e=y(s,"P",{});var i=b(e);a=_(i,`I had to leave shortly after finishing mine. They did another challenge and had a couple
+				more solutions. My version uses default arguments and recursion to get to the result without
+				having to write a second helper function.`),i.forEach(l),t=I(s),o=y(s,"P",{});var u=b(o);n=_(u,`The initial idea brought up by Gustaf in the screenshare was to add the same string to the
+				string again. Then, you can take a slice of the string's length starting from index 1 up to
+				the length of the string. While it takes a bit more memory, I'm still wondering which
+				approach would be faster for the computer to work with.`),u.forEach(l)},m(s,i){f(s,e,i),k(e,a),f(s,t,i),f(s,o,i),k(o,n)},p:A,d(s){s&&l(e),s&&l(t),s&&l(o)}}}function K(r){let e,a;return e=new x({props:{$$slots:{default:[G]},$$scope:{ctx:r}}}),{c(){h(e.$$.fragment)},l(t){c(e.$$.fragment,t)},m(t,o){m(e,t,o),a=!0},p(t,o){const n={};o&4&&(n.$$scope={dirty:o,ctx:t}),e.$set(n)},i(t){a||(d(e.$$.fragment,t),a=!0)},o(t){$(e.$$.fragment,t),a=!1},d(t){p(e,t)}}}function q(r){let e;return{c(){e=g(`This is the second challenge that has been done. I didn't create a solution during the event
+			as I had to leave early. From what I could see in the chat, my solution differs in the way of
+			the for loops: If you switch them (count backwards in the outer one), it's possible to return
+			the outer one as the length of the palindrome.`)},l(a){e=_(a,`This is the second challenge that has been done. I didn't create a solution during the event
+			as I had to leave early. From what I could see in the chat, my solution differs in the way of
+			the for loops: If you switch them (count backwards in the outer one), it's possible to return
+			the outer one as the length of the palindrome.`)},m(a,t){f(a,e,t)},d(a){a&&l(e)}}}function E(r){let e,a;return e=new x({props:{$$slots:{default:[q]},$$scope:{ctx:r}}}),{c(){h(e.$$.fragment)},l(t){c(e.$$.fragment,t)},m(t,o){m(e,t,o),a=!0},p(t,o){const n={};o&4&&(n.$$scope={dirty:o,ctx:t}),e.$set(n)},i(t){a||(d(e.$$.fragment,t),a=!0)},o(t){$(e.$$.fragment,t),a=!1},d(t){p(e,t)}}}function F(r){let e,a,t,o;return e=new v({props:{id:"586560a639c5ab3a260000f3",name:"All Star Code Challenge #15",solution:r[0],$$slots:{default:[K]},$$scope:{ctx:r}}}),t=new v({props:{id:"54bb6f887e5a80180900046b",name:"Longest Palindrome",solution:r[1],$$slots:{default:[E]},$$scope:{ctx:r}}}),{c(){h(e.$$.fragment),a=P(),h(t.$$.fragment)},l(n){c(e.$$.fragment,n),a=I(n),c(t.$$.fragment,n)},m(n,s){m(e,n,s),f(n,a,s),m(t,n,s),o=!0},p(n,s){const i={};s&4&&(i.$$scope={dirty:s,ctx:n}),e.$set(i);const u={};s&4&&(u.$$scope={dirty:s,ctx:n}),t.$set(u)},i(n){o||(d(e.$$.fragment,n),d(t.$$.fragment,n),o=!0)},o(n){$(e.$$.fragment,n),$(t.$$.fragment,n),o=!1},d(n){p(e,n),n&&l(a),p(t,n)}}}function L(r){let e,a;return e=new S({props:{$$slots:{default:[F]},$$scope:{ctx:r}}}),{c(){h(e.$$.fragment)},l(t){c(e.$$.fragment,t)},m(t,o){m(e,t,o),a=!0},p(t,[o]){const n={};o&4&&(n.$$scope={dirty:o,ctx:t}),e.$set(n)},i(t){a||(d(e.$$.fragment,t),a=!0)},o(t){$(e.$$.fragment,t),a=!1},d(t){p(e,t)}}}function M(r){return[`function rotate(str, acc = [], at = 0) {
+  if (str.length <= at) {
+    return acc;
+  }
+  const nextSlice = str.slice(at + 1) + str.slice(0, at + 1);
+  return rotate(str, [...acc, nextSlice], at + 1);
+}`,`function longestPalindrome(s){
+  let maxAmountOfChars = 0;
+  for (let start = 0; start < (s.length - maxAmountOfChars); start++) {
+    for (let endOfPalindrome = s.length; (start + maxAmountOfChars) < endOfPalindrome; endOfPalindrome--) {
+      if (isPalindrome(s.slice(start, endOfPalindrome))) {
+        maxAmountOfChars = endOfPalindrome - start;
+      }
+    }
+  }
+  return maxAmountOfChars;
+}
+
+function isPalindrome(s) {
+  return s.split("").reverse().join("") === s;
+}`]}class z extends O{constructor(e){super(),C(this,e,M,L,T,{})}}export{z as component};
---
+++ _app/immutable/nodes/38.069e3e63.js
@@ -0,0 +1,50 @@
+import{S as f,i as $,s as m,y as s,z as p,A as l,g as d,d as c,B as u,a as b,c as _,b as h,h as x,q as z,r as k}from"../chunks/index.b1660a48.js";import{N as T}from"../chunks/Narigo.1c5e5246.js";import{P as w,a as y}from"../chunks/PuzzlePageLayout.fbe6c2ec.js";function F(i){let t;return{c(){t=z(`That was "fun" using gradients for the teeth and nose. The linear-gradient creates the long
+		parts of the teeth, using the background color and a transparent one in between. The background
+		size and origin start it at the appropriate pixels. The radial-gradients sit on top of these
+		teeth to create the rounded part of the teeth. The nose had to be split up and needed to be
+		included in both parts: The upper skull and the jar.`)},l(n){t=k(n,`That was "fun" using gradients for the teeth and nose. The linear-gradient creates the long
+		parts of the teeth, using the background color and a transparent one in between. The background
+		size and origin start it at the appropriate pixels. The radial-gradients sit on top of these
+		teeth to create the rounded part of the teeth. The nose had to be split up and needed to be
+		included in both parts: The upper skull and the jar.`)},m(n,e){h(n,t,e)},d(n){n&&x(t)}}}function v(i){let t,n,e,r;return t=new y({props:{title:"Puzzle 56 solution",solution:i[0]}}),e=new T({props:{$$slots:{default:[F]},$$scope:{ctx:i}}}),{c(){s(t.$$.fragment),n=b(),s(e.$$.fragment)},l(a){p(t.$$.fragment,a),n=_(a),p(e.$$.fragment,a)},m(a,o){l(t,a,o),h(a,n,o),l(e,a,o),r=!0},p(a,o){const g={};o&2&&(g.$$scope={dirty:o,ctx:a}),e.$set(g)},i(a){r||(d(t.$$.fragment,a),d(e.$$.fragment,a),r=!0)},o(a){c(t.$$.fragment,a),c(e.$$.fragment,a),r=!1},d(a){u(t,a),a&&x(n),u(e,a)}}}function P(i){let t,n;return t=new w({props:{title:"Skull",$$slots:{default:[v]},$$scope:{ctx:i}}}),{c(){s(t.$$.fragment)},l(e){p(t.$$.fragment,e)},m(e,r){l(t,e,r),n=!0},p(e,[r]){const a={};r&2&&(a.$$scope={dirty:r,ctx:e}),t.$set(a)},i(e){n||(d(t.$$.fragment,e),n=!0)},o(e){c(t.$$.fragment,e),n=!1},d(e){u(t,e)}}}function S(i){return[`<div class=skull>
+  <div class=top></div>
+  <div class=bottom></div>
+</div>
+<style>
+  body {
+    background: #191919;
+    display: grid;
+    place-items: center;
+  }
+  .skull {
+    display: grid;
+    place-items: center;
+  }
+  .skull>* {
+    background: #4F77FF;
+    height: 100px;
+    width: 120px;
+  }
+  .top {
+    border-radius: 60px 60px 10px 10px;
+    background:
+      radial-gradient(circle at 35px 73px, #191919 20px, #0000 0),
+      radial-gradient(circle at 85px 73px, #191919 20px, #0000 0),
+      radial-gradient(circle at 60px 101px, #191919 10px, #0000 0),
+      #4F77FF
+    ;
+  }
+  .bottom {
+    width: 80px;
+    height: 30px;
+    border-radius: 0 0 20px 20px;
+    background:
+      linear-gradient(90deg, #191919 10px, #0000 0 15px, #191919 0 25px, #0000 0 30px, #191919 0) 20px 25px / 40px 5px no-repeat,
+      radial-gradient(circle at 40px 1px, #191919 10px, #0000 0),
+      radial-gradient(circle at 25px 25px, #191919 5px, #0000 0),
+      radial-gradient(circle at 40px 25px, #191919 5px, #0000 0),
+      radial-gradient(circle at 55px 25px, #191919 5px, #0000 0),
+      #4F77FF
+    ;
+  }
+</style>`]}class A extends f{constructor(t){super(),$(this,t,S,P,m,{})}}export{A as component};
