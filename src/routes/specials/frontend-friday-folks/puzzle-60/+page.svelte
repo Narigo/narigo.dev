eba9851d8fe4f8bc529246afffe4b4760257e0f4
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-07-22 14:51:02 +0200

    feat: Prepare solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-60/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Evil Triangles">
+	<Puzzle title="Puzzle 60 solution" {solution} />
+	<Narigo>
+		Satoshi had the great idea to add a background and draw black zig-zag lines on top. I've done
+		that with <code>grid</code> today and it felt quite nice 😅
+	</Narigo>
+</PuzzlePageLayout>
