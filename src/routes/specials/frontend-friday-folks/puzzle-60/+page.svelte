6afd3adf7eadda0effe8c42b5ae67dcc258d88a6
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-07-23 13:17:53 +0200

    feat: Add solution for puzzle 60
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/specials/frontend-friday-folks/puzzle-60/+page.svelte
+++ src/routes/specials/frontend-friday-folks/puzzle-60/+page.svelte
@@ -3,7 +3,41 @@
 	import Puzzle from '../Puzzle.svelte';
 	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
 
-	const solution = ``;
+	const solution = `<r>
+<div a></div>
+<div b></div>
+<div a></div>
+<div b></div>
+<div a></div>
+<div b></div>
+<div a></div>
+<div b></div>
+</r>
+<style>
+  body {
+    background:#191919;
+    display: grid;
+    place-items:center;
+  }
+  r{
+    display: grid;
+    place-items:center;
+    background: #4F77FF;
+    row-gap:50px;
+    padding-top:25px;
+    grid-template-columns: repeat(4, 50px);
+    height: 125px;
+  }
+  [a],[b] {
+    background: #191919;
+    height:50px;
+    width:50px;
+    transform: skewY(45deg);
+  }
+  [b] {
+    transform: skewY(-45deg);
+  }
+</style>`;
 </script>
 
 <PuzzlePageLayout title="Evil Triangles">
