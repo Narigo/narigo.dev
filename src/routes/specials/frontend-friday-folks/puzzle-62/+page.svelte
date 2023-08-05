1c10c0a8dc088cd3a61ee89f1c7fde2d913e4335
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-08-06 00:20:15 +0200

    feat: Add puzzle 62 solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-62/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div></div>
+<style>
+  body {
+    background: #191919;
+    display: grid;
+    place-items: center;
+  }
+  div {
+    background: 
+      radial-gradient(circle at 100% 100%, #824B20 100px, #0000 0),
+      radial-gradient(circle at 0 100%, #E08027 100px, #0000 0),
+      radial-gradient(circle, #FFF58F 30px, #0000 0) 0 20px,
+      #F2AD43;
+    width: 150px;
+    height: 200px;
+    border-radius: 75px 75px 20px 20px;
+  }
+</style>
+`;
+</script>
+
+<PuzzlePageLayout title="Sunset">
+	<Puzzle title="Puzzle 62 solution" {solution} />
+	<Narigo>
+		Ari, Satoshi and I were doing this together today. This was a nice puzzle to do with radial
+		gradients again!
+	</Narigo>
+</PuzzlePageLayout>
