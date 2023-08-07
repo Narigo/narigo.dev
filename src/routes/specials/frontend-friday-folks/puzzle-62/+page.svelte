4d25daa2462e202ab9fc9c6325c67299e114f4cf
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-08-08 00:00:17 +0200

    feat: Add animation solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/specials/frontend-friday-folks/puzzle-62/+page.svelte
+++ src/routes/specials/frontend-friday-folks/puzzle-62/+page.svelte
@@ -23,7 +23,41 @@
 </style>
 `;
 
-	const solutionAnimation = ``;
+	const solutionAnimation = `<div></div>
+<style>
+  body {
+    background: #191919;
+    display: grid;
+    place-items: center;
+  }
+  div {
+    background:
+      radial-gradient(circle at 100% 100%, #999 90px, #bbb 100px, transparent 0) no-repeat,
+      radial-gradient(circle at 0 100%, #666 90px, #999 100px, transparent 0) no-repeat,
+      radial-gradient(circle at 50% 120px, #FFF58F 30px, #f30 0 50px, #f60 60px, #f90 70px, transparent 100px) no-repeat,
+      deepskyblue;
+    width: 150px;
+    height: 200px;
+    border-radius: 75px 75px 20px 20px;
+    animation: anim 2s infinite ease-in-out;
+  }
+
+  @keyframes anim {
+    0% {
+      rotate: -15deg;
+      background-position-y: 0, 0, -50px, 0;
+    }
+    50% {
+      rotate: 15deg;
+      background-position-y: 0, 0, 50px, 0;
+      background-position-x: 50px, -50px, 0, 0;
+    }
+    100% {
+      rotate: -15deg;
+      background-position-y: 0, 0, -50px, 0;
+    }
+  }
+</style>`;
 </script>
 
 <PuzzlePageLayout title="Sunset">
