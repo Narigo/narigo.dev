5aa4eef1cfbef38311d4d6c559596fb266b6f4bd
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-08-12 20:09:53 +0200

    feat: Add puzzle 63
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/specials/frontend-friday-folks/puzzle-63/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div></div>
+<style>
+  *{background:#191919;overflow:visible;}
+  div,div:before,div:after {
+    content:'';
+    position:fixed;
+    inset: 38% 166px;
+    width: 50px;
+    height: 50px;
+    border: 10px solid #5DBCF9;
+    -webkit-box-reflect: right -70px;
+  }
+  div:after {
+    border-radius:50% 50% 0;
+    translate:-60px -60px;
+    -webkit-box-reflect: below 50px;
+  }
+</style>`;
+</script>
+
+<PuzzlePageLayout title="Command Key">
+	<Puzzle title="Puzzle 63 solution" {solution} />
+	<Narigo>
+		This time it was Satoshi and me, but I had a little blocker on my arm, so I had to solve it a
+		day later on my own. I like <code>-webkit-box-reflect</code>, but I'm always a bit surprised how
+		it works - especially when using absolute or fixed positioning and pseudo-elements. I think, I
+		need to look into this more!
+	</Narigo>
+</PuzzlePageLayout>
