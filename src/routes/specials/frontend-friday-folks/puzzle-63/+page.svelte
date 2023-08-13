5df4743195bd4d9dbe75339e1fc59108300a0237
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-08-13 23:47:47 +0200

    feat: Share frustration about unicode chars
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/specials/frontend-friday-folks/puzzle-63/+page.svelte
+++ src/routes/specials/frontend-friday-folks/puzzle-63/+page.svelte
@@ -26,9 +26,16 @@
 <PuzzlePageLayout title="Command Key">
 	<Puzzle title="Puzzle 63 solution" {solution} />
 	<Narigo>
-		This time it was Satoshi and me, but I had a little blocker on my arm, so I had to solve it a
-		day later on my own. I like <code>-webkit-box-reflect</code>, but I'm always a bit surprised how
-		it works - especially when using absolute or fixed positioning and pseudo-elements. I think, I
-		need to look into this more!
+		<p>
+			This time it was Satoshi and me, but I had a little blocker on my arm, so I had to solve it a
+			day later on my own. I like <code>-webkit-box-reflect</code>, but I'm always a bit surprised
+			how it works - especially when using absolute or fixed positioning and pseudo-elements. I
+			think, I need to look into this more!
+		</p>
+		<p>
+			I wanted to try solving it using the Unicode character "⌘" as the CSSBattle discussions
+			sometimes say that people are using UTF-8 characters to solve puzzles. I can't make it over
+			92.1% with this char, so I'm unsure how that works for others.
+		</p>
 	</Narigo>
 </PuzzlePageLayout>
