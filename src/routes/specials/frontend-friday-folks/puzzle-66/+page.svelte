781ef782a42896f31176221832a32ca004576ea5
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-09-02 00:02:00 +0200

    feat: Add another solution with background size
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/specials/frontend-friday-folks/puzzle-66/+page.svelte
+++ src/routes/specials/frontend-friday-folks/puzzle-66/+page.svelte
@@ -31,9 +31,42 @@
   }
 </style>
 `;
+
+	const solutionBackgroundPosition = `<div></div>
+<style>
+  body {background: #191919;display:grid;place-items:center}
+  div {
+    width: 250px;
+    height: 100px;
+    position:relative;
+    background:
+      radial-gradient(circle at 66px 160px, #191919 100px, #0000 0),
+      radial-gradient(circle at 185px 160px, #191919 100px, #0000 0),
+      radial-gradient(circle at 100% 50px, #191919 50px, #0000 0),
+      radial-gradient(circle at 0px 50px, #191919 50px, #0000 0),
+      #F2AD43;
+  }
+  div::before {
+    position:absolute;
+    inset: 0 85px 70px;
+    content: '';
+    background: 
+      radial-gradient(circle at 30px 10px, #F2AD43 5px, #0000 0),
+      radial-gradient(circle at 50px 10px, #F2AD43 5px, #0000 0),
+      linear-gradient(to right,#191919 30px, #F2AD43 0 50px, #191919 0) 0 10px no-repeat,
+      #191919;
+    border-radius: 0 0 10px 10px;
+  }
+</style>
+`;
 </script>
 
 <PuzzlePageLayout title="Batmicky">
 	<Puzzle title="Puzzle 66 solution" {solution} />
 	<Narigo>Again, having lots of fun with gradients!</Narigo>
+	<Puzzle title="Puzzle 66 solution" solution={solutionBackgroundPosition} />
+	<Narigo>
+		There is a minor improvement with this, by moving the background position down by 10px using
+		no-repeat.
+	</Narigo>
 </PuzzlePageLayout>
