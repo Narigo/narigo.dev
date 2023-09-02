ebf68e1949acff69cf1aa667f3602b1faa1dcf1f
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-09-03 01:18:21 +0200

    feat: Add another solution for background size
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/specials/frontend-friday-folks/puzzle-66/+page.svelte
+++ src/routes/specials/frontend-friday-folks/puzzle-66/+page.svelte
@@ -59,6 +59,34 @@
   }
 </style>
 `;
+
+	const solutionBackgroundSize = `<div></div>
+<style>
+  body {background: #191919;display:grid;place-items:center}
+  div {
+    width: 250px;
+    height: 100px;
+    position:relative;
+    background:
+      radial-gradient(circle at 66px 160px, #191919 100px, #0000 0),
+      radial-gradient(circle at 185px 160px, #191919 100px, #0000 0),
+      radial-gradient(circle at 100% 50px, #191919 50px, #0000 0),
+      radial-gradient(circle at 0px 50px, #191919 50px, #0000 0),
+      #F2AD43;
+  }
+  div::before {
+    position:absolute;
+    inset: 0 85px 70px;
+    content: '';
+    background: 
+      radial-gradient(circle at 30px 10px, #F2AD43 5px, #0000 0),
+      radial-gradient(circle at 50px 10px, #F2AD43 5px, #0000 0),
+      conic-gradient(#F2AD43,#F2AD43) 30px 10px / 20px 20px no-repeat,
+      #191919;
+    border-radius: 0 0 10px 10px;
+  }
+</style>
+`;
 </script>
 
 <PuzzlePageLayout title="Batmicky">
@@ -68,5 +96,11 @@
 	<Narigo>
 		There is a minor improvement with this, by moving the background position down by 10px using
 		no-repeat.
+	</Narigo>
+	<Puzzle title="Puzzle 66 solution" solution={solutionBackgroundSize} />
+	<Narigo>
+		If using background size, it will let us save a few more characters. Still, it needs some kind
+		of gradient to create an image first that can be set with background size. conic-gradient saves
+		another char vs radial and linear.
 	</Narigo>
 </PuzzlePageLayout>
