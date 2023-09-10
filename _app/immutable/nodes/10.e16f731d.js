dfa3f0244e9030b3e6506cd5a462854487d2d251
Narigo <Narigo@users.noreply.github.com>
2023-09-10 23:08:43 +0000

    Deploying to gh-pages from @ Narigo/narigo.dev@2db908d38792ea62bd54c40cf71d32f51d711b39 🚀

---
+++ _app/immutable/nodes/10.e16f731d.js
@@ -0,0 +1,28 @@
+import{s as R,f as C,a as v,l as N,g as k,h as z,A as B,c as w,d as u,m as O,i as V,x as i}from"../chunks/scheduler.58de62d3.js";import{S as U,i as X,b as I,d as x,m as b,a as S,t as q,e as E}from"../chunks/index.698fb7dd.js";import{N as M}from"../chunks/Narigo.08d9f4ea.js";import{C as Y}from"../chunks/Code.f01f5db6.js";import{C as Q}from"../chunks/CodeBlock.218a7fa3.js";import{K as Z}from"../chunks/KataLayout.e42156c9.js";function tt(r){let t;return{c(){t=N(`I think reducers are a really cool tool to do several things in one run through an array. It's
+			very easy for them to get complex and too clever to understand them quickly. I try moving
+			things out of them and keep descriptive names for variables and functions that I use.`)},l(e){t=O(e,`I think reducers are a really cool tool to do several things in one run through an array. It's
+			very easy for them to get complex and too clever to understand them quickly. I try moving
+			things out of them and keep descriptive names for variables and functions that I use.`)},m(e,n){V(e,t,n)},d(e){e&&u(t)}}}function et(r){let t;return{c(){t=N("for ... of string")},l(e){t=O(e,"for ... of string")},m(e,n){V(e,t,n)},d(e){e&&u(t)}}}function ot(r){let t;return{c(){t=N(`I didn't know that for ... of splits a string directly! It's fun to hear other people's
+			solutions and then come up with something else that nobody used to solve this. It seems to be
+			a good way of learning new stuff.`)},l(e){t=O(e,`I didn't know that for ... of splits a string directly! It's fun to hear other people's
+			solutions and then come up with something else that nobody used to solve this. It seems to be
+			a good way of learning new stuff.`)},m(e,n){V(e,t,n)},d(e){e&&u(t)}}}function nt(r){let t,e,n="Array.reduce",c,f,F="A somewhat readable reducer",L,h,P,m,A,a,y,G="for-of",T,p,W,d,H,K,g,j,$,D;return h=new Q({props:{code:r[0]}}),m=new M({props:{$$slots:{default:[tt]},$$scope:{ctx:r}}}),d=new Y({props:{$$slots:{default:[et]},$$scope:{ctx:r}}}),g=new Q({props:{code:r[1]}}),$=new M({props:{$$slots:{default:[ot]},$$scope:{ctx:r}}}),{c(){t=C("section"),e=C("h3"),e.textContent=n,c=v(),f=C("p"),f.textContent=F,L=v(),I(h.$$.fragment),P=v(),I(m.$$.fragment),A=v(),a=C("section"),y=C("h3"),y.textContent=G,T=v(),p=C("p"),W=N("Did you know that "),I(d.$$.fragment),H=N(" does not need to split the string?"),K=v(),I(g.$$.fragment),j=v(),I($.$$.fragment)},l(o){t=k(o,"SECTION",{});var s=z(t);e=k(s,"H3",{"data-svelte-h":!0}),B(e)!=="svelte-1f1xkij"&&(e.textContent=n),c=w(s),f=k(s,"P",{"data-svelte-h":!0}),B(f)!=="svelte-qoqzd5"&&(f.textContent=F),L=w(s),x(h.$$.fragment,s),P=w(s),x(m.$$.fragment,s),s.forEach(u),A=w(o),a=k(o,"SECTION",{});var l=z(a);y=k(l,"H3",{"data-svelte-h":!0}),B(y)!=="svelte-1gnuakn"&&(y.textContent=G),T=w(l),p=k(l,"P",{});var _=z(p);W=O(_,"Did you know that "),x(d.$$.fragment,_),H=O(_," does not need to split the string?"),_.forEach(u),K=w(l),x(g.$$.fragment,l),j=w(l),x($.$$.fragment,l),l.forEach(u)},m(o,s){V(o,t,s),i(t,e),i(t,c),i(t,f),i(t,L),b(h,t,null),i(t,P),b(m,t,null),V(o,A,s),V(o,a,s),i(a,y),i(a,T),i(a,p),i(p,W),b(d,p,null),i(p,H),i(a,K),b(g,a,null),i(a,j),b($,a,null),D=!0},p(o,s){const l={};s&4&&(l.$$scope={dirty:s,ctx:o}),m.$set(l);const _={};s&4&&(_.$$scope={dirty:s,ctx:o}),d.$set(_);const J={};s&4&&(J.$$scope={dirty:s,ctx:o}),$.$set(J)},i(o){D||(S(h.$$.fragment,o),S(m.$$.fragment,o),S(d.$$.fragment,o),S(g.$$.fragment,o),S($.$$.fragment,o),D=!0)},o(o){q(h.$$.fragment,o),q(m.$$.fragment,o),q(d.$$.fragment,o),q(g.$$.fragment,o),q($.$$.fragment,o),D=!1},d(o){o&&(u(t),u(A),u(a)),E(h),E(m),E(d),E(g),E($)}}}function st(r){let t,e;return t=new Z({props:{id:"52fba66badcd10859f00097e",name:"Disemvowel Trolls",$$slots:{default:[nt]},$$scope:{ctx:r}}}),{c(){I(t.$$.fragment)},l(n){x(t.$$.fragment,n)},m(n,c){b(t,n,c),e=!0},p(n,[c]){const f={};c&4&&(f.$$scope={dirty:c,ctx:n}),t.$set(f)},i(n){e||(S(t.$$.fragment,n),e=!0)},o(n){q(t.$$.fragment,n),e=!1},d(n){E(t,n)}}}function rt(r){return[`function isVowel(c) {
+  return "aeiou".includes(c.toLowerCase());
+}
+
+function disemvowel(str) {
+  return str.split('').reduce((stringWithoutVowels, char) => {
+    if (isVowel(char)) {
+      return stringWithoutVowels;
+    }
+    return stringWithoutVowels + char;
+  }, '');
+}`,`function disemvowel(str) {
+  let out = '';
+  for (const c of str) {
+    if (!['a','e','i','o','u'].includes(c.toLowerCase())) {
+      out += c;
+    }
+  }
+  return out;
+}`]}class mt extends U{constructor(t){super(),X(this,t,rt,st,R,{})}}export{mt as component};
---
+++ _app/immutable/nodes/35.f4f6ac33.js
@@ -0,0 +1,71 @@
+import{s as $,a as v,c as y,i as n,d as l,l as h,f as _,m as b,g as C,A as w,j as g,y as k}from"../chunks/scheduler.58de62d3.js";import{S as z,i as A,b as p,d as u,m as d,a as c,t as m,e as x}from"../chunks/index.698fb7dd.js";import{N as P}from"../chunks/Narigo.08d9f4ea.js";import{P as F,a as S}from"../chunks/PuzzlePageLayout.8e00c1f0.js";function T(i){let e,a,s="the VirtualCoffee.io discussion thread for CSS Battle #50",o;return{c(){e=h(`Today we didn't really manage to create very short solutions for this. There is probably a
+		better way how to solve this, but it seems like there should be a way to make this a lot
+		shorter. Maybe I'll get back to it - or someone else has shared a better solution on `),a=_("a"),a.textContent=s,o=h("."),this.h()},l(t){e=b(t,`Today we didn't really manage to create very short solutions for this. There is probably a
+		better way how to solve this, but it seems like there should be a way to make this a lot
+		shorter. Maybe I'll get back to it - or someone else has shared a better solution on `),a=C(t,"A",{href:!0,rel:!0,"data-svelte-h":!0}),w(a)!=="svelte-7webx8"&&(a.textContent=s),o=b(t,"."),this.h()},h(){g(a,"href","https://github.com/orgs/Virtual-Coffee/discussions/891"),g(a,"rel","external")},m(t,r){n(t,e,r),n(t,a,r),n(t,o,r)},p:k,d(t){t&&(l(e),l(a),l(o))}}}function j(i){let e,a,s,o;return e=new S({props:{title:"Puzzle 50 solution",solution:i[0]}}),s=new P({props:{$$slots:{default:[T]},$$scope:{ctx:i}}}),{c(){p(e.$$.fragment),a=v(),p(s.$$.fragment)},l(t){u(e.$$.fragment,t),a=y(t),u(s.$$.fragment,t)},m(t,r){d(e,t,r),n(t,a,r),d(s,t,r),o=!0},p(t,r){const f={};r&2&&(f.$$scope={dirty:r,ctx:t}),s.$set(f)},i(t){o||(c(e.$$.fragment,t),c(s.$$.fragment,t),o=!0)},o(t){m(e.$$.fragment,t),m(s.$$.fragment,t),o=!1},d(t){t&&l(a),x(e,t),x(s,t)}}}function I(i){let e,a;return e=new F({props:{title:"Use Hand Sanitizer",$$slots:{default:[j]},$$scope:{ctx:i}}}),{c(){p(e.$$.fragment)},l(s){u(e.$$.fragment,s)},m(s,o){d(e,s,o),a=!0},p(s,[o]){const t={};o&2&&(t.$$scope={dirty:o,ctx:s}),e.$set(t)},i(s){a||(c(e.$$.fragment,s),a=!0)},o(s){m(e.$$.fragment,s),a=!1},d(s){x(e,s)}}}function M(i){return[`<div class="bottle">
+  <div class="neck"></div>
+  <div class="upper"></div>
+  <div class="container"></div>
+</div>
+<div class="push"></div>
+<div class="tip"></div>
+<div class="dropa"></div>
+<div class="dropb"></div>
+<style>
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background: #1A4341;
+    margin: 0;
+  }
+  .bottle {
+    position: absolute;
+    display: grid;
+    justify-items: center;
+    inset: 70px 150px;
+  }
+  .tip {
+    background: #F3AC3C;
+    border-radius: 0 0 10px 10px;
+    position: absolute;
+    inset: 70px 100px 210px 280px;
+  }
+  .dropa,.dropb {
+    background: #998235;
+    border-radius: 10px;
+    position: absolute;
+    inset: 100px 100px 180px 280px;
+  }
+  .dropb {
+    inset: 130px 100px 150px 280px;
+  }
+  .push {
+    background: #F3AC3C;
+    border-radius: 10px 10px 0;
+    position: absolute;
+    inset: 50px 150px;
+    width: 150px;
+    height: 20px;
+  }
+  .bottle > * {
+    background: #F3AC3C;
+  }
+  .neck {
+    height: 20px;
+    width: 20px;
+  }
+  .upper {
+    border-radius: 10px 10px 0 0;
+    height: 20px;
+    width: 50px;
+  }
+  .container {
+    background: radial-gradient(circle at 25px 55px, #998235 25px, transparent 0),
+      radial-gradient(circle at 75px 55px, #F3AC3C 25px, transparent 0),
+      linear-gradient(#F3AC3C 55px, #998235 0);
+    border-radius: 20px;
+    height: 140px;
+    width: 100px;
+  }
+</style>`]}class H extends z{constructor(e){super(),A(this,e,M,I,$,{})}}export{H as component};
