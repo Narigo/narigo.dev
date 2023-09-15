c075999d4a4d82185c46aa03cb1b895635875f2b
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-09-15 17:37:29 +0200

    feat: Add puzzle 68 solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-68/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Bell">
+	<Puzzle title="Puzzle 68 solution" {solution} />
+	<Narigo>
+		That was fast and fun to work on. I learned that I can use body::before to get rid of an extra
+		z-index if I'd used a div::before...!
+	</Narigo>
+</PuzzlePageLayout>
