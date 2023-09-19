bdd3a3bdc17e45443ce46c27f254fdc99df60c09
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-09-19 18:11:02 +0200

    chore: Add code blocks around inline code
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/frontend-friday-folks/puzzle-68/+page.svelte
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-68/+page.svelte
@@ -33,7 +33,7 @@
 <PuzzlePageLayout title="Bell">
 	<Puzzle title="Puzzle 68 solution" {solution} />
 	<Narigo>
-		That was fast and fun to work on. I learned that I can use body::before to get rid of an extra
-		z-index if I'd used a div::before...!
+		That was fast and fun to work on. I learned that I can use <code>body::before</code> to get rid
+		of an extra <code>z-index</code> if I'd used a <code>div::before</code>...!
 	</Narigo>
 </PuzzlePageLayout>
