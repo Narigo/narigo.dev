8224d62bf61e37e57eb555f3c4f7d43616a24e15
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-09-27 20:04:42 +0200

    chore: Add shell for puzzle 69 that I missed
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-69/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="PushOwl">
+	<Puzzle title="Puzzle 69 solution" {solution} />
+	<Narigo>I missed doing this puzzle with VirtualCoffee, so I had to do this on my own.</Narigo>
+</PuzzlePageLayout>
