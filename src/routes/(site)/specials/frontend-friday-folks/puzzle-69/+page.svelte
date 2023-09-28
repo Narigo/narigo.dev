44cebe48b40e3baeb23e8f4844c445ba14703245
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-09-28 23:33:30 +0200

    feat: Add 99% solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/frontend-friday-folks/puzzle-69/+page.svelte
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-69/+page.svelte
@@ -3,10 +3,62 @@
 	import Puzzle from '../Puzzle.svelte';
 	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
 
-	const solution = ``;
+	const solution99 = `<r>
+  <div l></div>
+  <p></p>
+  <div r></div>
+</r>
+<style>
+  body {
+    background:#191919;
+    display: grid;
+    place-items:center;
+  }
+  r {
+    width:242px;
+    height:166px;
+    position:relative;
+  }
+  p{
+    position:absolute;
+    background: #E08027;
+    bottom:5px;
+    left:95px;
+    rotate:45deg;
+    height:50px;
+    width:50px;
+  }
+  div {
+    position:absolute;
+    box-sizing:border-box;
+    width: 134px;
+    height: 134px;
+    background:
+      radial-gradient(#191919 45px, #E08027 0),
+      #dd6b4d;
+    border:10px solid #191919;
+    border-radius: 0 50% 50%;
+  }
+  div::after {
+    content: '';
+    position:absolute;
+    inset: 47px 41px;
+    height: 12px;
+    width: 12px;
+    border: 10px solid #E08027;
+    border-radius:50%;
+    border-top-color: #0000;
+    border-left-color: #0000;
+    rotate:225deg
+  }
+  [r]{
+    right:0;
+    transform:scale(-1, 1);
+  }
+</style>`;
 </script>
 
 <PuzzlePageLayout title="PushOwl">
-	<Puzzle title="Puzzle 69 solution" {solution} />
-	<Narigo>I missed doing this puzzle with VirtualCoffee, so I had to do this on my own.</Narigo>
+	<Puzzle title="Puzzle 69 solution" solution={solution99} />
+	<Narigo>I missed doing this puzzle with VirtualCoffee, so I had to do this on my own. Only achieved 99.9% this time.</Narigo>
 </PuzzlePageLayout>
