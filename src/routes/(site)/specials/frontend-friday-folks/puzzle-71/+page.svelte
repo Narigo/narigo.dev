ce6de1a315ae45ae78831f6fc395680bf60e40a6
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-10-10 23:41:39 +0200

    feat: Add a link to solution since I did not built one myself
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-71/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Elephant">
+	<Narigo>
+		I couldn't make it this week. Thanks to David, it still happened and a <a
+			href="https://github.com/orgs/Virtual-Coffee/discussions/1043"
+			rel="external">solution for this puzzle</a
+		> has been posted.
+	</Narigo>
+</PuzzlePageLayout>
