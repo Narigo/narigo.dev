fc14111fbd4cc38c1810511a65cb10c559fdee03
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-10-11 23:57:39 +0200

    chore: Prepare puzzle solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/frontend-friday-folks/puzzle-71/+page.svelte
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-71/+page.svelte
@@ -7,6 +7,7 @@
 </script>
 
 <PuzzlePageLayout title="Elephant">
+	<Puzzle title="Puzzle 71 solution" {solution} />
 	<Narigo>
 		I couldn't make it this week. Thanks to David, it still happened and a <a
 			href="https://github.com/orgs/Virtual-Coffee/discussions/1043"
