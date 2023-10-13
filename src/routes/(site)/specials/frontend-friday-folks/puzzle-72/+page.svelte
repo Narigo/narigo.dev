8ce423d90658c2ca2c66bab8bd23f168e4a69908
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-10-14 00:27:50 +0200

    chore: Prepare puzzle 72
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-72/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Sheep">
+	<Puzzle title="Puzzle 72 solution" {solution} />
+	<Narigo>
+		This one is again something I only got 99.9% for. David has seen this as two divs with one
+		rotated 45 degrees.
+	</Narigo>
+</PuzzlePageLayout>
