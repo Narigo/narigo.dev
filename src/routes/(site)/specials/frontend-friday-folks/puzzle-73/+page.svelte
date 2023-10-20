8131cc1017e5b9ecdf502f6ae3f73ef1fb941f86
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-10-20 20:11:27 +0200

    chore: Prepare solution 73
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-73/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Happy Tiger">
+	<Puzzle title="Puzzle 73 solution" {solution} />
+	<Narigo>
+		Lots of gradients! 100% again! Finally! Yes! This time, using background positioning and
+		background sizes to get the correct result.
+	</Narigo>
+</PuzzlePageLayout>
