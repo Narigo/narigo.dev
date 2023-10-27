1d111f0f701d2e2f1073ca4612783b878da2899e
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-10-28 00:26:08 +0200

    chore: Prepare solution 74
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-74/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Danger Noodle">
+	<Puzzle title="Puzzle 74 solution" {solution} />
+	<Narigo>
+		This was quite hard to do. I've used gradients only and one div for the head. Since most parts
+		of the gradients are the same, I've used variables extensively. I learned that you cannot use a
+		variable to start off a function and another one to close it. You can put the arguments into a
+		variable, if you need them though.
+	</Narigo>
+</PuzzlePageLayout>
