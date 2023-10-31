6aa6e2c2a0069db2fd163ef4641f559a267b5ad9
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-10-31 23:09:47 +0100

    chore: Add a smaller solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/frontend-friday-folks/puzzle-32/+page.svelte
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-32/+page.svelte
@@ -6,6 +6,7 @@
 	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
 
 	const solution = `<r><r><style>*+*{display:grid;place-items:center}r{position:fixed;rotate:45deg;width:50;height:200;background:#A3A368}r>r{rotate:90deg;background:#F3AC3C;mix-blend-mode:screen`;
+	const solution2 = `<r><r><style>*+*,r{display:grid;place-items:center}r{rotate:45deg;width:50;height:200;background:#A3A368;mix-blend-mode:screen}r>r{rotate:90deg;background:#F3AC3C`;
 </script>
 
 <PuzzlePageLayout title="Band-aid">
@@ -13,5 +14,11 @@
 	<Narigo>
 		I saw the Savvy Saturday Stylers Slaying CSSBattle.dev doing puzzle 32 already, so I thought I
 		should add it since I only started from puzzle 33.
+	</Narigo>
+	<Puzzle title="Puzzle 32 solution" solution={solution2} />
+	<Narigo>
+		And after Meg challenging me after me saying "I got a character less!", this is my newest
+		solution. I saw that using some undefined element needs display:block to work like regular divs.
+		Otherwise you have to resort to using position:fixed or display:block on those elements. 🤔
 	</Narigo>
 </PuzzlePageLayout>
