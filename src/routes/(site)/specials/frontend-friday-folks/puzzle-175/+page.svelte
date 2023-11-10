fbdc22fe0afe10a9a34b2eaeaf29275a23043bcd
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-11-10 23:57:24 +0100

    chore: Prepare 175
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-175/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Evil Cat">
+	<Puzzle title="Puzzle 175 solution" {solution} />
+	<Narigo>
+	</Narigo>
+</PuzzlePageLayout>
