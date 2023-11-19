f3d09dda9eb0fd2943a1bea08c2cc56cb9a241de
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-11-19 23:47:29 +0100

    chore: Prepare puzzle 154
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-154/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Poker Chip">
+	<Puzzle title="Puzzle 154 solution" {solution} />
+	<Narigo>
+		Savvy Saturday Stylers Slaying CSSBattle.dev is the Saturday version of this. On Nov 18th, 2023,
+		they played the "Poker Chip".
+	</Narigo>
+</PuzzlePageLayout>
