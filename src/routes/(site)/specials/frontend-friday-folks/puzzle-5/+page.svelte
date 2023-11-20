c5de9edffb8fda73cac2fc84deeefd24972542ba
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-11-21 00:42:17 +0100

    feat: Add two solutions for puzzle 5
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-5/+page.svelte
@@ -0,0 +1,84 @@
+<script>
+	import Code from '$lib/common/Code.svelte';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const firstSolution = `<div class="c r"></div>
+<div class="d m"></div>
+<div class="d l"></div>
+<style>
+  body {margin:0;background:#0B2429;}
+  div {
+    position: absolute;
+    width: 120px;
+    height: 120px;
+    background: #F3AC3C;
+    border-radius: 50%;
+  }
+  .m {
+    background: #998235;
+    top: 50%;
+    left: 50%;
+    translate: -50% -50%;
+  }
+  .c {
+    bottom: 50%;
+    left: 50%;
+  }
+  .d {
+    border-top-right-radius: 0;
+  }
+  .l {
+    top: 50%;
+    right: 50%;
+  }
+</style>
+`;
+
+	const secondSolution = `<div></div>
+<div id=b></div>
+<style>
+  body {background: #0B2429;display: grid;place-items:center;}
+  #b {
+    position: absolute;
+    transform: scale(-1);
+    z-index: -1;
+  }
+  div {
+    background: #F3AC3C;
+    border-radius: 50% 0 50% 50%;
+    position: relative;
+    height: 120px;
+    width: 120px;
+  }
+  div:before,div:after {
+    border-radius: 50% 0;
+    content: '';
+    inset: 0;
+    position: absolute;
+  }
+  div:before {
+    background: #998235;
+  }
+  div:after {
+    background: #F3AC3C;
+    inset: 60px 60px -60px -60px;
+    border-radius: 50% 0 50% 50%;
+  }
+</style>
+`;
+</script>
+
+<PuzzlePageLayout title="">
+	<Puzzle title="Puzzle 5 solution" solution={firstSolution} />
+	<Narigo>
+		It looks like I've done this puzzle multiple times. This is the first solution available to me
+		in CSSBattle.dev.
+	</Narigo>
+	<Puzzle title="Second solution for puzzle 5" solution={secondSolution} />
+	<Narigo>
+		This is the second solution, half a year later. Is it better or worse now...? Since it's been
+		half a year already, I should maybe do it a third time...
+	</Narigo>
+</PuzzlePageLayout>
