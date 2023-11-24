df8d2905c80bd303272e44884f92c8decb72f281
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-11-24 20:10:24 +0100

    chore: Add puzzle 75
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-75/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Code from '$lib/common/Code.svelte';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Hippo">
+	<Puzzle title="Puzzle 75 solution" {solution} />
+	<Narigo>
+		I've tried using <Code>-webkit-box-reflect</Code>, but it doesn't seem to work out with absolute
+		positioning...? Anyways, I think the main idea here is using border radius in pixels and
+		percentages to get to 100%.
+	</Narigo>
+</PuzzlePageLayout>
