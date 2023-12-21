5d15f0cd68261db0a7a255693a5ed84265f22105
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-12-21 23:46:38 +0100

    chore: Move to tailwindcss
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/lib/common/PageLayout/ArticleLayout.svelte
+++ src/lib/common/PageLayout/ArticleLayout.svelte
@@ -13,15 +13,8 @@
 	<article>
 		<h1>{title}</h1>
 		{#if lastUpdateOn}
-			<div class="date">Last update on <span>{lastUpdateOn}</span></div>
+			<div class="text-xs opacity-70">Last update on <span>{lastUpdateOn}</span></div>
 		{/if}
 		<slot />
 	</article>
 </PageLayout>
-
-<style>
-	.date {
-		font-size: 0.8em;
-		opacity: 0.7;
-	}
-</style>
