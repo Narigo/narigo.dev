67fe3c354fffdc74582326d86450a35271ef341a
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-12-24 23:14:55 +0100

    chore: Prepare puzzle 77
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-77/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Notes">
+	<Puzzle title="Puzzle 77 solution" {solution} />
+	<Narigo></Narigo>
+</PuzzlePageLayout>
