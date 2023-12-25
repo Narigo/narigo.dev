bf7a054516981154471265a70835e23dd9f7c276
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-12-25 02:55:01 +0100

    feat: Add solution to puzzle 77
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/frontend-friday-folks/puzzle-77/+page.svelte
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-77/+page.svelte
@@ -3,10 +3,26 @@
 	import Puzzle from '../Puzzle.svelte';
 	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
 
-	const solution = ``;
+	const solution = `<div></div>
+<style>
+  body{display:grid;place-items:center;background:#191919}
+  div {
+    width: 290;
+    height: 120;
+    background:
+      radial-gradient(ellipse,#FE5F55 25px,#0000 0) 185px 60px / 100px 80px no-repeat,
+      radial-gradient(ellipse,#A64942 25px,#0000 0) 115px 60px / 100px 80px no-repeat,
+      radial-gradient(ellipse,#FE5F55 25px,#0000 0) 45px 60px / 100px 80px no-repeat,
+      radial-gradient(ellipse,#FE5F55 25px,#0000 0) -25px 60px / 100px 80px no-repeat,
+      linear-gradient(to right,#0000 40px,#FE5F55 0 120px,#0000 0 180px,#A64942 0 220px,#0000 0 250px,#FE5F55 0 290px,#0000 0) 100% -110px no-repeat,
+      linear-gradient(to right,#0000 40px,#FE5F55 0 50px,#0000 0 110px,#FE5F55 0 120px,#0000 0 180px,#A64942 0 190px,#0000 0 250px,#FE5F55 0 260px,#0000 0) 100% -20px no-repeat,
+      linear-gradient(#0000 10px,#191919 0 20px,#0000 0),
+      linear-gradient(to right,#0000 260px,#FE5F55 0 290px,#0000 0) 100% -90px no-repeat;
+  }
+</style>`;
 </script>
 
 <PuzzlePageLayout title="Notes">
 	<Puzzle title="Puzzle 77 solution" {solution} />
-	<Narigo></Narigo>
+	<Narigo>Playing around with background sizes and positions in gradients here.</Narigo>
 </PuzzlePageLayout>
