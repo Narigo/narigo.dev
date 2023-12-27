ffc27cd1f56244e4e5cd907d6226c4bb46739229
Joern Bernhardt <joern.bernhardt@googlemail.com>
2023-12-28 00:10:54 +0100

    chore: Move announcement to tailwind with animation
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/lib/common/Announcement.svelte
+++ src/lib/common/Announcement.svelte
@@ -5,64 +5,18 @@
 <section>
 	<slot name="title">
 		{#if title}
-			<h2>
-				<div class="icon">
+			<h2 class="animate-announce motion-reduce:animate-none flex flex-wrap gap-4 items-center justify-center mx-auto mb-2 text-center">
+				<div class="inline-block">
 					📢
-					<div class="-scale-x-100">🗣️</div>
+					<div class="inline-block -scale-x-100">🗣️</div>
 				</div>
-				<div>{title}</div>
-				<div class="icon">
+				<div class="inline-block">{title}</div>
+				<div class="inline-block">
 					🗣️
-					<div class="-scale-x-100">📢</div>
+					<div class="inline-block -scale-x-100">📢</div>
 				</div>
 			</h2>
 		{/if}
 	</slot>
 	<slot />
 </section>
-
-<style>
-	h2 {
-		align-items: center;
-		animation: announcement 2s infinite;
-		display: flex;
-		flex-wrap: wrap;
-		gap: 1em;
-		justify-content: center;
-		margin: 0 auto 0.5em;
-		text-align: center;
-	}
-
-	h2 div {
-		display: inline-block;
-	}
-
-	@keyframes announcement {
-		0% {
-			rotate: 0deg;
-			scale: 1;
-		}
-		25% {
-			rotate: -2deg;
-			scale: 1.05;
-		}
-		50% {
-			rotate: 0deg;
-			scale: 1;
-		}
-		75% {
-			rotate: 2deg;
-			scale: 1.05;
-		}
-		100% {
-			rotate: 0deg;
-			scale: 1;
-		}
-	}
-
-	@media (prefers-reduced-motion) {
-		h2 {
-			animation: none;
-		}
-	}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,30 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
+			animation: {
+				announce: 'announce 2000ms infinite ease'
+			},
 			colors: {
 				primary: {
 					light: 'rgb(var(--color-primary-light) / <alpha-value>)',
 					DEFAULT: 'rgb(var(--color-primary) / <alpha-value>)',
 					dark: 'rgb(var(--color-primary-dark) / <alpha-value>)'
+				}
+			},
+			keyframes: {
+				announce: {
+					'0%, 50%, 100%': {
+						rotate: '0deg',
+						scale: 1
+					},
+					'25%': {
+						rotate: '-2deg',
+						scale: 1.05
+					},
+					'75%': {
+						rotate: '2deg',
+						scale: 1.05
+					}
 				}
 			}
 		}
