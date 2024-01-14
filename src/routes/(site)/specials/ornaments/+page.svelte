6d2e99c5b07dba491793359b4c48883eb654d0d8
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-01-14 23:50:58 +0100

    chore: Prepare page for ornaments
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/ornaments/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import PageLayout from '$lib/common/PageLayout/PageLayout.svelte';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+</script>
+
+<PageLayout>
+    <h1>Ornaments</h1>
+    <Narigo>
+        hello.
+    </Narigo>
+</PageLayout>
