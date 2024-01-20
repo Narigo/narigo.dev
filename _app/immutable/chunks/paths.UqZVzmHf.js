6cfc598853345ca62cb91ec24c14b392452bf736
Narigo <Narigo@users.noreply.github.com>
2024-01-20 23:43:42 +0000

    Deploying to gh-pages from @ Narigo/narigo.dev@1df4456683d1e3d67a6a3335a2936d7e82178b31 🚀

---
+++ _app/immutable/chunks/paths.UqZVzmHf.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_2eicwi?.base??"",e=globalThis.__sveltekit_2eicwi?.assets??s;export{e as a,s as b};
--- _app/immutable/chunks/paths.ezweKNTA.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_gnqcgk?.base??"",a=globalThis.__sveltekit_gnqcgk?.assets??s;export{a,s as b};
