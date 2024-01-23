36100a630ec241b0559219414f67f474b69ee6bd
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-01-23 23:13:40 +0100

    feat: Add some thoughts and link to potential improvements issue
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/articles/jscc23/+page.svelte
+++ src/routes/(site)/articles/jscc23/+page.svelte
@@ -14,4 +14,50 @@
 		things were organized "on the fly", like having enough BBQ grills. And some stuff just worked
 		out by chance shortly before the event - like having Friday's breakfast sponsored.
 	</p>
+	<h2>What went well</h2>
+	<p>
+		When organizing such an event, I'm focused more on the things that did not work out so well.
+		It's great that we received a lot of positive feedback from the participants. People mentioned
+		they'll join again next time and the community discord was very active and attracted quite a few
+		new members - even after the event!
+	</p>
+	<h2>Things we can improve</h2>
+	<p>
+		We have written down a couple of <a
+			href="https://github.com/jscraftcamp/website/issues/1723"
+			rel="external">improvement opportunities in the JSCraftCamp repository</a
+		>. If you have comments or feedback, I'd be happy to read them in the comments of the linked
+		issue!
+	</p>
+	<ul>
+		<li>PR is a hurdle, takes time and is "difficult"</li>
+		<li>explain/show pacman rule to make groups of people open</li>
+		<li>split session presentation and room finding, to speed it up</li>
+		<li>propose people to have others present their session, if wanted</li>
+		<li>
+			prepare a location-orga-cheatsheet, when does what happen and who takes care, prepared upfront
+		</li>
+		<li>
+			in multiple sessions: 2 or 3 people spoke ~80% of time without looking if others wanted to
+			talk. making it hard to include those people → some people talk louder (mention this issue at
+			the start of the day) and maybe session hosts can moderate if necessary
+		</li>
+	</ul>
+	<h3>Registration process</h3>
+	<p>
+		The general feedback for the PR was usually that it's too time consuming and difficult. I have
+		helped everyone who wanted to register and I even went into video chats with people to do it
+		with them. There were probably others who didn't take the time or had the motivation to register
+		due to this fact. Another problem with this registration is the openness of it. Some people just
+		don't want to tell everybody what events they attend and we should make sure they can feel
+		welcome as well.
+	</p>
+	<p>
+		I have to admit that I kind of like the PR approach as it weeds out people who are not willing
+		to invest time into this event. We have a very low no-show rate at the event and I believe this
+		is due to the fact that people had to commit something to be able to attend (pun only slightly
+		intended). I'm okay with trying out a different registration process next time though: Helping
+		people to register and unregister was quite a big time investment for myself.
+	</p>
+	<p>I'd be happy to hear ideas from the community about this!</p>
 </ArticleLayout>
