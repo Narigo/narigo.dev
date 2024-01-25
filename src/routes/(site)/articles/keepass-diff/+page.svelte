416ccfef21ecbf8974aa7064c3a02635e3082a24
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-01-25 18:59:36 +0100

    chore: Prepare keepass-diff article page
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/articles/keepass-diff/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import ArticleLayout from '$lib/common/PageLayout/ArticleLayout.svelte';
+</script>
+
+<ArticleLayout title="keepass-diff" lastUpdateOn="2024-01-25">
+    preparing...
+</ArticleLayout>
