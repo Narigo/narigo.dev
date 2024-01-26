f81ddf440e8aeecc419c00a8092aca6534041d11
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-01-26 23:01:54 +0100

    chore: Add basic outline
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/articles/keepass-diff/+page.svelte
+++ src/routes/(site)/articles/keepass-diff/+page.svelte
@@ -3,5 +3,12 @@
 </script>
 
 <ArticleLayout title="keepass-diff" lastUpdateOn="2024-01-25">
-    preparing...
+	<p>
+		TL;DR: I've created a tool called <a href="https://keepass-diff.narigo.dev/" rel="external"
+			>keepass-diff, to diff two Keepass databases (.kdbx) files</a
+		>.
+	</p>
+	<h2>Motivation</h2>
+	<h2>How does it work?</h2>
+	<h2>Check it out</h2>
 </ArticleLayout>
