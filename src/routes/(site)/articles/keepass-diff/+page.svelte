0bf339075e5b89a2ff1beafb3d3bfd9f8a7c9357
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-01-27 00:31:41 +0100

    chore: Add motivation
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/articles/keepass-diff/+page.svelte
+++ src/routes/(site)/articles/keepass-diff/+page.svelte
@@ -9,6 +9,23 @@
 		>.
 	</p>
 	<h2>Motivation</h2>
+	<p>
+		The first real password manager I've used was Keepass. Sharing databases with others happened
+		through OwnCloud / NextCloud instances and .kdbx files. When I started using it for my own
+		passwords, I could sync my database with multiple devices in a similar fashion using NextCloud.
+	</p>
+	<p>
+		At some point in time, you may forget closing the database on one machine and still use it to
+		edit something on another. If that happens, NextCloud creates two files, showing that there is a
+		conflict as it can't merge these encrypted files automatically. Since I don't really have to
+		look in the directory all the time, it happened that I had conflicting files and didn't realize
+		it until I forgot which passwords I've changed. With bigger databases, checking each individual
+		password becomes a tedious and time consuming process.
+	</p>
+	<p>
+		As I'm a software engineer, I figured out that I should probably write something that helps me
+		with this task. And I wanted to check out the Rust programming language for a while.
+	</p>
 	<h2>How does it work?</h2>
 	<h2>Check it out</h2>
 </ArticleLayout>
