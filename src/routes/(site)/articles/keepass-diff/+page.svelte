1b3fa38d69aa615dc5ca4556bdeaef25860a90e4
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-01-29 00:40:55 +0100

    chore: Add links, emphasize motivation to learn rust
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/articles/keepass-diff/+page.svelte
+++ src/routes/(site)/articles/keepass-diff/+page.svelte
@@ -10,9 +10,13 @@
 	</p>
 	<h2>Motivation</h2>
 	<p>
-		The first real password manager I've used was Keepass. Sharing databases with others happened
-		through OwnCloud / NextCloud instances and .kdbx files. When I started using it for my own
-		passwords, I could sync my database with multiple devices in a similar fashion using NextCloud.
+		The first real password manager I've used was <a href="https://keepass.info/" rel="external"
+			>KeePass</a
+		>. Sharing databases with others happened through
+		<a href="https://owncloud.com/" rel="external"> OwnCloud</a>
+		/ <a href="https://nextcloud.com/" rel="external">NextCloud</a> instances and .kdbx files. When I
+		started using it for my own passwords, I could sync my database with multiple devices in a similar
+		fashion using NextCloud.
 	</p>
 	<p>
 		At some point in time, you may forget closing the database on one machine and still use it to
@@ -23,11 +27,10 @@
 		password becomes a tedious and time consuming process.
 	</p>
 	<p>
-		As I'm a software engineer, I figured out that I should probably write something that helps me
-		with this task. And I wanted to check out the <a
-			href="https://www.rust-lang.org/"
-			rel="external">Rust programming language</a
-		> for a while.
+		As I'm a software engineer, I wanted to write something that helps me with this task. And I
+		wanted to check out the <a href="https://www.rust-lang.org/" rel="external"
+			>Rust programming language</a
+		> for a while. What a great excuse to learn something new!
 	</p>
 	<h2>How does it work?</h2>
 	<h2>Check it out</h2>
