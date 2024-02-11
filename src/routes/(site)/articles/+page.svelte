45d080a6be12de4a2396c59f75573fb67a54db55
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-02-11 16:05:57 +0100

    chore: Reverse sort order
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/articles/+page.svelte
+++ src/routes/(site)/articles/+page.svelte
@@ -7,14 +7,14 @@
 	<h2>Articles</h2>
 	<ul>
 		<li>
-			Read about <a href="{base}/articles/pocuter-one">my experience with the pocuter one</a>.
+			I have written <a href="{base}/articles/jscc23">some thoughts about JSCraftCamp 2023</a>.
 		</li>
 		<li>
 			Read about <a href="{base}/articles/keepass-diff">the keepass-diff CLI tool written in Rust</a
 			>.
 		</li>
 		<li>
-			I have written <a href="{base}/articles/jscc23">some thoughts about JSCraftCamp 2023</a>.
+			Read about <a href="{base}/articles/pocuter-one">my experience with the pocuter one</a>.
 		</li>
 	</ul>
 </PageLayout>
