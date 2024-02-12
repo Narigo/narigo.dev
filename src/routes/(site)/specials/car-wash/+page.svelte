7d4654312f8d477658de8241e59fc1b4aba15db9
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-02-12 19:14:49 +0100

    chore: Add explanation what car-wash should be
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/car-wash/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import PageLayout from '$lib/common/PageLayout/PageLayout.svelte';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+</script>
+
+<PageLayout>
+	<h2>Car Wash</h2>
+	<Narigo>
+		This is going to be something for my kids. They like cars. They like dirty cars getting cleaned.
+		I believe this <a href="https://retne-games.itch.io/car-wash" rel="external noopener noreferrer"
+			>Car Wash (for Toddlers) game by retne_games</a
+		> can be done with CSS and little JavaScript. Let's try it!
+	</Narigo>
+</PageLayout>
