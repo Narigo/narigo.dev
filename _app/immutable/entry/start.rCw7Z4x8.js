1141599a60c55ed3faa685d8409066265adcd63e
Narigo <Narigo@users.noreply.github.com>
2024-02-16 23:07:49 +0000

    Deploying to gh-pages from @ Narigo/narigo.dev@099d27bf95901138254af6ade47eafc9c531e5b1 🚀

---
+++ _app/immutable/entry/start.rCw7Z4x8.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.B_xY0wCf.js";export{t as start};
---
+++ _app/immutable/nodes/42.DF4-0ViV.js
@@ -0,0 +1,24 @@
+import{s as g,b as h,j as _,k as f,i as c,t as b,h as x}from"../chunks/scheduler.C0BfHF-J.js";import{S as z,i as y,c as i,b as u,m as l,t as m,a as p,d as $}from"../chunks/index.LRqrGZ80.js";import{N as w}from"../chunks/Narigo.DFshNKlD.js";import{P as k,a as P}from"../chunks/PuzzlePageLayout.DY1QgVke.js";function v(a){let t;return{c(){t=b(`Dan hosted it today. I don't think there was a solution that can get rid of the inner element
+		in order to get the inner "border" round from the outer element.`)},l(o){t=x(o,`Dan hosted it today. I don't think there was a solution that can get rid of the inner element
+		in order to get the inner "border" round from the outer element.`)},m(o,e){f(o,t,e)},d(o){o&&c(t)}}}function F(a){let t,o,e,r;return t=new P({props:{title:"Puzzle 48 solution",solution:a[0]}}),e=new w({props:{$$slots:{default:[v]},$$scope:{ctx:a}}}),{c(){i(t.$$.fragment),o=h(),i(e.$$.fragment)},l(n){u(t.$$.fragment,n),o=_(n),u(e.$$.fragment,n)},m(n,s){l(t,n,s),f(n,o,s),l(e,n,s),r=!0},p(n,s){const d={};s&2&&(d.$$scope={dirty:s,ctx:n}),e.$set(d)},i(n){r||(m(t.$$.fragment,n),m(e.$$.fragment,n),r=!0)},o(n){p(t.$$.fragment,n),p(e.$$.fragment,n),r=!1},d(n){n&&c(o),$(t,n),$(e,n)}}}function A(a){let t,o;return t=new k({props:{title:"Wash your hands",$$slots:{default:[F]},$$scope:{ctx:a}}}),{c(){i(t.$$.fragment)},l(e){u(t.$$.fragment,e)},m(e,r){l(t,e,r),o=!0},p(e,[r]){const n={};r&2&&(n.$$scope={dirty:r,ctx:e}),t.$set(n)},i(e){o||(m(t.$$.fragment,e),o=!0)},o(e){p(t.$$.fragment,e),o=!1},d(e){$(t,e)}}}function D(a){return[`<div><dt></dt></div>
+<style>
+  body {
+    background: #293462
+  }
+  div {
+    background: #FE5F55;
+    border: 30px solid #FE5F55;
+    border-radius: 20px;
+    box-shadow: 0 20px #A64942;
+    width: 140px;
+    inset: 90px 0 110px;
+    margin: auto;
+    position: absolute;
+  }
+  dt {
+    background: #A64942;
+    border-radius: 40px;
+    height: 40px;
+  }
+</style>
+`]}class j extends z{constructor(t){super(),y(this,t,D,A,g,{})}}export{j as component};
