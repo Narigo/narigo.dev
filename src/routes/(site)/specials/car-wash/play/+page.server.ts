f73c17d61f2a61a634e5798efd8da372d6d11cf6
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-02-16 11:38:07 +0100

    feat: Animate and move parts
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/car-wash/play/+page.server.ts
+++ src/routes/(site)/specials/car-wash/play/+page.server.ts
@@ -1,7 +1,11 @@
 import type { PageServerLoad } from './$types';
+import car1 from '../_assets/car1.svg?raw';
+import car2 from '../_assets/car2.svg?raw';
 
 export const load: PageServerLoad = async () => {
-	const imageFiles = await import.meta.glob('../_assets/*.svg', { eager: true });
-	const images = Object.values(imageFiles).map((module) => module.default);
+	// const imageFiles = await import.meta.glob('../_assets/car1.svg', { eager: true });
+	// const images = Object.values(imageFiles).map((module) => module.default);
+	const images = [car1, car2];
+	// const images = [car1];
 	return { images };
 };
--- src/routes/(site)/specials/car-wash/play/+page.svelte
+++ src/routes/(site)/specials/car-wash/play/+page.svelte
@@ -30,35 +30,107 @@
 				break;
 		}
 	};
+
+	const AMOUNT_OF_FINGERS = 20;
+	const AMOUNT_OF_ROWS = 5;
 </script>
 
 <PageLayout>
-	<section class="overflow-clip {currentStep}">
-		<div>shower</div>
-		<div>
-			<img src={image} alt={image} />
+	<section class="aspect-video relative grid overflow-clip {currentStep}">
+		<div
+			class="mitter back absolute grid h-full w-full"
+			class:moving={currentStep === 'cleaning'}
+			class:rotating={['cleaning', 'drying'].includes(currentStep)}
+		>
+			<div class="absolute right-8 top-[5%] h-[90%] w-24">
+				{#each new Array(AMOUNT_OF_FINGERS * AMOUNT_OF_ROWS) as _, index}
+					<div
+						class="finger rounded absolute w-1 bg-gradient-to-b {index % 2 === 0
+							? 'from-blue-600 to-blue-800'
+							: 'from-blue-800 to-blue-900'}"
+						style="height: {100 / AMOUNT_OF_ROWS}%;left:{((index % AMOUNT_OF_FINGERS) /
+							AMOUNT_OF_FINGERS) *
+							100}%;top:{(Math.floor(index / AMOUNT_OF_FINGERS) * 100) / AMOUNT_OF_ROWS}%"
+					></div>
+				{/each}
+			</div>
 		</div>
-		<div>roller</div>
-		<button on:click={() => nextStep()}>{currentStep}</button>
-		<a href="{base}/specials/car-wash/credits">Credits</a>
+		<div
+			class="track absolute grid place-items-center items-end h-full w-full pb-1 [&_svg_.body]:fill-blue-500 [&_svg_.darker]:fill-blue-800 [&_svg_.lighter]:fill-blue-300 {[
+				'start',
+				'dirty',
+				'cleaning'
+			].includes(currentStep)
+				? ''
+				: '[&_svg_.dirt]:fill-none'}"
+		>
+			{@html image}
+		</div>
+		<div
+			class="mitter front absolute grid h-full w-full"
+			class:moving={currentStep === 'cleaning'}
+			class:rotating={['cleaning', 'drying'].includes(currentStep)}
+		>
+			<div class="absolute right-4 top-0 h-full w-24">
+				{#each new Array(AMOUNT_OF_FINGERS * AMOUNT_OF_ROWS) as _, index}
+					<div
+						class="finger rounded absolute w-1 bg-gradient-to-b {index % 2 === 0
+							? 'from-blue-500 to-blue-400'
+							: 'from-blue-600 to-blue-700'}"
+						style="height: {100 / AMOUNT_OF_ROWS}%;left:{((index % AMOUNT_OF_FINGERS) /
+							AMOUNT_OF_FINGERS) *
+							100}%;top:{(Math.floor(index / AMOUNT_OF_FINGERS) * 100) / AMOUNT_OF_ROWS}%"
+					></div>
+				{/each}
+			</div>
+		</div>
 	</section>
+	<button class="" on:click={() => nextStep()}>{currentStep}</button>
 </PageLayout>
 
 <style lang="postcss">
-	section:global(.start) img {
+	.mitter {
+		transform: translate(0);
+		transition: transform 2000ms;
+	}
+	.moving {
+		transform: translate(calc(-100% + 6rem + 2rem));
+	}
+	.back.moving {
+		transform: translate(calc(-100% + 6rem + 4rem));
+	}
+	.finger {
+		transform-origin: 50% 0;
+		transition: transform 2000ms;
+	}
+	.rotating .finger {
+		transform: rotate(20deg);
+	}
+	.mitter.rotating > div {
+		animation: rotateCurtain 500ms infinite linear;
+	}
+	@keyframes rotateCurtain {
+		0% {
+			transform: rotateY(0deg);
+		}
+		100% {
+			transform: rotateY(360deg);
+		}
+	}
+	section:global(.start) .track {
 		visibility: hidden;
 		transform: translateX(-100%);
 	}
-	section:global(.dirty) img,
-	section:global(.cleaning) img,
-	section:global(.drying) img {
-		visibility: visible;
+	section:global(.dirty) .track,
+	section:global(.cleaning) .track,
+	section:global(.drying) .track {
 		transform: translateX(0);
+		visibility: visible;
 	}
-	section:global(.done) img {
+	section:global(.done) .track {
 		transform: translateX(100%);
 	}
-	img {
+	.track {
 		transition: transform 1000ms;
 	}
 </style>
