7f5cbd45ba145742a4128d60f5acd4af1953a6c0
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-02-19 03:15:14 +0100

    chore: Add another car
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/car-wash/play/+page.server.ts
+++ src/routes/(site)/specials/car-wash/play/+page.server.ts
@@ -1,11 +1,9 @@
 import type { PageServerLoad } from './$types';
 import car1 from '../_assets/car1.svg?raw';
 import car2 from '../_assets/car2.svg?raw';
+import car3 from '../_assets/car3.svg?raw';
 
 export const load: PageServerLoad = async () => {
-	// const imageFiles = await import.meta.glob('../_assets/car1.svg', { eager: true });
-	// const images = Object.values(imageFiles).map((module) => module.default);
-	const images = [car1, car2];
-	// const images = [car1];
+	const images = [car1, car2, car3];
 	return { images };
 };
