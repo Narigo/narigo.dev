8e891343b47aed8d7cc9fc15d6529c9c93863a31
Narigo <Narigo@users.noreply.github.com>
2024-02-19 02:15:45 +0000

    Deploying to gh-pages from @ Narigo/narigo.dev@7f5cbd45ba145742a4128d60f5acd4af1953a6c0 🚀

---
+++ _app/immutable/nodes/67.D5-hDz6k.js
@@ -0,0 +1,45 @@
+import{s as $,b as x,j as h,k as f,i as m,t as _,h as b}from"../chunks/scheduler.C0BfHF-J.js";import{S as z,i as k,c as s,b as p,m as c,t as l,a as u,d}from"../chunks/index.LRqrGZ80.js";import{N as v}from"../chunks/Narigo.DG2UikyY.js";import{P as y,a as A}from"../chunks/PuzzlePageLayout.DmvoZ6HP.js";function w(r){let t;return{c(){t=_(`Lots of gradients! 100% again! Finally! Yes! This time, using background positioning and
+		background sizes to get the correct result.`)},l(n){t=b(n,`Lots of gradients! 100% again! Finally! Yes! This time, using background positioning and
+		background sizes to get the correct result.`)},m(n,e){f(n,t,e)},d(n){n&&m(t)}}}function P(r){let t,n,e,i;return t=new A({props:{title:"Puzzle 73 solution",solution:r[0]}}),e=new v({props:{$$slots:{default:[w]},$$scope:{ctx:r}}}),{c(){s(t.$$.fragment),n=x(),s(e.$$.fragment)},l(a){p(t.$$.fragment,a),n=h(a),p(e.$$.fragment,a)},m(a,o){c(t,a,o),f(a,n,o),c(e,a,o),i=!0},p(a,o){const g={};o&2&&(g.$$scope={dirty:o,ctx:a}),e.$set(g)},i(a){i||(l(t.$$.fragment,a),l(e.$$.fragment,a),i=!0)},o(a){u(t.$$.fragment,a),u(e.$$.fragment,a),i=!1},d(a){a&&m(n),d(t,a),d(e,a)}}}function C(r){let t,n;return t=new y({props:{title:"Happy Tiger",$$slots:{default:[P]},$$scope:{ctx:r}}}),{c(){s(t.$$.fragment)},l(e){p(t.$$.fragment,e)},m(e,i){c(t,e,i),n=!0},p(e,[i]){const a={};i&2&&(a.$$scope={dirty:i,ctx:e}),t.$set(a)},i(e){n||(l(t.$$.fragment,e),n=!0)},o(e){u(t.$$.fragment,e),n=!1},d(e){d(t,e)}}}function F(r){return[`<div e></div><div h></div><div m></div><div n></div>
+<style>
+  body {
+    background: #F3AC3C;
+    display:grid;
+    place-items:center;
+  }
+  div {
+    position:absolute;
+  }
+  [e] {
+    top:65;
+    background:
+      radial-gradient(circle at 30px 50%,#1A4341 20px,#998235 0 30px,#0000 0) 0 0 / 110px;
+    width:170;
+    height:60;
+  }
+  [h] {
+    background:
+      radial-gradient(circle at 35px 70px, #1A4341 10px, #0000 0),
+      radial-gradient(circle at 115px 70px, #1A4341 10px, #0000 0),
+      conic-gradient(from -45deg at 50% 25px, #1A4341 90deg, #998235 0);
+    border-radius: 75px 75px 60px 60px;
+    width:150;
+    height:150;
+  }
+  [m] {
+    top:170;
+    width:100;
+    height:40;
+    border-radius:25px 25px 60px 60px;
+    background:
+      radial-gradient(circle at 50% 0, #fff 10px, #1A4341 0 20px, #0000 0) -5px 10px / 80% 50% no-repeat,
+      radial-gradient(circle at 50% 0, #fff 10px, #1A4341 0 20px, #0000 0) 25px 10px / 80% 50% no-repeat,
+      #fff;
+  }
+  [n] {
+    width:10;
+    height:20;
+    background:#1A4341;
+    top:160;
+  }
+</style>`]}class Y extends z{constructor(t){super(),k(this,t,F,C,$,{})}}export{Y as component};
