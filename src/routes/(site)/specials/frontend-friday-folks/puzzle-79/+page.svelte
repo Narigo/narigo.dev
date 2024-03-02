e18c81a2b2c80b33df0afec3405c3df236e332c8
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-03-02 22:02:27 +0100

    feat: Add puzzle 79
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-79/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Code from '$lib/common/Code.svelte';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Tambourine">
+	<Puzzle title="Puzzle 79 solution" {solution} />
+	<Narigo>
+		Sometimes, repeating divs and moving them around seems to be the most simple solution. There may
+		be a background repeat solution for the "jingles" (or "zils" as we learned from ChatGPT).
+	</Narigo>
+</PuzzlePageLayout>
