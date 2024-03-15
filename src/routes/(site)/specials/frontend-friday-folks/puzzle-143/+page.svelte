d0a6481e229bdfa593569e23bce6a278f85879a2
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-03-15 19:21:44 +0100

    chore: Prepare puzzle 143
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-143/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Simply Circle" {solution}>
+	<Narigo>
+		Due to time zone change, there was a puzzle selected out of order. Looks like an easy one, but
+		pretty hard to get to little amount of characters!
+	</Narigo>
+</PuzzlePageLayout>
