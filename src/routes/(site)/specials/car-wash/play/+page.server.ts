aad30437dc91e11308f942984aa323815f485fb2
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-03-28 17:40:08 +0100

    chore: Add all other cars again
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/car-wash/play/+page.server.ts
+++ src/routes/(site)/specials/car-wash/play/+page.server.ts
@@ -1,10 +1,10 @@
 import type { PageServerLoad } from './$types';
-// import car1 from '../_assets/car1.svg?raw';
-// import car2 from '../_assets/car2.svg?raw';
-// import car3 from '../_assets/car3.svg?raw';
+import car1 from '../_assets/car1.svg?raw';
+import car2 from '../_assets/car2.svg?raw';
+import car3 from '../_assets/car3.svg?raw';
 import car4 from '../_assets/car4.svg?raw';
 
 export const load: PageServerLoad = async () => {
-	const images = [car4];
+	const images = [car1, car2, car3, car4];
 	return { images };
 };
