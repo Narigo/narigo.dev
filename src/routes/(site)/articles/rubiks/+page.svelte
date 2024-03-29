16d454655948e41c82e237ae30f615b45fca403c
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-03-29 23:04:07 +0100

    chore: Add note for myself
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/articles/rubiks/+page.svelte
+++ src/routes/(site)/articles/rubiks/+page.svelte
@@ -1,7 +1,10 @@
 <script>
-	import PageLayout from "$lib/common/PageLayout/PageLayout.svelte";
-
+	import CodeBlock from '$lib/common/CodeBlock.svelte';
+	import PageLayout from '$lib/common/PageLayout/PageLayout.svelte';
 </script>
+
 <PageLayout>
-    Need to write about this 4x4...
+	<h1>Rubiks 4x4</h1>
+	<p>I need some place where I can put the note for the "degde" parity.</p>
+	<CodeBlock code="r U2 r U2 r' U2 r U2 l' U2 r U2 r' U2 l r2 U2 r'" />
 </PageLayout>
