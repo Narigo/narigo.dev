7f770e410b967b904c7aa6180077fb0dd4faaf06
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-03-30 07:15:11 +0100

    chore: Add intro text for article
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/articles/rubiks/+page.svelte
+++ src/routes/(site)/articles/rubiks/+page.svelte
@@ -4,7 +4,18 @@
 </script>
 
 <PageLayout>
-	<h1>Rubiks 4x4</h1>
+	<h1>Rubiks Cubes</h1>
+	<p>
+		I like solving these cubes and it always takes a while until I can do all the necessary
+		algorithms. In the past, I've seen that I tend to forget some key algorithms when I don't solve
+		the cubes regularly. Especially some parity situations are hard to remember.
+	</p>
+	<p>
+		The way I selected the algorithms were mostly for simplicity. This page won't help you
+		speed-solving cubes, but if you just want a generic way to solve the cubes, this might be for
+		you. There are a lot of solutions to a lot of situations - I will try to show the ones I use.
+	</p>
+	<h2>Rubiks 4x4</h2>
 	<p>I need some place where I can put the note for the "degde" parity.</p>
 	<CodeBlock code="r U2 r U2 r' U2 r U2 l' U2 r U2 r' U2 l r2 U2 r'" />
 </PageLayout>
