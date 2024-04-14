2c4fb987a528cf50ee8e27556e95c24b31d15ba4
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-04-14 22:15:35 +0200

    chore: Prepare puzzle 81
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-81/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Code from '$lib/common/Code.svelte';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Odoo" {solution}>
+	<Narigo>I missed this one and I still need to solve this.</Narigo>
+</PuzzlePageLayout>
