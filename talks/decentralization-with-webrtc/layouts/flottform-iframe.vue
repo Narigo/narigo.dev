c5450605714c0ceccbbf5f3e59e82fc15daffec2
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-04-25 23:46:29 +0200

    chore: Copy long talk version
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ talks/decentralization-with-webrtc/layouts/flottform-iframe.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="grid grid-cols-4 h-full w-full auto-rows-fr">
+		<div class="slidev-layout default">
+			<slot />
+		</div>
+		<div class="grid-col-span-3 h-full w-full">
+			<iframe class="h-full w-full" width="900" src="https://demo.flottform.io" title="Flottform demo"></iframe>
+		</div>
+	</div>
+</template>
+
+<style></style>
