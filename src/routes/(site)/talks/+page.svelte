b4459c4977d51dedd760c4762a5faf4c57cfb570
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-05-05 17:25:16 +0200

    chore: Add small descriptions to the talks
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/talks/+page.svelte
+++ src/routes/(site)/talks/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { base } from '$app/paths';
 	import PageLayout from '$lib/common/PageLayout/PageLayout.svelte';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
 </script>
 
 <PageLayout>
@@ -13,39 +14,57 @@
 		There are a few older ones that I didn't have anymore. If you're looking for one of them, please
 		let me know.
 	</p>
-	<ul>
+	<ul class="grid gap-4">
 		<li>
-			<a href="{base}/talks/static-pages-with-dynamic-content" rel="external"
-				>Static pages with "dynamic" content</a
-			>
-			-
-			<a
-				href="https://www.meetup.com/de-DE/frankenjs/events/289645924/"
-				rel="external noopener noreferrer">FrankenJS Meetup 2022-12-07</a
-			>
+			<div>
+				<a href="{base}/talks/static-pages-with-dynamic-content" rel="external"
+					>Static pages with "dynamic" content</a
+				>
+				-
+				<a
+					href="https://www.meetup.com/frankenjs/events/289645924/"
+					rel="external noopener noreferrer">FrankenJS Meetup 2022-12-07</a
+				>
+			</div>
+			<Narigo>
+				<p>My first time at FrankenJS and first time speaking since a long time!</p>
+			</Narigo>
 		</li>
 		<li>
-			<a href="{base}/talks/webrtc-use-cases" rel="external"
-				>Beyond Calls - Inspiring Use-Cases for WebRTC</a
-			>
-			-
-			<a
-				href="https://www.meetup.com/de-DE/munichjs-user-group/events/299867927/"
-				rel="external noopener noreferrer">MunichJS Meetup 2024-04-18</a
-			>
+			<div>
+				<a href="{base}/talks/webrtc-use-cases" rel="external"
+					>Beyond Calls - Inspiring Use-Cases for WebRTC</a
+				>
+				-
+				<a
+					href="https://www.meetup.com/de-DE/munichjs-user-group/events/299867927/"
+					rel="external noopener noreferrer">MunichJS Meetup 2024-04-18</a
+				>
+			</div>
+			<Narigo>
+				<p>
+					First time speaking about Flottform.io as one of the showcased use-cases. We are always
+					looking for people to test and integrate it.
+				</p>
+			</Narigo>
 		</li>
 		<li>
-			<a href="{base}/talks/webrtc-use-cases" rel="external"
-				>Decentralization with WebRTC - Inspiring Use-Cases</a
-			>
-			-
-			<a href="https://www.youtube.com/watch?v=pzLXQYZpOPU" rel="external noopener noreferrer"
-				>VirtualCoffee.io Lightning Talks April 2024</a
-			>
-			(<a
-				href="https://www.youtube.com/watch?v=pzLXQYZpOPU&t=1528s"
-				rel="external noopener noreferrer">video of my talk</a
-			>)
+			<div>
+				<a href="{base}/talks/webrtc-use-cases" rel="external"
+					>Decentralization with WebRTC - Inspiring Use-Cases</a
+				>
+				-
+				<a href="https://www.youtube.com/watch?v=pzLXQYZpOPU" rel="external noopener noreferrer"
+					>VirtualCoffee.io Lightning Talks April 2024</a
+				>
+				(<a
+					href="https://www.youtube.com/watch?v=pzLXQYZpOPU&t=1528s"
+					rel="external noopener noreferrer">video of my talk</a
+				>)
+			</div>
+			<Narigo>
+				<p>This was a shorter version of the one held at MunichJS Meetup.</p>
+			</Narigo>
 		</li>
 	</ul>
 </PageLayout>
