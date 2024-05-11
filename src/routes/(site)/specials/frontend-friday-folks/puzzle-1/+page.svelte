b215964a13f9507710f97b1b2dafe843397319c0
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-05-12 01:41:24 +0200

    chore: Add solution for puzzle 1
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-1/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Code from '$lib/common/Code.svelte';
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<style>*>*{background:linear-gradient(#b5e0ba,#b5e0ba)0 0/50%200px no-repeat,#5d3a3a`;
+</script>
+
+<PuzzlePageLayout title="Simply Square" {solution}>
+	<Narigo
+		>We haven't done this puzzle in the Frontend Friday folks, but I kind of like how it could be
+		done...</Narigo
+	>
+</PuzzlePageLayout>
