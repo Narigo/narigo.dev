91a7077044f42202de7987a507b9b6640b02334c
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-05-13 00:02:59 +0200

    chore: Add smallest solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/frontend-friday-folks/puzzle-1/+page.svelte
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-1/+page.svelte
@@ -1,14 +1,18 @@
 <script>
-	import Code from '$lib/common/Code.svelte';
 	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
 	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
 
-	const solution = `<style>*>*{background:linear-gradient(#b5e0ba,#b5e0ba)0 0/50%200px no-repeat,#5d3a3a`;
+	const solution = `<style>*>*{background:conic-gradient(#b5e0ba,#b5e0ba)0 0/50%212q no-repeat,#5d3a3a`;
+	const smallestSolution = `<style>*>*{height:384;background:conic-gradient(#5d3a3a 75%,#b5e0ba 0`;
 </script>
 
-<PuzzlePageLayout title="Simply Square" {solution}>
+<PuzzlePageLayout title="Simply Square">
+	<Puzzle title="Almost Megs solution" {solution} />
 	<Narigo
 		>We haven't done this puzzle in the Frontend Friday folks, but I kind of like how it could be
-		done...</Narigo
+		done... Looks like Meg knows how to save a character here though...!</Narigo
 	>
+	<Puzzle title="Smallest solution" solution={smallestSolution} />
+	<Narigo>Finally got a smaller solution!</Narigo>
 </PuzzlePageLayout>
