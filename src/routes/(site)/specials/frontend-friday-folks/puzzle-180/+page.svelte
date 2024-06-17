41391f97d1d538a2b56709c7387b0a3957cdba1c
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-06-17 23:08:54 +0200

    chore: Add hint how to find the best solutions
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/frontend-friday-folks/puzzle-180/+page.svelte
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-180/+page.svelte
@@ -9,10 +9,17 @@
 <PuzzlePageLayout title="Kuwait">
 	<Puzzle title="Puzzle 180 solution" {solution} />
 	<Narigo>
-		The second session of the CSS Battles at JSCraftCamp 2024 started with this puzzle. There were
-		multiple ideas what this could be: Four divs for each color. One background with linear gradient
-		and a black div on top. One div with different border colors. The last one seemed to be the
-		simplest of them and we used this to quickly create it. We looked a bit into code golfing but we
-		could see that the best solution seemed to use some other idea.
+		<p>
+			The second session of the CSS Battles at JSCraftCamp 2024 started with this puzzle. There were
+			multiple ideas what this could be: Four divs for each color. One background with linear
+			gradient and a black div on top. One div with different border colors. The last one seemed to
+			be the simplest of them and we used this to quickly create it. We looked a bit into code
+			golfing but we could see that the best solution seemed to use some other idea.
+		</p>
+		<p>
+			We didn't bother googling it, but since I got asked, I shared how I found out about better
+			solutions usually: Check the leaderboard, google for the names of the first places, check
+			their GitHub or websites whether they share their solutions somewhere.
+		</p>
 	</Narigo>
 </PuzzlePageLayout>
