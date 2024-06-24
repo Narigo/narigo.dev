11a9c2a7bf5580aed37d0333f960ad94112aac0c
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-06-24 03:19:57 +0200

    chore: Add jscc recap
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/articles/jscc24/+page.svelte
@@ -0,0 +1,91 @@
+<script>
+	import ArticleLayout from '$lib/common/PageLayout/ArticleLayout.svelte';
+</script>
+
+<ArticleLayout title="JavaScript CraftCamp 2024 - #JSCC24" lastUpdateOn="2024-06-24">
+	<p>
+		It's done! The JavaScript CraftCamp 2024 is over! Due to a lot of other commitments before the
+		event, it has been really exhausting for me this time.
+	</p>
+	<p>
+		But before jumping into anything here: Thanks a ton for all the participants of this year's
+		JSCC! I have never seen such a great spirit towards learning and crafting elsewhere. So glad
+		this is working out.
+	</p>
+	<p>
+		There wasn't really much negative feeback this time and most of the people I asked could only
+		share good memories. We definitely have a few things we can improve next time (especially a
+		longer wrap-up), but overall, I'd say the JSCraftCamp has been a huge success.
+	</p>
+	<p>
+		There is always some trouble before the event to get everything done on schedule. I think we're
+		always underestimating the time it takes for t-shirts: Find a sponsor, find a company to handle
+		printing, check in with the sponsor, whether the price is still okay after getting offers for
+		printing and after getting the sponsors approval: Find another company quickly because the
+		printing company found out that they don't have the bandwidth to print it on time anymore.
+	</p>
+	<p>
+		Really, we all have to thank Sina for handling the t-shirt situation perfectly and getting not
+		only the shirts but a lot more logistics organized prior to the event, despite any issues
+		popping up! Also, she stayed around quite a while and came in early just that the participants
+		could stay as long as possible. And she has over an hour to commute...!
+	</p>
+	<p>
+		Another big thanks to the Maiborn-Wolff team who helped us a lot during the event itself. It was
+		extremely helpful to have a couple of people around who could help with the building, giving
+		directions and jumping in with cables and other things when needed.
+	</p>
+	<p>
+		During the event itself, I'm trying my best to be a participant myself and hope not to have to
+		organize too much on the fly. It's great to have people on the orga team like Bernd who can
+		easily switch from participant to (hardcore) orga mode: He got people to go out with him to buy
+		ice cream for everybody on both days, be around when food or drinks get delivered, making
+		everybody aware of the situations and even getting us to find a just-in-time sponsor! Really
+		glad to have him!
+	</p>
+	<h2>Improvements</h2>
+	<p>
+		We had a bit of a mix-up regarding the rooms and I feel bad about it since some sponsors were a
+		little bit less exposed due to the naming of the rooms.
+	</p>
+	<p>
+		Another thing we did was reserving sponsors up to 5 seats. We didn't communicate well enough to
+		have them reserve the seats through pull requests as well. In the end, it all went okay, but it
+		was definitely something that could have caused bigger issues.
+	</p>
+	<p>
+		While I love having lots of sponsors and I'm happy for every company that sponsors any amount of
+		money, I feel that sponsors who invest more money should have at least some incentive to do
+		more. This could be more seats, more exposure (room names come to mind) or other things. We
+		don't want to have one big sponsor ruling over everything though. It should still be a lot more
+		community members than seats from sponsors.
+	</p>
+	<p>
+		There will probably be more points to add after we've done the "official" retro with the orga
+		team.
+	</p>
+	<h2>Personal Conclusion</h2>
+	<p>
+		For me, organizing the JSCraftCamp feels like a great way to give back to the community that
+		helped me out so much already. I have found friends and business partners through these meetups
+		that started this and it feels great when people say thanks for helping them.
+	</p>
+	<p>
+		It's good to hear from participants that they really like this event and they want to have this
+		more often. It seems to be inspiring and motivating for a lot of people here and I would
+		definitely like to see more events like this. Maybe having this in a smaller scale throughout
+		the year would be cool.
+	</p>
+	<p>
+		Still, organizing an event like the JSCC, getting the community involved, finding sponsors and
+		keeping all of them hyped sometimes felt like a second job to me. As my time constraints are
+		much more limited than they have been before becoming a parent, I'm unsure whether I can
+		continue delivering a good job on the orga front. I'd love to see more people stepping up and
+		trying to help us organize the whole thing. I can't even tell yet whether I have enough time
+		next year.
+	</p>
+	<p>
+		Let's see whether I can at least do some online work for the community...! I'll start with some
+		CSSBattle sessions and maybe someone will resurrect the Crafters vs Codewars at some point...?
+	</p>
+</ArticleLayout>
