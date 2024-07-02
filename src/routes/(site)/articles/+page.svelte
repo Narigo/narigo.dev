f89417ba9ba07cae0fb46fafbf075a686fa2d26a
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-07-02 15:43:34 +0200

    chore: Use bullets for index
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/articles/+page.svelte
+++ src/routes/(site)/articles/+page.svelte
@@ -7,18 +7,16 @@
 	<h2>Articles</h2>
 	<ul>
 		<li>
-			You can read <a href="{base}/articles/jscc24">my personal retrospective of JSCraftCamp 2024</a
-			>.
+			<a href="{base}/articles/jscc24">My personal retrospective of JSCraftCamp 2024</a>
 		</li>
 		<li>
-			I have written <a href="{base}/articles/jscc23">some thoughts about JSCraftCamp 2023</a>.
+			<a href="{base}/articles/jscc23">Some thoughts about JSCraftCamp 2023</a>
 		</li>
 		<li>
-			Read about <a href="{base}/articles/keepass-diff">the keepass-diff CLI tool written in Rust</a
-			>.
+			<a href="{base}/articles/keepass-diff">The keepass-diff CLI tool, written in Rust</a>
 		</li>
 		<li>
-			Read about <a href="{base}/articles/pocuter-one">my experience with the pocuter one</a>.
+			<a href="{base}/articles/pocuter-one">My experiences with the pocuter one</a>
 		</li>
 	</ul>
 </PageLayout>
