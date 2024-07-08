7009fa1d3a96fdc31bcdf7726a4765b398363eba
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-07-09 00:03:33 +0200

    chore: Add todos
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/articles/rubiks/+page.svelte
+++ src/routes/(site)/articles/rubiks/+page.svelte
@@ -3,7 +3,7 @@
 	import ArticleLayout from '$lib/common/PageLayout/ArticleLayout.svelte';
 </script>
 
-<ArticleLayout title="Rubiks Cubes" lastUpdateOn="2024-07-03">
+<ArticleLayout title="Rubiks Cubes" lastUpdateOn="2024-07-09">
 	<p class="bg-opacity-40 bg-red-200 border border-solid border-red-300 p-4">
 		This is still a work in progress. I had to save two algorithms so I won't forget them.
 	</p>
@@ -27,10 +27,10 @@
 		<li><a href="#cross-top-color">Cross with the top color</a></li>
 		<li><a href="#solve-top-color-and-corners">Solve top color and corners</a></li>
 		<li><a href="#sides-to-get-the-first-two-rows">Sides to get the first two rows</a></li>
-		<li>Bottom color cross</li>
-		<li>Bottom color face complete</li>
-		<li>Bottom corners solved</li>
-		<li>Bottom sides solved / complete cube</li>
+		<li><a href="bottom-color-cross">Bottom color cross</a></li>
+		<li><a href="bottom-color-face-complete">Bottom color face complete</a></li>
+		<li><a href="bottom-corners-solved">Bottom corners solved</a></li>
+		<li><a href="bottom-sides-solved-complete-cube">Bottom sides solved / complete cube</a></li>
 	</ol>
 	<h4 id="cross-top-color">Cross with the top color</h4>
 	<p>
@@ -98,12 +98,25 @@
 		it should be started from: lining up with a center piece.
 	</p>
 
+	<h4 id="bottom-color-cross">Bottom color cross</h4>
+	<p>TODO: Add necessary steps</p>
+
+	<h4 id="bottom-color-face-complete">Bottom color face complete</h4>
+	<p>TODO: Add necessary steps</p>
+
+	<h4 id="bottom-corners-solved">Bottom corners solved</h4>
+	<p>TODO: Add necessary steps</p>
+
+	<h4 id="bottom-sides-solved-complete-cube">Bottom sides solved / complete cube</h4>
+	<p>TODO: Add necessary steps</p>
+
 	<h2>Rubiks 4x4</h2>
 	<p>
 		I like the genericness of the 4x4 cube as you can play it like a 3x3 and a 2x2, depending on how
 		you scramble it. There are two extra steps and two parity situations, which in turn made me
 		learn four more algorithms vs the 3x3.
 	</p>
+	<h3>Solution steps</h3>
 	<ol>
 		<li><a href="#correct-center-pieces">Correct center pieces</a></li>
 		<li><a href="#correct-pairs-on-every-side">Correct pairs on every side</a></li>
@@ -115,7 +128,7 @@
 		<li>Solving bottom sides</li>
 		<li>Parity situation "sides mirrored"</li>
 	</ol>
-	<h3 id="correct-center-pieces">Correct center pieces</h3>
+	<h4 id="correct-center-pieces">Correct center pieces</h4>
 	<p>
 		White is top, yellow is bottom, on the sides I remember red, then blue, then orange, then green
 		(RBOG). Since the edges and corners are not really to necessary to look at yet, getting a color
@@ -124,7 +137,7 @@
 		correctly colored piece from the current center, the center can be turned before moving the
 		color from somewhere else into it.
 	</p>
-	<h3 id="correct-pairs-on-every-side">Correct pairs on every side</h3>
+	<h4 id="correct-pairs-on-every-side">Correct pairs on every side</h4>
 	<p>
 		This makes all edges on the sides be of the same colors. As long as there are still broken
 		sides, the algorithm is to move the same colors on the same face but in different directions.
@@ -146,7 +159,7 @@
 		broken parts, they can be moved in a way to solve all three.
 	</p>
 	<p>TODO: Write down the algorithm to do three at the same time 😅</p>
-	<h3 id="solving-the-top-3-layers-like-a-3x3">Solving the top 3 layers like a 3x3</h3>
+	<h4 id="solving-the-top-3-layers-like-a-3x3">Solving the top 3 layers like a 3x3</h4>
 	<p>
 		There are a few steps now that feel like solving the 3x3 again. In essence, the top color gets a
 		cross with the center pieces on the side, the top face corners get solved and then the side
