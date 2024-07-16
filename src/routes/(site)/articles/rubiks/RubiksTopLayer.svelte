966bf48c35b53177076feb279044e188482f0da2
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-07-17 01:01:57 +0200

    chore: Improve styling of css images
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/articles/rubiks/RubiksTopLayer.svelte
+++ src/routes/(site)/articles/rubiks/RubiksTopLayer.svelte
@@ -18,59 +18,65 @@
 	};
 </script>
 
-<div class="grid grid-cols-3">
-
+<div class="grid grid-cols-3 aspect-square w-full [font-size:clamp(1px,calc(calc(-360px*5/504+1px)_+_calc(5/504*100vw)),6px)]">
 	<div></div>
-	<div class="bg-black grid gap-2 rotate-180">
-		{#each back as [c1, c2, c3]}
-			<div class="grid grid-cols-3 gap-2">
-				<div class="rounded aspect-square {f[c1]}"></div>
-				<div class="rounded aspect-square {f[c2]}"></div>
-				<div class="rounded aspect-square {f[c3]}"></div>
-			</div>
-		{/each}
-	</div>
+	<div></div>
 	<div></div>
 
-	<div class="bg-black grid gap-2 rotate-90">
-		{#each left as [c1, c2, c3]}
-			<div class="grid grid-cols-3 gap-2">
-				<div class="rounded aspect-square {f[c1]}"></div>
-				<div class="rounded aspect-square {f[c2]}"></div>
-				<div class="rounded aspect-square {f[c3]}"></div>
-			</div>
-		{/each}
-	</div>
+	<div></div>
+	<div class="relative [perspective-origin:50%_50%] [perspective:300px]">
+		<div class="shadow-black shadow-2xl absolute inset-0 bg-black grid gap-[2em] p-[1em] [transform:rotate(180deg)_translateY(50%)_rotateX(-30deg)_translateY(50%)] z-10">
+			{#each back as [c1, c2, c3]}
+				<div class="grid grid-cols-3 gap-[2em]">
+					<div class="rounded aspect-square {f[c1]}"></div>
+					<div class="rounded aspect-square {f[c2]}"></div>
+					<div class="rounded aspect-square {f[c3]}"></div>
+				</div>
+			{/each}
+		</div>
 
-	<div class="bg-black grid gap-2">
-		{#each top as [c1, c2, c3]}
-			<div class="grid grid-cols-3 gap-2">
-				<div class="rounded aspect-square {f[c1]}"></div>
-				<div class="rounded aspect-square {f[c2]}"></div>
-				<div class="rounded aspect-square {f[c3]}"></div>
-			</div>
-		{/each}
-	</div>
+		<div class="shadow-black shadow-2xl absolute inset-0 bg-black grid gap-[2em] p-[1em] [transform:rotate(90deg)_translateY(50%)_rotateX(-30deg)_translateY(50%)]">
+			{#each left as [c1, c2, c3]}
+				<div class="grid grid-cols-3 gap-[2em]">
+					<div class="rounded aspect-square {f[c1]}"></div>
+					<div class="rounded aspect-square {f[c2]}"></div>
+					<div class="rounded aspect-square {f[c3]}"></div>
+				</div>
+			{/each}
+		</div>
 
-	<div class="bg-black grid gap-2 -rotate-90">
-		{#each right as [c1, c2, c3]}
-			<div class="grid grid-cols-3 gap-2">
-				<div class="rounded aspect-square {f[c1]}"></div>
-				<div class="rounded aspect-square {f[c2]}"></div>
-				<div class="rounded aspect-square {f[c3]}"></div>
-			</div>
-		{/each}
+		<div class="absolute inset-0 bg-black grid gap-[2em] p-[1em]">
+			{#each top as [c1, c2, c3]}
+				<div class="grid grid-cols-3 gap-[2em]">
+					<div class="rounded aspect-square {f[c1]}"></div>
+					<div class="rounded aspect-square {f[c2]}"></div>
+					<div class="rounded aspect-square {f[c3]}"></div>
+				</div>
+			{/each}
+		</div>
+
+		<div class="shadow-black shadow-2xl absolute inset-0 bg-black grid gap-[2em] p-[1em] [transform:rotate(-90deg)_translateY(50%)_rotateX(-30deg)_translateY(50%)]">
+			{#each right as [c1, c2, c3]}
+				<div class="grid grid-cols-3 gap-[2em]">
+					<div class="rounded aspect-square {f[c1]}"></div>
+					<div class="rounded aspect-square {f[c2]}"></div>
+					<div class="rounded aspect-square {f[c3]}"></div>
+				</div>
+			{/each}
+		</div>
+
+		<div class="shadow-black shadow-2xl absolute inset-0 bg-black grid gap-[2em] p-[1em] [transform:translateY(50%)_rotateX(-30deg)_translateY(50%)]">
+			{#each front as [c1, c2, c3]}
+				<div class="grid grid-cols-3 gap-[2em]">
+					<div class="rounded aspect-square {f[c1]}"></div>
+					<div class="rounded aspect-square {f[c2]}"></div>
+					<div class="rounded aspect-square {f[c3]}"></div>
+				</div>
+			{/each}
+		</div>
 	</div>
 
 	<div></div>
-	<div class="bg-black grid gap-2">
-		{#each front as [c1, c2, c3]}
-			<div class="grid grid-cols-3 gap-2">
-				<div class="rounded aspect-square {f[c1]}"></div>
-				<div class="rounded aspect-square {f[c2]}"></div>
-				<div class="rounded aspect-square {f[c3]}"></div>
-			</div>
-		{/each}
-	</div>
+	<div></div>
 	<div></div>
 </div>
