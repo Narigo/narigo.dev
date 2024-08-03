54af4c2d9d65768f6080f24b89958bcf2a23346d
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-08-04 00:22:06 +0200

    chore: Add second solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/frontend-friday-folks/puzzle-88/+page.svelte
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-88/+page.svelte
@@ -3,7 +3,7 @@
 	import Puzzle from '../Puzzle.svelte';
 	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
 
-	const firstSolution = `<div></div>
+	const solution1 = `<div></div>
 <div b></div>
 <style>
   &{background:#D25B70}
@@ -34,14 +34,42 @@
     rotate:180deg;
   }
 </style>`;
+	const solution2 = `<div></div>
+<div b></div>
+<style>
+  &{background:#F7F3DA}
+  div {
+    position:fixed;
+    inset: 110px 50% 0 0;
+    border-radius: 0 10px;
+    background:#D25B70;
+  }
+  div:before {
+    content:'';
+    position:absolute;
+    inset:40px 0 -20px 0;
+    border-radius:0 20px;
+    background:#F7F3DA;
+  }
+  [b]{
+    transform-origin:100% 40px;
+    rotate:180deg;
+  }
+</style>`;
 </script>
 
 <PuzzlePageLayout title="Tight Corner">
-	<Puzzle title="First solution" solution={firstSolution} />
+	<Puzzle title="First solution" solution={solution1} />
 	<Narigo>
 		The first idea we had - I wanted to finish it, even though B Suraj came in and had the fantastic
 		idea to create it in a different way by stacking two divs on top of each other. So this solution
 		is basically using pseudo elements to make use of the full width and go a bit over the end of
 		the div.
+	</Narigo>
+
+  <Puzzle title="Second solution" solution={solution2} />
+	<Narigo>
+		As mentioned above, the second one was a cleaner version based on the idea that B Suraj had. I
+		think this could be used to make a fun animation as well...! 🤔
 	</Narigo>
 </PuzzlePageLayout>
