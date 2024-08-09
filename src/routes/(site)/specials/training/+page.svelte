5c505d8ab815af5c6135292b8bafd957cc5ec30b
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-08-10 01:05:58 +0200

    feat: Add new page for workout I want to redo
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/training/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import PageLayout from '$lib/common/PageLayout/PageLayout.svelte';
+</script>
+
+<PageLayout>
+	<h2>Training</h2>
+	<p>
+		At some point, my dentist sent me a link to a workout that strengthens your back. I've been
+		doing these workouts myself but at some point, it didn't really work well with the iPad anymore
+		(screen turns off and sound doesn't play). So I'm trying to recreate this, to make it work on
+		all of my devices.
+	</p>
+	<p>The page started with some settings, to customize the workout for yourself.</p>
+</PageLayout>
