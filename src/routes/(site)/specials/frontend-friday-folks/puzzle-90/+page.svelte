5e4d794ed4b2224119e0948efe07ae67c0dd0b3e
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-09-20 22:57:38 +0200

    chore: Add puzzle 90 solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-90/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Code from '$lib/common/Code.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<style>
+  body {
+    --b:radial-gradient(1q,#998235 200px,#0000 0) 0 150px / 100% 400px;
+    background:
+      var(--b) no-repeat,
+      radial-gradient(1q,#1A4341 100px,#F3AC3C 0 125px,#0000 0),
+      var(--b),
+      #F3AC3C
+    ;
+  }
+</style>
+`;
+</script>
+
+<PuzzlePageLayout title="Eclipse">
+	<Puzzle title="Solution" {solution} />
+	<Narigo>
+		<p>
+			We started late, but we saw a couple of people joining later and learning about our CSS
+			tricks. <a href="https://www.twitch.tv/notmichaella" rel="external"
+				>Micha, who streamed solving CSS battles on Twitch</a
+			>
+			before, joined us this time. Find
+			<a href="https://github.com/orgs/Virtual-Coffee/discussions/1214" rel="external"
+				>our solutions on the VC discussion board</a
+			> as well!
+		</p>
+	</Narigo>
+</PuzzlePageLayout>
