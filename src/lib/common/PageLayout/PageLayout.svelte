ddebca780d5dbce83e4171868d57fe5e059e979c
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-09-24 00:08:30 +0200

    chore: Add meta tags for SEO
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/lib/common/PageLayout/PageLayout.svelte
+++ src/lib/common/PageLayout/PageLayout.svelte
@@ -1,7 +1,89 @@
-<script>
+<script lang="ts">
 	import { base } from '$app/paths';
 	import Navigation from '../navigation/Navigation.svelte';
+	import defaultPreviewImage from './preview-image.png';
+
+	export let title: string = 'Thoughts, Talks and Trials by Narigo';
+	export let description: string | undefined = undefined;
+	export let author: string | undefined = undefined;
+	export let publishDate: string | undefined = undefined;
+	export let og_title: string | undefined = undefined;
+	export let og_description: string | undefined = undefined;
+	export let og_image: string | undefined = undefined;
+	export let twitter_title: string | undefined = undefined;
+	export let twitter_description: string | undefined = undefined;
+	export let twitter_image: string | undefined = undefined;
+
+	const defaultAuthor = 'Jörn Bernhardt';
+	const defaultDescription =
+		'Welcome to the personal website of Jörn Bernhardt, where I share my thoughts, lessons learned, and experimental projects. Explore my talks and musings on technology, software development, and more.';
 </script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="title" content={title} />
+	{#if description}
+		<meta name="description" content={description} />
+	{:else}
+		<meta name="description" content={defaultDescription} />
+	{/if}
+	{#if og_title}
+		<meta property="og:title" content={og_title} />
+	{:else}
+		<meta property="og:title" content={title} />
+	{/if}
+	{#if og_description}
+		<meta property="og:description" content={og_description} />
+	{:else if description}
+		<meta property="og:description" content={description} />
+	{:else}
+		<meta name="og:description" content={defaultDescription} />
+	{/if}
+	{#if og_image}
+		<meta name="image" property="og:image" content={og_image} />
+	{:else}
+		<meta name="image" property="og:image" content={defaultPreviewImage} />
+	{/if}
+	{#if author}
+		<meta name="author" content={author} />
+	{:else}
+		<meta name="author" content={defaultAuthor} />
+	{/if}
+	{#if publishDate}
+		<meta
+			name="publish_date"
+			property="og:publish_date"
+			content={new Date(publishDate).toISOString()}
+		/>
+	{/if}
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@NarigoDF" />
+	<meta name="twitter:creator" content="@NarigoDF" />
+	<meta name="twitter:domain" content="https://narigo.dev/" />
+	{#if twitter_title}
+		<meta name="twitter:title" content={twitter_title} />
+	{:else if og_title}
+		<meta name="twitter:title" content={og_title} />
+	{:else}
+		<meta name="twitter:title" content={title} />
+	{/if}
+	{#if twitter_description}
+		<meta name="twitter:description" content={twitter_description} />
+	{:else if og_description}
+		<meta name="twitter:description" content={og_description} />
+	{:else if description}
+		<meta name="twitter:description" content={description} />
+	{:else}
+		<meta name="twitter:description" content={defaultDescription} />
+	{/if}
+	{#if twitter_image}
+		<meta name="twitter:image" content={twitter_image} />
+	{:else if og_image}
+		<meta name="twitter:image" content={og_image} />
+	{:else}
+		<meta name="twitter:image" content={defaultPreviewImage} />
+	{/if}
+</svelte:head>
 
 <div class="page font-primary">
 	<header>
@@ -18,8 +100,13 @@
 		<div class="flex flex-row flex-wrap gap-8 justify-between mx-auto max-w-[--max-page-width] p-4">
 			<div><a class="no-underline" href="{base}/imprint">ℹ️ Imprint</a></div>
 			<div>&copy; Jörn Bernhardt</div>
-			<div>Statically hosted on GitHub Pages.</div>
-			<div><a class="no-underline" href="https://github.com/Narigo/narigo.dev" rel="external">Source</a></div>
+			<div>
+				Statically hosted on GitHub Pages. <a
+					class="no-underline"
+					href="https://github.com/Narigo/narigo.dev"
+					rel="external">Source</a
+				>
+			</div>
 		</div>
 	</footer>
 </div>
