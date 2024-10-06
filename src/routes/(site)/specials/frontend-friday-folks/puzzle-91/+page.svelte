78b5f1b8548f1606eb0d05f635d93699f0503167
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-10-06 16:36:45 +0200

    chore: Add solution for puzzle 91
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-91/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Code from '$lib/common/Code.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<style>*&{
+  background:
+    linear-gradient(#6CB3A9 15px, #0000 0 25px, #6CB3A9 0 40px, #0000 0 50px, #6CB3A9 0 65px, #0000 0 75px, #6CB3A9 0 90px,#0000 0) 0 150px / 100% 50% no-repeat,
+    radial-gradient(1q,#F6DF96 100px, #0000 0),
+    linear-gradient(#D25B70 50%, #6CB3A9 0);
+}`;
+
+</script>
+
+<PuzzlePageLayout title="Reflection">
+	<Puzzle title="Solution" {solution} />
+	<Narigo>
+		<p>
+			The first solution was pretty straight forward, drawing red and blue, the sun and then a
+			gradient to add the reflection effect.
+		</p>
+	</Narigo>
+</PuzzlePageLayout>
