c5311e63faf805f262d6d99b8ce71d09fb0c63db
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-10-07 01:13:34 +0200

    chore: Add second solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/frontend-friday-folks/puzzle-91/+page.svelte
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-91/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import Narigo from '$lib/common/bubble/Narigo.svelte';
-	import Code from '$lib/common/Code.svelte';
 	import Puzzle from '../Puzzle.svelte';
 	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
 
@@ -11,6 +10,11 @@
     linear-gradient(#D25B70 50%, #6CB3A9 0);
 }`;
 
+	const solution2 = `<style>&{
+  background:
+    radial-gradient(1q at 50% 100%,#F6DF96 100px, #D25B70 0) 0 0 / 400px 150px no-repeat,
+    radial-gradient(1q,#0000 100px, #6CB3A9 0),
+    linear-gradient(#6CB3A9 15px, #F6DF96 0 25px)0 0/100%25px`;
 </script>
 
 <PuzzlePageLayout title="Reflection">
@@ -19,6 +23,14 @@
 		<p>
 			The first solution was pretty straight forward, drawing red and blue, the sun and then a
 			gradient to add the reflection effect.
+		</p>
+	</Narigo>
+	<Puzzle title="Solution 2" solution={solution2} />
+	<Narigo>
+		<p>
+			By making the reflection first, using a "transparent" sun and moving the red sun part as a
+			single, non-repeating background on top, this saved some characters. Thinking about
+			transparency and layers really allows a lot of options using gradients.
 		</p>
 	</Narigo>
 </PuzzlePageLayout>
