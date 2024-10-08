126c6ddbc76d7ca19405c3b2161085a4126fb263
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-10-08 23:04:06 +0200

    chore: Add form for training
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/training/+page.svelte
+++ src/routes/(site)/specials/training/+page.svelte
@@ -1,19 +1,57 @@
 <script lang="ts">
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
 	import PageLayout from '$lib/common/PageLayout/PageLayout.svelte';
 </script>
 
 <PageLayout>
 	<h2>Training</h2>
-	<p>
-		At some point, my dentist sent me a link to a workout that strengthens your back. I've been
-		doing these workouts myself but at some point, it didn't really work well with the iPad anymore
-		(screen turns off and sound doesn't play). So I'm trying to recreate this, to make it work on
-		all of my devices.
-	</p>
-	<p>
-		The page started with some settings, to customize the workout for yourself. When you press
-		start, it gives you a few seconds to see an image of someone doing the exercise and text
-		explaining what to do. After that, you should do the exercise for some seconds before it jumps
-		to the next one.
-	</p>
+	<Narigo>
+		<p>
+			At some point, my dentist sent me a link to a workout that strengthens your back. I've been
+			doing these workouts myself but at some point, it didn't really work well with the iPad
+			anymore (screen turns off and sound doesn't play). So I'm trying to recreate this, to make it
+			work on all of my devices.
+		</p>
+		<p>
+			The page started with some settings, to customize the workout for yourself. When you press
+			start, it gives you a few seconds to see an image of someone doing the exercise and text
+			explaining what to do. After that, you should do the exercise for some seconds before it jumps
+			to the next one.
+		</p>
+	</Narigo>
+	<form class="grid">
+		<p>Work in progress, you won't be able to submit yet!</p>
+		<h2>Select your training mode to start training</h2>
+		<label>
+			<span>Time per workout:</span>
+			<input
+				name="timePerWorkout"
+				type="number"
+				class="border-b-2 border-black text-center w-fit"
+				style="width:calc(1ch * 4)"
+				on:input={(e) => {
+					e.currentTarget.setAttribute(
+						'style',
+						`width:calc(1ch * ${(e.currentTarget.value.length || 1) + 2});`
+					);
+				}}
+				value="45"
+			/>
+			<span>seconds</span>
+		</label>
+		<p>Please select the type of the workout:</p>
+		<label>
+			<input name="typeOfWorkout" type="radio" value="short" />
+			<span>Short workout</span>
+		</label>
+		<label>
+			<input name="typeOfWorkout" type="radio" value="regular" checked />
+			<span>Regular workout</span>
+		</label>
+		<label>
+			<input name="typeOfWorkout" type="radio" value="long" />
+			<span>Long workout</span>
+		</label>
+		<button disabled>Start training</button>
+	</form>
 </PageLayout>
