0e1b0d6d7d48f4e82b83a15e7ccb1ecbc355c3d5
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-10-13 22:22:53 +0200

    chore: Prepare for puzzle 92
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-92/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="?">
+	<Puzzle title="Solution" {solution} />
+	<Narigo>
+		<p>
+			I couldn't make it this Friday, due to me being at a Hackathon. I'll try doing it by myself
+			this time then and check their solutions later!
+		</p>
+	</Narigo>
+</PuzzlePageLayout>
