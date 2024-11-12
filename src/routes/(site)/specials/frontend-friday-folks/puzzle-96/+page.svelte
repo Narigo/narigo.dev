dccb80ecd4a1ac1588a01e03a118e3177b56f1d5
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-11-13 00:52:17 +0100

    chore: Add puzzle 96
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-96/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<style>
+  &{
+    background:
+      radial-gradient(1q, transparent 65px, #243D83 0 85px, transparent) 0 0 / 100% 150px,
+      radial-gradient(1q, transparent 65px, #243D83 0 85px, transparent) 50px 0px / 150px 100%,
+      radial-gradient(1q, #6592CF 100px, #243D83 0)
+    ;
+  }
+</style>
+`;
+</script>
+
+<PuzzlePageLayout title="Mandala">
+	<Puzzle title="Solution" {solution} />
+	<Narigo>
+		<p>
+			This is a puzzle we did the hour before <a href="../puzzle-95">puzzle 95</a>, which I wanted
+			to use as example for different gradients. In here, we used gradients and repeating the
+			pattern to get to the desired result.
+		</p>
+	</Narigo>
+</PuzzlePageLayout>
