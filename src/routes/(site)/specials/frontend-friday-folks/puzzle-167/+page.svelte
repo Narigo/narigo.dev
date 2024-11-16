c0a8bcbc8947430382e04d41ac43bf371645c5b5
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-11-16 02:03:50 +0100

    chore: Add solution for puzzle 167
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-167/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div c></div>
+<div class="ellipse white"></div>
+<div class="ellipse green"></div>
+<div class="ellipse orange"></div>
+<style>
+  body {
+    background: #0D1335;
+  }
+  [c] {
+    background: #73C6EA;
+    border-radius: 100%;
+    height: 40px;
+    width: 40px;
+    position: absolute;
+    inset: 130px 180px;
+  }
+  .ellipse {
+    border-radius: 100%;
+    height: 90px;
+    width: 230px;
+    background: #1230;
+    position: absolute;
+    inset: 95px 75px;
+  }
+  .white {
+    border: 10px solid #FBFAE2;
+  }
+  .green {
+    border: 10px solid #4FA07B;
+    rotate: 120deg;
+  }
+  .orange {
+    border: 10px solid #DC6638;
+    rotate: 60deg;
+  }
+</style>`;
+</script>
+
+<PuzzlePageLayout title="React India">
+	<Puzzle title="Puzzle 167 solution" {solution} />
+	<Narigo
+		>An in-person session we did at the <a href="https://www.bayerwald-hackathon.de/" rel="external"
+			>B4Y3RW4LD Hackathon</a
+		>.</Narigo
+	>
+</PuzzlePageLayout>
