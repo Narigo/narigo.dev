92c6d6bf9b04a68c4bdb8c7b22e2d07723e9b264
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-11-19 22:53:18 +0100

    chore: Add gradient solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/frontend-friday-folks/puzzle-102/+page.svelte
+++ src/routes/(site)/specials/frontend-friday-folks/puzzle-102/+page.svelte
@@ -71,6 +71,80 @@
     >p{background:#fff;margin:0;width:15;height:20}
   }
 </style>`;
+
+	const solutionGradients = `<div f>
+<div h>
+  <div e l></div>
+  <div e r></div>
+</div>
+<div ts></div>
+</div>
+<style>
+  body{
+    background:#000;
+    display:grid;
+    place-content:center;
+  }
+  [e] {
+    background:#000;
+    position:absolute;
+    top:80;
+    left:29;
+    border-radius:50%;
+    rotate:150deg;
+    height:39;
+    width:50;
+  }
+  [l] {
+    left:90;
+    rotate:30deg;
+  }
+  [f] {
+    position:relative;
+    &:after,
+    &:before{
+      z-index:-1;
+      position:absolute;
+      left:-22;top:23;
+      content:'';
+      background:
+        radial-gradient(1q at 14q 136q,#fff 13q,#0000),
+        radial-gradient(1q at 26q 148q,#fff 13q,#0000),
+        linear-gradient(135deg,#000 32px,#0000 0 52px,#000 0)0 100q / 80px 80px no-repeat,
+        linear-gradient(45deg, #000 37px,#fff  0 67px,#000 0)0 100q / 80px 80px no-repeat,
+        radial-gradient(1q at 199q 14q,#fff 13q,#0000),
+        radial-gradient(1q at 211q 26q,#fff 13q,#0000),
+        linear-gradient(135deg,#000 25px,#0000 0 45px,#000 0)164q 10q / 80px 80px no-repeat,
+        linear-gradient(45deg, #000 52px,#fff  0 75px,#000 0)164q 10q / 80px 80px no-repeat,
+        radial-gradient(1q at 199q 148q,#fff 13q,#0000),
+        radial-gradient(1q at 211q 136q,#fff 13q,#0000),
+        linear-gradient(45deg, #000 46px,#0000 0 66px,#000 0)164q 100q / 80px 80px no-repeat,
+        linear-gradient(135deg,#000 31px,#fff  0 61px,#000 0)164q 100q / 80px 80px no-repeat,
+        radial-gradient(1q at 14q 26q,#fff 13q,#0000),
+        radial-gradient(1q at 26q 14q,#fff 13q,#0000),
+        linear-gradient(45deg, #000 46px,#0000 0 66px,#000 0)0 0 / 80px 80px no-repeat,
+        linear-gradient(135deg,#000 32px,#fff  0 52px,#000 0)0 0 / 80px 80px no-repeat,
+        #000
+      ;
+      height:300px;
+      width:300px;
+    }
+  }
+  [h] {
+    margin-top:40;
+    background:#fff;
+    border:10px solid #000;
+    border-radius:60px;
+    height:100;
+    width:150;
+  }
+  [ts] {
+    margin-left:57;
+    height:20;
+    width:57;
+    background:linear-gradient(90deg,#fff 15px,#000 0 20px,#fff 0 35px,#000 0 40px,#fff 0 55px,#000 0);
+  }
+</style>`;
 </script>
 
 <PuzzlePageLayout title="One Piece">
@@ -81,5 +155,9 @@
 			manage to get this done to 100% within an hour later. Let's see whether I can fix it some
 			other point in time.
 		</p>
+	</Narigo>
+	<Puzzle title="99.9% Gradient solution" solution={solutionGradients} />
+	<Narigo>
+		<p>Another solution, this time with gradients only. Didn't make it to 100% as well.</p>
 	</Narigo>
 </PuzzlePageLayout>
