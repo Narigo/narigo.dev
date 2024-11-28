5c739d05df03868ccab389bafd1517f010970cb7
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-11-29 00:25:11 +0100

    feat: Remove the animation on the specials page
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/+page.svelte
+++ src/routes/(site)/specials/+page.svelte
@@ -49,47 +49,6 @@
 	</FullWidthSection>
 	<ContentSection>
 		<SpecialNavigation />
-		<AnimationContext name="dialog" defaultDelay={1000}>
-			{#snippet children({ isDone, stopAllAnimations })}
-				{#if !isDone}
-					<div style="position:relative;height:2em">
-						<div style="position:absolute;top:0;right:0;">
-							<StopAllAnimations {stopAllAnimations} />
-						</div>
-					</div>
-				{/if}
-
-				<Bubble>Hey, what's up with this page?</Bubble>
-				<Narigo side="right">I thought I may want to write some code here.</Narigo>
-				<Bubble>Okay, but ...</Bubble>
-				<Bubble delay={1500}>What?!</Bubble>
-				<Narigo side="right">For example an animation for this "exchange" here.</Narigo>
-				<Narigo side="right"
-					>You can <a href="{base}/specials/talking/create">create your own</a>, if you want.</Narigo
-				>
-				<Narigo delay={1000} side="right"
-					>Or a clone of a word game where you can provide your own word: <a
-						href="{base}/specials/weirdle">Weirdle</a
-					></Narigo
-				>
-				<Narigo delay={600} side="right"
-					>Or <a href="{base}/specials/recharge">Recharge</a> someone.</Narigo
-				>
-				<Narigo delay={800} side="right"
-					>Or look up solutions from <a href="{base}/specials/frontend-friday-folks"
-						>Frontend Friday Folks</a
-					>.</Narigo
-				>
-				<Narigo delay={900} side="right"
-					>Or check previous events from <a href="{base}/specials/crafters-vs-codewars"
-						>Crafters vs CodeWars</a
-					>.</Narigo
-				>
-				<Narigo delay={600} side="right"
-					>Or read about things I learnt in small <a href="{base}/specials/today-i-learnt">TIL</a> posts.</Narigo
-				>
-			{/snippet}
-		</AnimationContext>
 		<section>
 			<h3>Some projects / links</h3>
 			<Narigo>
