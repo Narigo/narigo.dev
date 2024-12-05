ee45b3337942a7e885b6eb7344bf90b29db1afac
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-12-05 23:50:52 +0100

    chore: Add test for jumping to next step
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/training/run/+page.svelte
+++ src/routes/(site)/specials/training/run/+page.svelte
@@ -2,19 +2,25 @@
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 
-	const workouts = {
-		short: [],
-		regular: [],
-		long: []
+	const workouts: Record<string, Array<string>> = {
+		short: ['first', 'second'],
+		regular: ['first', 'second'],
+		long: ['first', 'second']
 	};
 
 	const timePerWorkout = $derived($page.url.searchParams.get('timePerWorkout'));
 	const typeOfWorkout = $derived($page.url.searchParams.get('typeOfWorkout'));
-	let currentStep = $state($page.url.searchParams.get('currentStep') ?? 0);
+	let currentStep = $state(parseInt($page.url.searchParams.get('currentStep') ?? '0', 10));
+	const x = $derived(typeOfWorkout ? workouts[typeOfWorkout]?.[currentStep] : null);
+
+	setTimeout(() => {
+		currentStep += 1;
+	}, 2000);
 </script>
 
 <pre>
     timePerWorkout: {timePerWorkout}
      typeOfWorkout: {typeOfWorkout}
        currentStep: {currentStep}
+                 x: {x}
 </pre>
