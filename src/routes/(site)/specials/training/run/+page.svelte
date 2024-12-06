972aed102162c92469f263a4e62c6b8d66e56e78
Joern Bernhardt <joern.bernhardt@googlemail.com>
2024-12-06 23:58:49 +0100

    chore: Check update
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/routes/(site)/specials/training/run/+page.svelte
+++ src/routes/(site)/specials/training/run/+page.svelte
@@ -1,17 +1,57 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
 
-	const workouts: Record<string, Array<string>> = {
-		short: ['first', 'second'],
-		regular: ['first', 'second'],
-		long: ['first', 'second']
+	type Exercise = {
+		title: string;
+		description: Array<string>;
+		image?: string;
+	};
+
+	const exercises = {
+		first: {
+			title: 'Doing A',
+			desription: [
+				'This is the setup.',
+				'Change sides at half time.',
+				'Keeping the feet higher, makes it harder.'
+			]
+		},
+		second: {
+			title: 'Exercise B',
+			desription: [
+				'This is the setup.',
+				'Change sides at half time.',
+				'Keeping the feet higher, makes it harder.'
+			]
+		},
+		third: {
+			title: 'C Workout',
+			desription: [
+				'This is the setup.',
+				'Change sides at half time.',
+				'Keeping the feet higher, makes it harder.'
+			]
+		},
+		fourth: {
+			title: 'D Something',
+			desription: [
+				'This is the setup.',
+				'Change sides at half time.',
+				'Keeping the feet higher, makes it harder.'
+			]
+		}
+	};
+
+	const workoutSessions: Record<string, Array<keyof typeof exercises>> = {
+		short: ['first', 'third'],
+		regular: ['first', 'second', 'third'],
+		long: ['first', 'second', 'third', 'fourth']
 	};
 
 	const timePerWorkout = $derived($page.url.searchParams.get('timePerWorkout'));
 	const typeOfWorkout = $derived($page.url.searchParams.get('typeOfWorkout'));
 	let currentStep = $state(parseInt($page.url.searchParams.get('currentStep') ?? '0', 10));
-	const x = $derived(typeOfWorkout ? workouts[typeOfWorkout]?.[currentStep] : null);
+	const x = $derived(typeOfWorkout ? workoutSessions[typeOfWorkout]?.[currentStep] : null);
 
 	setTimeout(() => {
 		currentStep += 1;
