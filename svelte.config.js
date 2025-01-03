294641f792c4948b6d33368cf53a5352c8667a86
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-01-03 13:56:29 +0100

    chore: Use svelte.config.js
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { Config } from '@sveltejs/kit';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+/** @type import('@sveltejs/kit').Config */
+const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter()
 	}
-} satisfies Config;
+};
+
+export default config;
