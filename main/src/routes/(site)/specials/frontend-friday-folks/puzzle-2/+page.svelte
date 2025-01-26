cb14372e6e75e83e22e391253ed0bf8a8f42904e
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-01-27 00:05:07 +0100

    chore: Format
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- main/src/routes/(site)/specials/frontend-friday-folks/puzzle-2/+page.svelte
+++ main/src/routes/(site)/specials/frontend-friday-folks/puzzle-2/+page.svelte
@@ -6,7 +6,7 @@
 	const solution = `<p><p><p><p><style>body{background:#62374e;margin:34px 50px;display:grid;grid-template-columns:50px 50px;gap:68px 200px}p{background:#fdc57b}`;
 </script>
 
-<PuzzlePageLayout title="Carrom" puzzleId=2>
+<PuzzlePageLayout title="Carrom" puzzleId="2">
 	<Puzzle title="Grid solution" {solution} />
 	<Narigo>
 		This seems to be my highscore right now. Using margins, gaps and a grid seemed to be an easy way
