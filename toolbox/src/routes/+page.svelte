0aaa983ecd136712254928cbbd1b2190b0d7e03c
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-02-05 23:51:13 +0100

    feat: Add toolbox package
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ toolbox/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<h1>Toolbox</h1>
+<p>
+	I use a lot of online tools myself and thanks to ads and cookie banners, the experience is not
+	really that great. So I thought I'm going to build a couple of tools myself whenever I have time
+	and see what I can do. Maybe even have some webassembly tools like image converter or similar
+	things. Tools that could be used offline as well. Let's see where this goes.
+</p>
+<p>For now, I want to build:</p>
+<ul>
+	<li>Base64 encoder / decoder</li>
+	<li>URL encoder / decoder</li>
+	<li>JWT extractor</li>
+</ul>
+<p>
+	Oh, and I'm going to try out this localisation that was added by <code>sv create</code>:
+	ParaglideJS.
+</p>
