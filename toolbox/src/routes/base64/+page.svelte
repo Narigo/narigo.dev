c98af04322c1a3bb6e84556ef75db76d46f38756
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-02-09 23:32:31 +0100

    feat: Add base64 encoder
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ toolbox/src/routes/base64/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let left: HTMLTextAreaElement;
+	let right: HTMLTextAreaElement;
+
+	onMount(() => {
+		left.addEventListener('input', () => {
+			right.value = encodeURIComponent(left.value);
+		});
+	});
+</script>
+
+<section class="container grid grid-cols-1 md:grid-cols-2">
+	<textarea bind:this={left}></textarea>
+	<textarea bind:this={right}></textarea>
+</section>
