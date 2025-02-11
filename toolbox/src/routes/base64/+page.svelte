287592246b3a0ea07bb195f38a78351b3f40cf73
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-02-11 19:45:01 +0100

    chore: Add option to switch inputs
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/src/routes/base64/+page.svelte
+++ toolbox/src/routes/base64/+page.svelte
@@ -12,10 +12,19 @@
 			left.value = decodeURIComponent(right.value);
 		});
 	});
+
+	const swapInputs = () => {
+		const currentLeftValue = left.value;
+		left.value = right.value;
+		right.value = currentLeftValue;
+	};
 </script>
 
 <section class="@container">
-	<div class="grid w-full auto-cols-fr grid-cols-1 @md:grid-cols-2">
+	<div class="grid w-full auto-cols-fr grid-cols-1 @md:grid-cols-3">
+		<h3>Encoded</h3>
+		<button class="row-span-2 bg-gray-200" onclick={swapInputs}>Switch</button>
+		<h3>Decoded</h3>
 		<textarea bind:this={left} class="w-full"></textarea>
 		<textarea bind:this={right} class="w-full"></textarea>
 	</div>
