336ac4683fd4eb725608ecec1e1443d4b67dc4d5
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-02-12 20:28:22 +0100

    chore: Encode and decode when pressing switch
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/src/routes/base64/+page.svelte
+++ toolbox/src/routes/base64/+page.svelte
@@ -4,27 +4,43 @@
 	let left: HTMLTextAreaElement;
 	let right: HTMLTextAreaElement;
 
+	let decodedSide: 'left' | 'right' = $state('left');
+
 	onMount(() => {
 		left.addEventListener('input', () => {
-			right.value = encodeURIComponent(left.value);
+			if (decodedSide === 'left') {
+				right.value = encodeURIComponent(left.value);
+			} else {
+				right.value = decodeURIComponent(left.value);
+			}
 		});
 		right.addEventListener('input', () => {
-			left.value = decodeURIComponent(right.value);
+			if (decodedSide === 'right') {
+				left.value = encodeURIComponent(right.value);
+			} else {
+				left.value = decodeURIComponent(right.value);
+			}
 		});
 	});
 
 	const swapInputs = () => {
-		const currentLeftValue = left.value;
-		left.value = right.value;
-		right.value = currentLeftValue;
+		decodedSide = decodedSide === 'left' ? 'right' : 'left';
+		if (decodedSide === 'left') {
+			left.value = decodeURIComponent(left.value);
+			right.value = encodeURIComponent(right.value);
+		}
 	};
 </script>
 
 <section class="@container">
 	<div class="grid w-full auto-cols-fr grid-cols-1 @md:grid-cols-3">
-		<h3>Encoded</h3>
+		<h3>
+			{#if decodedSide === 'left'}Decoded{:else}Encoded{/if}
+		</h3>
 		<button class="row-span-2 bg-gray-200" onclick={swapInputs}>Switch</button>
-		<h3>Decoded</h3>
+		<h3>
+			{#if decodedSide === 'right'}Decoded{:else}Encoded{/if}
+		</h3>
 		<textarea bind:this={left} class="w-full"></textarea>
 		<textarea bind:this={right} class="w-full"></textarea>
 	</div>
