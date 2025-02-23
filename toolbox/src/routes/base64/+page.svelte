1db0e16025b747155ace2d82d6dcebc37b9c3dcb
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-02-24 00:50:01 +0100

    fix: Actual base64 encoding instead of url encoding
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/src/routes/base64/+page.svelte
+++ toolbox/src/routes/base64/+page.svelte
@@ -2,10 +2,15 @@
 	import FullWidthSection from '$lib/common/FullWidthSection.svelte';
 	import PageLayout from '$lib/common/PageLayout.svelte';
 	import Encoder from '$lib/tools/encoder/Encoder.svelte';
+
+	const decode = (s: string) => Uint8Array.from(atob(s), (c) => c.charCodeAt(0));
+	const encode = (b: Uint8Array) => btoa(String.fromCharCode(...b));
+	const decodeToString = (s: string) => new TextDecoder().decode(decode(s));
+	const encodeString = (s: string) => encode(new TextEncoder().encode(s));
 </script>
 
 <PageLayout>
 	<FullWidthSection>
-		<Encoder encode={atob} decode={btoa} />
+		<Encoder encode={encodeString} decode={decodeToString} />
 	</FullWidthSection>
 </PageLayout>
