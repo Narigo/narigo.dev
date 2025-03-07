1cd35b23649fa7594de9de51f74a06fc5fed4e79
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-03-07 01:14:55 +0100

    chore: Add font to view
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/src/routes/+layout.svelte
+++ toolbox/src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import '@fontsource/fira-sans';
+	import '@fontsource/fira-code';
+	import '@fontsource/fira-mono';
 	import { i18n } from "$lib/i18n";
 	import { ParaglideJS } from "@inlang/paraglide-sveltekit";
 	import '../app.css';
