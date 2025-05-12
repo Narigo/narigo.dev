50a3d787c34318c1e1dacd52f796f066b93e7fe1
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-05-13 00:34:20 +0200

    chore: Use mkcert certificate instead of basicSsl to enable android emulator test
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/vite.config.ts
+++ toolbox/vite.config.ts
@@ -1,7 +1,7 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { readFileSync } from 'node:fs';
 
 export default defineConfig({
 	plugins: [
@@ -9,9 +9,17 @@ export default defineConfig({
 		paraglide({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
-		}),
-		basicSsl()
+		})
 	],
+
+	server: {
+		https: {
+			key: readFileSync('./localcert-key.pem'),
+			cert: readFileSync('./localcert.pem')
+		},
+		host: '0.0.0.0',
+		port: 5173
+	},
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
