1bbd26496bdca70025fefc570aa2121d6d75789a
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-06-02 02:12:30 +0200

    fix: Bundle fonts (hopefully)
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- main/vite.config.ts
+++ main/vite.config.ts
@@ -1,11 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite'
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig, mergeConfig } from 'vitest/config';
 import type { UserConfig } from 'vite';
 
 export default mergeConfig(
 	{
-		plugins: [enhancedImages(), sveltekit()]
+		plugins: [tailwindcss(), enhancedImages(), sveltekit()]
 	},
 	defineConfig({
 		test: {
