8e16a0486b9a8381bd143619a492b82e3a9dc2f3
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-06-04 23:42:15 +0200

    chore: Fix tailwind intellisense by using an app.css file instead app.postcss
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/src/routes/+layout.svelte
+++ toolbox/src/routes/+layout.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
-	import '@fontsource/fira-sans/200';
-	import '@fontsource/fira-sans/400';
-	import '@fontsource/fira-sans/400-italic';
-	import '@fontsource/fira-sans/600';
-	import '@fontsource/fira-sans/700';
-	import '@fontsource/fira-code';
-	import '@fontsource/fira-mono';
-	import { i18n } from "$lib/i18n";
-	import { ParaglideJS } from "@inlang/paraglide-sveltekit";
-	import '../app.postcss';
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import '../app.css';
 	let { children } = $props();
 </script>
 
--- toolbox/src/routes/document-scanner/+page.svelte
+++ toolbox/src/routes/document-scanner/+page.svelte
@@ -148,38 +148,44 @@
 				<button class="rounded border bg-gray-100 p-4" onclick={nextCamera}>Next camera</button>
 			{/if}
 		{:else if scannerState === 'processing' && openCv}
-			<ol class="flex h-32 flex-row gap-4">
-				{#each extractedImages as image, index}
-					{#if image.result}
-						<li
-							class="relative max-h-32 max-w-32"
-							style="aspect-ratio:{image.result!.height}/{image.result!.width}"
+			<section>
+				<ol class="flex h-32 flex-row gap-4">
+					{#each extractedImages as image, index}
+						{#if image.result}
+							<li
+								class="relative max-h-32 max-w-32"
+								style="aspect-ratio:{image.result!.height}/{image.result!.width}"
+							>
+								<input type="checkbox" name="image-{index}" value="selected" />
+								<button
+									class="h-4 w-4"
+									onclick={() => {
+										extractedImages = [
+											...extractedImages.slice(0, index),
+											...extractedImages.slice(index + 1)
+										];
+									}}>x</button
+								>
+								<button
+									class="absolute top-0 right-0 h-full"
+									onclick={() => (image.result = undefined)}
+									><img
+										class="max-h-full"
+										src={image.result.toDataURL('image/jpeg', 0.9)}
+										alt="Scanned image number {index}"
+									/></button
+								>
+							</li>
+						{/if}
+					{/each}
+					<li class="relative max-h-32 max-w-32" style="aspect-ratio:3/2">
+						<button class="h-full w-full border bg-gray-100" onclick={() => startScanning()}
+							>+</button
 						>
-							<button
-								class="h-4 w-4"
-								onclick={() =>
-									(extractedImages = [
-										...extractedImages.slice(0, index),
-										...extractedImages.slice(index + 1)
-									])}>x</button
-							>
-							<button
-								class="absolute top-0 right-0 h-full"
-								onclick={() => (image.result = undefined)}
-								><img
-									class="max-h-full"
-									src={image.result.toDataURL('image/jpeg', 0.9)}
-									alt="Scanned image number {index}"
-								/></button
-							>
-						</li>
-					{/if}
-				{/each}
-				<li class="relative max-h-32 max-w-32" style="aspect-ratio:3/2">
-					<button class="h-full w-full border bg-gray-100" onclick={() => startScanning()}>+</button
-					>
-				</li>
-			</ol>
+					</li>
+				</ol>
+			</section>
+
 			<section class="isolate grid grid-cols-1 grid-rows-1">
 				{#each extractedImages as image, index}
 					{#if !image.result}
