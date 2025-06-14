1469f587fde754d412228b1b1deb57a318094817
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-06-14 04:25:56 +0200

    chore: Update styling of LandingScreen
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/src/routes/document-scanner/LoadingScreen.svelte
+++ toolbox/src/routes/document-scanner/LoadingScreen.svelte
@@ -16,18 +16,48 @@
 	.loader {
 		--percentage: 0;
 		--orbit-size: 4em;
-		animation: 4s loader infinite;
+		animation: 5s loader infinite linear;
 
-        width: 10em;
-        height: 10em;
+		width: calc(2 * (var(--orbit-size) + 1.25em));
+		height: calc(2 * (var(--orbit-size) + 1.25em));
 
-		background: radial-gradient(
-				circle at calc(cos(var(--percentage)) * var(--orbit-size))
-					calc(cos(var(--percentage)) * var(--orbit-size)),
-				hsl(240, 100%, calc(var(--percentage) * 100%)) calc(var(--percentage) * var(--orbit-size)),
+		background:
+			radial-gradient(
+				circle,
+				hsl(calc((var(--percentage) + .00) * 720), 100%, calc(60% + sin(var(--percentage) * 540deg) * 10%)) calc(1em + sin((var(--percentage) + .00) * 1080deg) * .25em),
 				transparent 0
-			)
-			lime;
+			) calc(50% + sin((var(--percentage) + .00) * 360deg) * var(--orbit-size))
+			  calc(50% - cos((var(--percentage) + .00) * 360deg) * var(--orbit-size))
+              no-repeat,
+			radial-gradient(
+				circle,
+				hsl(calc((var(--percentage) + .15) * 720), 100%, calc(60% + sin(var(--percentage) * 540deg) * 10%)) calc(1em + sin((var(--percentage) + .5) * 1080deg) * .25em),
+				transparent 0
+			) calc(50% + sin((var(--percentage) + .20) * 360deg) * var(--orbit-size))
+			  calc(50% - cos((var(--percentage) + .20) * 360deg) * var(--orbit-size))
+              no-repeat,
+			radial-gradient(
+				circle,
+				hsl(calc((var(--percentage) + .30) * 720), 100%, calc(60% + sin(var(--percentage) * 540deg) * 10%)) calc(1em + sin((var(--percentage) + .10) * 1080deg) * .25em),
+				transparent 0
+			) calc(50% + sin((var(--percentage) + .40) * 360deg) * var(--orbit-size))
+			  calc(50% - cos((var(--percentage) + .40) * 360deg) * var(--orbit-size))
+              no-repeat,
+			radial-gradient(
+				circle,
+				hsl(calc((var(--percentage) + .45) * 720), 100%, calc(60% + sin(var(--percentage) * 540deg) * 10%)) calc(1em + sin((var(--percentage) + .15) * 1080deg) * .25em),
+				transparent 0
+			) calc(50% + sin((var(--percentage) + .60) * 360deg) * var(--orbit-size))
+			  calc(50% - cos((var(--percentage) + .60) * 360deg) * var(--orbit-size))
+              no-repeat,
+			radial-gradient(
+				circle,
+				hsl(calc((var(--percentage) + .60) * 720), 100%, calc(60% + sin(var(--percentage) * 540deg) * 10%)) calc(1em + sin((var(--percentage) + .20) * 1080deg) * .25em),
+				transparent 0
+			) calc(50% + sin((var(--percentage) + .80) * 360deg) * var(--orbit-size))
+			  calc(50% - cos((var(--percentage) + .80) * 360deg) * var(--orbit-size))
+              no-repeat
+            ;
 	}
 
 	@property --percentage {
@@ -41,7 +71,7 @@
 			--percentage: 0;
 		}
 		100% {
-			--percentage: 1;
+			--percentage: 2;
 		}
 	}
 </style>
