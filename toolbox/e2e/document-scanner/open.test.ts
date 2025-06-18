5a0044eb1ab8f21a4ed4c980d927f9cc500a8513
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-06-18 22:52:38 +0200

    chore: Add test for preview and camera
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/e2e/document-scanner/open.test.ts
+++ toolbox/e2e/document-scanner/open.test.ts
@@ -4,3 +4,9 @@ test('document scanner has expected start button', async ({ page }) => {
 	await page.goto('/document-scanner');
 	await expect(page.locator('button')).toHaveText('start', { ignoreCase: true });
 });
+
+test('document scanner has a preview bar and a camera section', async ({page}) => {
+    await page.goto('/document-scanner');
+    await expect(page.locator('section')).toHaveText('preview', { ignoreCase: true });
+    await expect(page.locator('section')).toHaveText('camera', { ignoreCase: true });
+})
