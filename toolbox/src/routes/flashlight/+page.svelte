b568b830dde5526a300444a13671a52a701274f4
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-07-14 00:03:16 +0200

    feat: Add flashlight icon
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/src/routes/flashlight/+page.svelte
+++ toolbox/src/routes/flashlight/+page.svelte
@@ -2,12 +2,16 @@
 	import { base } from '$app/paths';
 	import FullWidthSection from '$lib/common/FullWidthSection.svelte';
 	import PageLayout from '$lib/common/PageLayout.svelte';
+	import flashlightImageError from './flashlight-error.svg';
+	import flashlightImageOn from './flashlight-on.svg';
+	import flashlightImageOff from './flashlight-off.svg';
 	import { m } from '$lib/paraglide/messages';
 
 	type FlashlightState =
+		| 'error-needs-permission'
+		| 'error-no-flashlight-found'
 		| 'on'
 		| 'off'
-		| 'needs-permission'
 		| 'trying-to-turn-on'
 		| 'trying-to-turn-off';
 	let flashlightState = $state<FlashlightState>('off');
@@ -29,7 +33,7 @@
 					const videoTracks = userMedia.getVideoTracks();
 					if (videoTracks.length === 0) {
 						console.error('No video track found');
-						flashlightState = 'off';
+						flashlightState = 'error-no-flashlight-found';
 						return;
 					}
 					flashlightState = 'on';
@@ -39,22 +43,51 @@
 					return;
 				}
 			} else if (permissionStatus.state === 'prompt') {
-				flashlightState = 'needs-permission';
+				flashlightState = 'error-needs-permission';
 			} else {
 				flashlightState = 'off';
 			}
 		} else {
-			flashlightState = 'needs-permission';
+			flashlightState = 'error-needs-permission';
 		}
 	}
 </script>
 
 <PageLayout backLink="{base}/">
 	<FullWidthSection>
-		{#if flashlightState === 'on'}
-			<button class="h-dvh w-full" onclick={turnOff}>{m['flashlight.switchOff']()}</button>
+		{#if ['on', 'trying-to-turn-on'].includes(flashlightState)}
+			<button
+				class="grid h-dvh w-full grid-cols-1 place-items-center gap-4 border bg-gray-50"
+				disabled={flashlightState === 'trying-to-turn-on'}
+				onclick={turnOff}
+			>
+				<img class="self-end" src={flashlightImageOn} alt="Turned on flashlight" />
+				<div class="self-start">{m['flashlight.switchOff']()}</div>
+			</button>
+		{:else if flashlightState === 'error-no-flashlight-found'}
+			<button
+				class="grid h-dvh w-full grid-cols-1 place-items-center gap-4 border bg-red-200"
+				disabled
+			>
+				<img class="self-end" src={flashlightImageError} alt="Broken flashlight" />
+				<div class="self-start">{m['flashlight.errors.noFlashlightFound']()}</div>
+			</button>
+		{:else if flashlightState === 'error-needs-permission'}
+			<button
+				class="grid h-dvh w-full grid-cols-1 place-items-center gap-4 border bg-red-200"
+				disabled
+			>
+				<img class="self-end" src={flashlightImageError} alt="Broken flashlight" />
+				<div class="self-start">{m['flashlight.errors.noPermissionGranted']()}</div>
+			</button>
 		{:else}
-			<button class="h-dvh w-full" onclick={turnOn}>{m['flashlight.switchOn']()}</button>
+			<button
+				class="grid h-dvh w-full grid-cols-1 place-items-center gap-4 border bg-gray-600"
+				onclick={turnOn}
+			>
+				<img class="self-end" src={flashlightImageOff} alt="Turned off flashlight" />
+				<div class="self-start text-white">{m['flashlight.switchOn']()}</div>
+			</button>
 		{/if}
 	</FullWidthSection>
 </PageLayout>
