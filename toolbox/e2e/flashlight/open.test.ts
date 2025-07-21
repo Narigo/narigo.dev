ad9908936f153ae12b98d0f69730300c62c03716
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-07-21 02:11:21 +0200

    chore: Add a test for flashlight
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ toolbox/e2e/flashlight/open.test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from '@playwright/test';
+
+test('flashlight has expected start button', async ({ page }) => {
+	await page.goto('/flashlight');
+	await expect(page.locator('button img[alt^="Turned off"]')).toBeVisible();
+});
+
+test('flashlight asks for permission if it needs it', async ({ page, context }) => {
+	await page.goto('/flashlight');
+	await page.locator('button img[alt^="Turned off"]').click();
+	await expect(page.locator('button img[alt^="Broken"]')).toBeVisible();
+});
+
+test('flashlight should be turned on with proper permissions', async ({ page, context }) => {
+	await page.goto('/flashlight');
+	await page.locator('button img[alt^="Turned off"]').click();
+	await context.grantPermissions(['camera']);
+	await expect(page.locator('button img[alt^="Turned on"]')).toBeVisible();
+});
