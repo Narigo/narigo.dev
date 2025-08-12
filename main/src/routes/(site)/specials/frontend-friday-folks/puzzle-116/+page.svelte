cffa440ffad1c55a87612a15ed1594b4f3d9a143
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-08-12 03:58:03 +0200

    feat: Add solution for 116
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ main/src/routes/(site)/specials/frontend-friday-folks/puzzle-116/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<p l>
+<p r>
+<div>
+<style>
+  &{background:#293D7E}
+  p {
+    position:fixed;
+    background:#6E91CA;
+    top:149;
+    left: 115;
+    width: 60;
+    height: 80;
+    border-radius: 60px 60px 80px 80px;
+  }
+  [r]{left:225}
+  div{border:20px solid #6E91CA;border-bottom:0;border-radius:85px 85px 0 0;height:130;width:130;position:fixed;left:115;top:45}
+</style>`;
+</script>
+
+<PuzzlePageLayout title="Headphones" puzzleId="116">
+	<Puzzle title="Solution" {solution} />
+	<Narigo>
+		<p>
+			I guess this is almost straightforward. Using single elements turned out quite effective here.
+		</p>
+	</Narigo>
+</PuzzlePageLayout>
