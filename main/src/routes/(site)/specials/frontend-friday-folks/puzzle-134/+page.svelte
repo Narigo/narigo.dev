bc25ff871ee3d24dab2be1320eb47295dccaaa5e
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-11-01 23:51:09 +0100

    chore: Remove unnecessary import
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- main/src/routes/(site)/specials/frontend-friday-folks/puzzle-134/+page.svelte
+++ main/src/routes/(site)/specials/frontend-friday-folks/puzzle-134/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import { resolve } from '$app/paths';
 	import Narigo from '$lib/common/bubble/Narigo.svelte';
 	import Puzzle from '../Puzzle.svelte';
 	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
