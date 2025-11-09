2fad30e7963ddb1f5a492fc6caf2ddc40f043251
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-11-09 23:56:22 +0100

    feat: Add puzzle 112 solution
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ main/src/routes/(site)/specials/frontend-friday-folks/puzzle-112/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Code from '$lib/common/Code.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div o>
+<div a></div>
+<div b></div>
+<div c></div>
+</div>
+<style>
+  body {
+    background: #6592CF;
+  }
+  [o]{
+    -webkit-box-reflect:right;
+    width: 50%;
+  }
+  [a],[b],[c] {
+    position: absolute;
+    top:50%;
+    right:50%;
+    background: linear-gradient(38.5deg, #0000 78px, #293D7E 0 102px, #0000 0);
+    height: 100px;
+    width: 125px;
+  }
+  [b]{
+    top:100px;
+  }
+  [c]{
+    top:50px;
+  }
+</style>
+`;
+</script>
+
+<PuzzlePageLayout title="Chevron" puzzleId="112">
+	<Puzzle title="Gradient Mirror Solution" {solution} />
+	<Narigo>
+		<p>
+			I had multiple, different solutions to this puzzle. One of them was using a <Code
+				>linear-gradient</Code
+			> in three elements and a <Code>-webkit-box-reflect</Code> to mirror them to the right.
+		</p>
+	</Narigo>
+</PuzzlePageLayout>
