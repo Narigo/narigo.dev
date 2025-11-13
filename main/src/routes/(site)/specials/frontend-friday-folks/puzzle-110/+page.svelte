b9cb0db116fe748a24fa527c84d5500723b1a1a1
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-11-13 23:58:47 +0100

    feat: Add puzzle 110
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ main/src/routes/(site)/specials/frontend-friday-folks/puzzle-110/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<div><div><div><div><div><div><div>
+<style>
+  body {
+    background:#D25B70;
+    transform: translate(131px, 132px) rotate(-30deg);
+  }
+  div {
+    rotate: 30deg;
+    transform-origin: 120px 5px;
+    width: 100px;
+    height: 10px;
+    background: #F2E09F;
+    border-radius: 9px;
+  }
+</style>
+`;
+</script>
+
+<PuzzlePageLayout title="Sunrays" puzzleId="110">
+	<Puzzle title="Solution" {solution} />
+	<Narigo>
+		<p>Put the rotation point (transform-origin) in the middle and rotate the divs around it!</p>
+	</Narigo>
+</PuzzlePageLayout>
