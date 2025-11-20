2db0e73721cbeea02402bdf5e064e26dc88befce
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-11-21 00:49:48 +0100

    chore: Try decode something
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/src/routes/aton-nif/+page.svelte
+++ toolbox/src/routes/aton-nif/+page.svelte
@@ -5,8 +5,33 @@
 	import { m } from '$lib/paraglide/messages';
 	import Encoder from '$lib/tools/encoder/Encoder.svelte';
 
-	const decode = (decimal: string) => decimal;
-	const encode = (atonNif: string) => atonNif;
+	const atonNifToDecimalMap = {
+		'0': 0,
+		'1': 1,
+		'2': 2,
+		'3': 3,
+		'4': 4,
+		'5': 5,
+		A: 6,
+		'6': 7,
+		N: 8,
+		'7': 9,
+		'8': 10,
+		'9': 11
+	} as const;
+	const decode = (decimal: string) => {
+		return decimal;
+	};
+	const encode = (atonNif: string) => {
+		const atonNifDigits = atonNif.split('') as Array<keyof typeof atonNifToDecimalMap>;
+		console.log(atonNifDigits);
+		let decimal = 0;
+		for (let i = atonNifDigits.length; i > 0; i--) {
+			const decimalDigit = atonNifToDecimalMap[atonNifDigits[i - 1]];
+			decimal = decimalDigit;
+		}
+		return decimal;
+	};
 </script>
 
 <PageLayout
