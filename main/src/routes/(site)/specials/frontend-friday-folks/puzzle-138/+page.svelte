97dc82695f6c1ee44b9272e2beea8dbfef3b8583
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-11-22 22:37:52 +0100

    feat: Add puzzle 138
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ main/src/routes/(site)/specials/frontend-friday-folks/puzzle-138/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<p>
+<style>
+  *{background:#926927}
+  p,p:after,p:before {
+    margin:0;
+    position:absolute;
+    content:'';
+    width:100;
+    height:100;
+    border-radius: 100% 0;
+    background:#6D480A;
+    transform-origin:0 100%;
+    transform:translate(192px, 112px);
+  &:after {
+    background: #fff;
+    transform:translate(0px, 1px) rotate(-45deg);
+  }
+  &:before {
+    transform:rotate(-90deg);
+`;
+</script>
+
+<PuzzlePageLayout title="Lotus" puzzleId="138">
+	<Puzzle title="Lotus solution" {solution} />
+	<Narigo>
+		<p>
+			No radial gradients here, because it was so much easier to use divs with border radius for
+			this...!
+		</p>
+	</Narigo>
+</PuzzlePageLayout>
