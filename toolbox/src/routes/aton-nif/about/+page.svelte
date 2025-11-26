c39545536974db392cecbee4c7b2576c04ddc5d3
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-11-27 00:51:44 +0100

    chore: Make about text prose
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/src/routes/aton-nif/about/+page.svelte
+++ toolbox/src/routes/aton-nif/about/+page.svelte
@@ -10,16 +10,18 @@
 	backLink={resolve('/aton-nif')}
 >
 	<FullWidthSection>
-		<h2>The Aton-Nif system</h2>
-		<p>
-			During the B4Y3RW4LD-Hackathon in November 2025, we rediscovered two long lost numbers: <strong
-				>Aton</strong
-			>
-			and <strong>Nif</strong>.
-		</p>
-		<p>
-			Talking to some experts from the Silicon Valley, we have found some evidence that points to a
-			conspiracy regarding the current numbering system.
-		</p>
+		<div class="prose">
+			<h2>The Aton-Nif system</h2>
+			<p>
+				During the B4Y3RW4LD-Hackathon in November 2025, we rediscovered two long lost numbers: <strong
+					>Aton</strong
+				>
+				and <strong>Nif</strong>.
+			</p>
+			<p>
+				Talking to some experts from the Silicon Valley, we have found some evidence that points to
+				a conspiracy regarding the current numbering system.
+			</p>
+		</div>
 	</FullWidthSection>
 </PageLayout>
