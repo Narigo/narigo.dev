64661421028d97f969452c1c59f2d1307e4c04f5
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-11-28 20:05:42 +0100

    feat: Add puzzle 139
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ main/src/routes/(site)/specials/frontend-friday-folks/puzzle-139/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<style>&{background:
+  linear-gradient(#E38F66 90px,#3210 0),
+  conic-gradient(#E38F66 165deg,#FFFBDA 0 195deg, #E38F66 0)0 -265px / 100% 475px no-repeat,
+  linear-gradient(90deg,#E38F66 195px,#62306D 0 205px,#E38F66 0)`;
+</script>
+
+<PuzzlePageLayout title="Lamp" puzzleId="139">
+	<Puzzle title="Lamp solution" {solution} />
+	<Narigo>
+		<p>
+			Finally a good solution with gradients again! 😅 Making gradients not fill out the whole
+			background is nice.
+		</p>
+	</Narigo>
+</PuzzlePageLayout>
