76f24ba431a7da927213a2fe2c23ebf46bd8232d
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-11-29 23:07:33 +0100

    feat: Add puzzle 140
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ main/src/routes/(site)/specials/frontend-friday-folks/puzzle-140/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<style>&{background:
+  conic-gradient(#3210 135deg, #1038BF 0 225deg, #3210 0) 0 -24px / 100% 324px,
+  radial-gradient(1q,#4F77FF 40px, #3210 0)0 -20px / 100% 100% no-repeat,
+  radial-gradient(1q,#FFF 40px, #4F77FF 0)0 -60px`;
+</script>
+
+<PuzzlePageLayout title="Eclipse" puzzleId="140">
+	<Puzzle title="Eclipse solution" {solution} />
+	<Narigo>
+		<p>
+			I began with the mountain and when I removed the part of the white moon, I figured out that
+			changing the layers (and transparency values) makes more sense here.
+		</p>
+	</Narigo>
+</PuzzlePageLayout>
