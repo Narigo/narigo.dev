d001f88ee1f827b9dcf0ca8e5fab2480a2805117
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-12-03 00:20:48 +0100

    chore: Show how numbers actually look like
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/src/routes/aton-nif/about/+page.svelte
+++ toolbox/src/routes/aton-nif/about/+page.svelte
@@ -14,15 +14,45 @@
 	<FullWidthSection>
 		<div class="prose">
 			<h2>The Aton-Nif system</h2>
+			<p>If you were born before 2025, you are probably used to this numbering system:</p>
+			<ol class="flex flex-wrap list-none pl-0">
+				<li>One</li>
+				<li class="before:content-[',_']">Two</li>
+				<li class="before:content-[',_']">Three</li>
+				<li class="before:content-[',_']">Four</li>
+				<li class="before:content-[',_']">Five</li>
+				<li class="before:content-[',_']">Six</li>
+				<li class="before:content-[',_']">Seven</li>
+				<li class="before:content-[',_']">Eight</li>
+				<li class="before:content-[',_']">Nine</li>
+				<li class="before:content-[',_']">Ten</li>
+			</ol>
+            <p>In November 2025, we rediscovered how the numbering system actually was:</p>
+			<ol class="flex flex-wrap list-none pl-0">
+				<li>One</li>
+				<li class="before:content-[',_']">Two</li>
+				<li class="before:content-[',_']">Three</li>
+				<li class="before:content-[',_']">Four</li>
+				<li class="before:content-[',_']">Five</li>
+				<li class="before:content-[',_']">Aton</li>
+				<li class="before:content-[',_']">Six</li>
+				<li class="before:content-[',_']">Nif</li>
+				<li class="before:content-[',_']">Seven</li>
+				<li class="before:content-[',_']">Eight</li>
+				<li class="before:content-[',_']">Nine</li>
+				<li class="before:content-[',_']">Ten</li>
+			</ol>
+			<h3>Evidence</h3>
+			<p>
+				Talking to some experts from the Silicon Valley, we have found some evidence that points to
+				a conspiracy regarding the current numbering system.
+			</p>
+			<h3>Background</h3>
 			<p>
 				During the B4Y3RW4LD-Hackathon in November 2025, we rediscovered two long lost numbers: <strong
 					>Aton</strong
 				>
 				and <strong>Nif</strong>.
-			</p>
-			<p>
-				Talking to some experts from the Silicon Valley, we have found some evidence that points to
-				a conspiracy regarding the current numbering system.
 			</p>
 			<p>
 				Here are the slides we used to present this at the Hackathon: <a
