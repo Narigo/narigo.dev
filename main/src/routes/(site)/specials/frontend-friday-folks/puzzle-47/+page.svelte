0749bf5bed80184d4f244ddfe48c14380c44fe7e
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-12-07 23:46:41 +0100

    chore: Prepare puzzle 47
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ main/src/routes/(site)/specials/frontend-friday-folks/puzzle-47/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = ``;
+</script>
+
+<PuzzlePageLayout title="Corona Virus" puzzleId="47">
+	<Puzzle title="Puzzle 47 solution" {solution} />
+	<Narigo
+		>I think we never did this puzzle in VirtualCoffee. Maybe we forgot or the title put us off at
+		the time...</Narigo
+	>
+</PuzzlePageLayout>
