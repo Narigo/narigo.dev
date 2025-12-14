acc5fb85d059f15401475abdadcd6a18fcc9b910
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-12-14 04:50:31 +0100

    feat: Add puzzle 142
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

---
+++ main/src/routes/(site)/specials/frontend-friday-folks/puzzle-142/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Narigo from '$lib/common/bubble/Narigo.svelte';
+	import Code from '$lib/common/Code.svelte';
+	import Puzzle from '../Puzzle.svelte';
+	import PuzzlePageLayout from '../PuzzlePageLayout.svelte';
+
+	const solution = `<style>*{background:linear-gradient(90deg,#1038BF 60px,#3210 0 80px,#fff 0 100px,#3210 0 120px,#1038BF 0 180px,#3210 0 200px,#fff 0 220px,#3210 0 240px,#1038BF 0 300px,#3210 0)50px /100% 200px no-repeat,#4F77FF`;
+</script>
+
+<PuzzlePageLayout title="Curtains" puzzleId="142">
+	<Puzzle title="Curtains solution" {solution} />
+	<Narigo>
+		<p>
+			I couldn't make it to the VC round this time, but I've done it later. It just screamed
+			linear-gradient to me.
+		</p>
+	</Narigo>
+</PuzzlePageLayout>
