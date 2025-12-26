a31dcaab5c25e97cd9b8a273f255641d45ee8c70
Joern Bernhardt <joern.bernhardt@googlemail.com>
2025-12-26 02:09:17 +0100

    chore: Structure tests
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- toolbox/e2e/aton-nif/conversion.test.ts
+++ toolbox/e2e/aton-nif/conversion.test.ts
@@ -1,30 +1,39 @@
 import { expect, test } from '@playwright/test';
 import translations from '../../messages/en.json' with { type: 'json' };
 
-test('aton-nif converter works for small numbers', async ({ page }) => {
-	await page.goto('/aton-nif');
-
-	async function testAtonNifConversion(input: string, expected: string) {
+test.describe('aton-nif conversion', () => {
+	test('works for small numbers from aton-nif to decimal', async ({ page }) => {
+		await page.goto('/aton-nif');
 		const decoder = await page.getByLabel(translations.tools.atonNifConverter.decodeLabel);
-		await decoder.fill(input);
-		await expect(decoder).toHaveValue(input);
 		const encoder = await page.getByLabel(translations.tools.atonNifConverter.encodeLabel);
-		await expect(encoder).toHaveValue(expected);
-	}
+		async function testAtonNifToDecimalConversion(input: string, expected: string) {
+			await decoder.fill(input);
+			await expect(decoder).toHaveValue(input);
+			await expect(encoder).toHaveValue(expected);
+		}
 
-	await testAtonNifConversion('1', '1');
-	await testAtonNifConversion('A', '6');
-	await testAtonNifConversion('N', '8');
-	await testAtonNifConversion('10', '12');
-	await testAtonNifConversion('1A', '18');
-});
+		await testAtonNifToDecimalConversion('1', '1');
+		await testAtonNifToDecimalConversion('A', '6');
+		await testAtonNifToDecimalConversion('N', '8');
+		await testAtonNifToDecimalConversion('10', '12');
+		await testAtonNifToDecimalConversion('1A', '18');
+	});
 
-test('aton-nif converter has labelled encoder and decoder inputs', async ({ page }) => {
-	await page.goto('/aton-nif');
+	test('works for numbers from decimal to aton-nif', async ({ page }) => {
+		await page.goto('/aton-nif');
+		const decoder = await page.getByLabel(translations.tools.atonNifConverter.decodeLabel);
+		const encoder = await page.getByLabel(translations.tools.atonNifConverter.encodeLabel);
+		async function testAtonNifToDecimalConversion(input: string, expected: string) {
+			await encoder.fill(input);
+			await expect(encoder).toHaveValue(input);
+			await expect(decoder).toHaveValue(expected);
+		}
 
-	const encoder = await page.getByLabel(translations.tools.atonNifConverter.encodeLabel);
-	const decoder = await page.getByLabel(translations.tools.atonNifConverter.decodeLabel);
-
-	await expect(encoder).toBeVisible();
-	await expect(decoder).toBeVisible();
+		await testAtonNifToDecimalConversion('1', '1');
+		await testAtonNifToDecimalConversion('6', 'A');
+		await testAtonNifToDecimalConversion('8', 'N');
+		await testAtonNifToDecimalConversion('12', '10');
+		await testAtonNifToDecimalConversion('18', '1A');
+		await testAtonNifToDecimalConversion('144', '100');
+	});
 });
--- toolbox/e2e/aton-nif/structure.test.ts
+++ toolbox/e2e/aton-nif/structure.test.ts
@@ -1,19 +1,21 @@
 import { expect, test } from '@playwright/test';
 import translations from '../../messages/en.json' with { type: 'json' };
 
-test('aton-nif converter has a link to the about page', async ({ page }) => {
-	await page.goto('/aton-nif');
-	await expect(
-		page.locator('a').getByText(translations.tools.atonNifConverter.aboutLink)
-	).toBeVisible();
-});
+test.describe('aton-nif converter', () => {
+	test('has a link to the about page', async ({ page }) => {
+		await page.goto('/aton-nif');
+		await expect(
+			page.locator('a').getByText(translations.tools.atonNifConverter.aboutLink)
+		).toBeVisible();
+	});
 
-test('aton-nif converter has labelled encoder and decoder inputs', async ({ page }) => {
-	await page.goto('/aton-nif');
+	test('has labelled encoder and decoder inputs', async ({ page }) => {
+		await page.goto('/aton-nif');
 
-	const encoder = await page.getByLabel(translations.tools.atonNifConverter.encodeLabel);
-	const decoder = await page.getByLabel(translations.tools.atonNifConverter.decodeLabel);
+		const encoder = await page.getByLabel(translations.tools.atonNifConverter.encodeLabel);
+		const decoder = await page.getByLabel(translations.tools.atonNifConverter.decodeLabel);
 
-	await expect(encoder).toBeVisible();
-	await expect(decoder).toBeVisible();
+		await expect(encoder).toBeVisible();
+		await expect(decoder).toBeVisible();
+	});
 });
